806ad33131f78392c7bc53ecbf9b9d83349238a4
jackie.lee <ueelife@163.com>
2013-01-11 22:49:59 +0800

    remove mongo extend

--- code/webroot/protected/config/main.php
+++ code/webroot/protected/config/main.php
@@ -41,7 +41,7 @@ return array(
 			'allowAutoLogin'=>true,
 		),
 		'session'=>array(
-			'class'=>'CMongodbHttpSession',
+			//'class'=>'CMongodbHttpSession',
 		),
 		"solr" => array(
 	        "class" => "packages.solr.ASolrConnection",

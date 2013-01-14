0a0d967a2afb0ebe796b26dca519f3428c66ce33
xiaofuqian <xiao2008abc@163.com>
2013-01-14 21:37:05 +0800

    update user login

--- code/webroot/protected/views/site/index.php
+++ code/webroot/protected/views/site/index.php
@@ -2,6 +2,8 @@
 /* @var $this SiteController */
 
 $this->pageTitle=Yii::app()->name;
+$loginUserName=Yii::app()->user->getName();
+echo $loginUserName;
 ?>
 
 <h1>Welcome to <i><?php echo CHtml::encode(Yii::app()->name); ?></i></h1>

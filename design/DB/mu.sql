68d68bbe98ecee8c14e2487dcf68d2950dbe9ebe
xiaofuqian <xiao2008abc@163.com>
2013-01-23 21:29:25 +0800

    mu sql

---
+++ design/DB/mu.sql
@@ -0,0 +1,60 @@
+# --------------------------------------------------------
+# Host:                         127.0.0.1
+# Server version:               5.1.28-rc-community
+# Server OS:                    Win32
+# HeidiSQL version:             6.0.0.3603
+# Date/time:                    2013-01-23 21:28:37
+# --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+# Data exporting was unselected.
+
+# Data exporting was unselected.
+
+# Data exporting was unselected.
+
+# Data exporting was unselected.
+
+# Data exporting was unselected.
+
+# Data exporting was unselected.
+
+# Data exporting was unselected.
+
+# Data exporting was unselected.
+
+# Data exporting was unselected.
+
+# Data exporting was unselected.
+
+# Data exporting was unselected.
+
+# Data exporting was unselected.
+
+# Data exporting was unselected.
+
+# Data exporting was unselected.
+
+# Data exporting was unselected.
+
+# Data exporting was unselected.
+
+# Data exporting was unselected.
+
+# Data exporting was unselected.
+
+# Data exporting was unselected.
+
+# Data exporting was unselected.
+
+# Data exporting was unselected.
+
+# Data exporting was unselected.
+
+# Data exporting was unselected.
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;

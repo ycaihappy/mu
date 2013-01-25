271a005b619daa49a3aa5b04ce3ef8d58307a978
ycaihappy <ycaihappy@163.com>
2013-01-25 17:07:11 +0800

    index phase1

---
+++ code/webroot/themes/classic/views/layouts/main.php
@@ -0,0 +1,374 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>index</title>
+    <link rel="stylesheet" href="css/global.css">
+	<meta http-equiv="X-UA-Compatible" content="IE=Edge,chrome=1">
+	<link rel="shortcut icon" type="image/png" href="/img/favicon.png">
+
+
+</head>
+
+<body>
+
+
+<div id="p_index" class="pg-layout">
+
+<div class="m-top">
+	<div class="layout">
+	<p class="user-info">
+		<a href="">请登陆</a>
+		<a href="">免费注册 </a>
+	</p>
+	<p class="site-tool">
+		<span>|</span>
+		<a href="">找回密码</a>
+		<span>|</span>
+		<a href="">网站地图</a>
+		<span>|</span>
+		<a href="">联系客服</a>
+		<span>|</span>
+		<a href="">收藏本站</a>
+	</p>
+	</div>
+</div>
+
+<div class="layout head">
+	<div class="m-logo">
+		<a target="_self" href="" class="logo"><img title="xxx.com - xxxx" alt="zzz" src="images/logo.jpg"></a>
+	</div>
+	<div class="m-search">
+		<div class="search-triggers">
+			<ul class="switchable-nav">
+			  <li class="selected"><a href="">现货</a></li>
+			  <li><a href="">新闻</a></li>
+			  <li><a href="">行情</a></li>
+			  <li><a href="">行情</a></li>
+			  <li><a href="">求购</a></li>
+			</ul>
+		 </div>
+		 <div class="search-box">
+			<form id="J_SearchForm" name="search" action="" target="_top">
+			  <div class="search-panel-fields">       
+				<input autocomplete="off" autofocus="true" accesskey="s" name="q" id="q">
+				<s></s>
+			  </div>
+			  <button type="submit">搜 索</button>			 
+			 </form>
+		 </div>
+		<div class="clearfix"></div>
+	</div>
+	<div class="m-nav">
+		<div class="hd"></div>
+		<div class="bd">
+			<div class="nav-con divide">
+            <strong><a href="h">新闻</a></strong><a href="">军事</a><a href="">军事</a><a href="">军事</a><br />
+			 <strong><a href="h">新闻</a></strong><a href="">军事</a><a href="">军事</a><a href="">军事</a>
+			</div>
+			<div class="nav-con divide">
+            <strong><a href="h">新闻</a></strong><a href="">军事</a><a href="">军事</a><a href="">军事</a><br />
+			 <strong><a href="h">新闻</a></strong><a href="">军事</a><a href="">军事</a><a href="">军事</a>
+			</div>
+			<div class="nav-con divide">
+            <strong><a href="h">新闻</a></strong><a href="">军事</a><a href="">军事</a><a href="">军事</a><br />
+			 <strong><a href="h">新闻</a></strong><a href="">军事</a><a href="">军事</a><a href="">军事</a>
+			</div>
+			<div class="nav-con divide">
+            <strong><a href="h">新闻</a></strong><a href="">军事</a><a href="">军事</a><a href="">军事</a><br />
+			 <strong><a href="h">新闻</a></strong><a href="">军事</a><a href="">军事</a><a href="">军事</a>
+			</div>
+			<div class="nav-con divide lastdiv">
+            <strong><a href="h">新闻</a></strong><a href="">军事</a><a href="">军事</a><a href="">军事</a><br />
+			 <strong><a href="h">新闻</a></strong><a href="">军事</a><a href="">军事</a><a href="">军事</a>
+			</div>
+		</div>
+	</div>
+
+</div>
+
+<div class="layout main">
+
+	<div class="layout-area">
+		<div class="m-index-ad">
+			<div class="pics"><img src="images/ad.jpg" /></div>
+			<div class="btns"><a href="javascript:void(0)"></a><a href="javascript:void(0)"></a><a href="javascript:void(0)"></a><a href="javascript:void(0)" class="selected"></a></div>
+		</div>
+		<div class="m-tab-list middle">
+			
+			<div class="hd">
+				<span class="on"><a href="">新闻</a></span>
+				<span><a href="">资讯</a></span>
+				<a href="" class="more">更多</a>
+			</div>
+			<div class="bd">
+				<ul class="current">
+					<li class="b"><a href="">毛新宇谈毛泽东激动:国共抗日本质不同</a></li>
+					<li><a href="" target="_blank">沈阳地震主播淡定沈阳地震主播淡定</a></li>
+					<li><a href="" target="_blank">沈阳地震主播淡定沈阳地震主播淡定</a></li>
+					<li><a href="" target="_blank">沈阳地震主播淡定沈阳地震主播淡定</a></li>
+					<li><a href="" target="_blank">沈阳地震主播淡定沈阳地震主播淡定</a></li>
+					<li><a href="" target="_blank">沈阳地震主播淡定沈阳地震主播淡定</a></li>
+					<li><a href="" target="_blank">沈阳地震主播淡定沈阳地震主播淡定</a></li>
+					<li><a href="" target="_blank">沈阳地震主播淡定沈阳地震主播淡定</a></li>
+					<li><a href="" target="_blank">沈阳地震主播淡定沈阳地震主播淡定</a></li>
+					<li><a href="" target="_blank">沈阳地震主播淡定沈阳地震主播淡定</a></li>			
+				</ul>
+				<ul>
+					<li class="b"><a href="">xxxxxxxxxxxxxxxxxxxx</a></li>
+					<li><a href="" target="_blank">女子裸身泡温泉被偷窥女子裸身泡温泉被偷窥</a></li>
+					<li><a href="" target="_blank">女子裸身泡温泉被偷窥女子裸身泡温泉被偷窥</a></li>
+					<li><a href="" target="_blank">沈阳地震主播淡定沈阳地震主播淡定</a></li>
+					<li><a href="" target="_blank">沈阳地震主播淡定沈阳地震主播淡定</a></li>
+					<li><a href="" target="_blank">沈阳地震主播淡定沈阳地震主播淡定</a></li>
+					<li><a href="" target="_blank">沈阳地震主播淡定沈阳地震主播淡定</a></li>
+					<li><a href="" target="_blank">沈阳地震主播淡定沈阳地震主播淡定</a></li>
+					<li><a href="" target="_blank">沈阳地震主播淡定沈阳地震主播淡定</a></li>
+					<li><a href="" target="_blank">沈阳地震主播淡定沈阳地震主播淡定</a></li>
+			
+				</ul>
+			</div>
+		
+		</div>
+		<div class="m-qk-entry right">
+			<div class="hd">
+				<span><a href="">免费注册</a></span>
+				<span><a href="">登陆</a></span>
+				<span><a href="">现货专区</a></span>
+			</div>
+			<div class="bd">
+				<ul>
+					<li><a><i class="qk-1"></i>发布现货</a><s></s><a><i class="qk-2"></i>发布求购</a><div class="qk-b"></div></li>
+					<li><a><i class="qk-3"></i>企业商铺</a><s></s><a><i class="qk-4"></i>发布特价</a><div class="qk-b"></div></li>
+					<li class="last"><a><i class="qk-5"></i>我要找货</a><s></s><a><i class="qk-6"></i>会员服务</a></li>
+				</ul>
+			</div>
+			<div class="ft">
+				<a><img src="images/ad_1.jpg" /></a>
+			</div>
+			
+		</div>
+		<div class="clearfix"></div>
+	</div>
+	<div class="layout-area">
+		<div class="m-tab-list left">
+			<div class="hd">
+				<span class="on"><a href="">新闻</a></span>			
+				<a href="" class="more">更多</a>
+			</div>
+			<div class="bd">
+				<ul class="current">
+					<li class="b"><a href="">毛新宇谈毛泽东激动:国共抗日本质不同</a></li>
+					<li><a href="" target="_blank">沈阳地震主播淡定沈阳地震主播淡定</a></li>
+					<li><a href="" target="_blank">沈阳地震主播淡定沈阳地震主播淡定</a></li>
+					<li><a href="" target="_blank">沈阳地震主播淡定沈阳地震主播淡定</a></li>
+					<li><a href="" target="_blank">沈阳地震主播淡定沈阳地震主播淡定</a></li>
+					<li><a href="" target="_blank">沈阳地震主播淡定沈阳地震主播淡定</a></li>
+					<li><a href="" target="_blank">沈阳地震主播淡定沈阳地震主播淡定</a></li>
+					<li><a href="" target="_blank">沈阳地震主播淡定沈阳地震主播淡定</a></li>
+					<li><a href="" target="_blank">沈阳地震主播淡定沈阳地震主播淡定</a></li>
+					<li><a href="" target="_blank">沈阳地震主播淡定沈阳地震主播淡定</a></li>			
+				</ul>
+				
+			</div>
+		</div>
+		<div class="m-tab-list middle">
+			<div class="hd">
+				<span class="on"><a href="">新闻</a></span>			
+				<a href="" class="more">更多</a>
+			</div>
+			<div class="bd">
+				<ul class="current">
+					<li class="b"><a href="">毛新宇谈毛泽东激动:国共抗日本质不同</a></li>
+					<li><a href="" target="_blank">沈阳地震主播淡定沈阳地震主播淡定</a></li>
+					<li><a href="" target="_blank">沈阳地震主播淡定沈阳地震主播淡定</a></li>
+					<li><a href="" target="_blank">沈阳地震主播淡定沈阳地震主播淡定</a></li>
+					<li><a href="" target="_blank">沈阳地震主播淡定沈阳地震主播淡定</a></li>
+					<li><a href="" target="_blank">沈阳地震主播淡定沈阳地震主播淡定</a></li>
+					<li><a href="" target="_blank">沈阳地震主播淡定沈阳地震主播淡定</a></li>
+					<li><a href="" target="_blank">沈阳地震主播淡定沈阳地震主播淡定</a></li>
+					<li><a href="" target="_blank">沈阳地震主播淡定沈阳地震主播淡定</a></li>
+					<li><a href="" target="_blank">沈阳地震主播淡定沈阳地震主播淡定</a></li>			
+				</ul>
+				
+			</div>
+		</div>
+		<div class="m-tab-list right">
+			<div class="hd">
+				<span class="on"><a href="">新闻</a></span>			
+				<a href="" class="more">更多</a>
+			</div>
+			<div class="bd">
+				<ul class="current">
+					<li class="b"><a href="">毛新宇谈毛泽东激动:国共抗日本质不同</a></li>
+					<li><a href="" target="_blank">沈阳地震主播淡定沈阳地震主播淡定</a></li>
+					<li><a href="" target="_blank">沈阳地震主播淡定沈阳地震主播淡定</a></li>
+					<li><a href="" target="_blank">沈阳地震主播淡定沈阳地震主播淡定</a></li>
+					<li><a href="" target="_blank">沈阳地震主播淡定沈阳地震主播淡定</a></li>
+					<li><a href="" target="_blank">沈阳地震主播淡定沈阳地震主播淡定</a></li>
+					<li><a href="" target="_blank">沈阳地震主播淡定沈阳地震主播淡定</a></li>
+					<li><a href="" target="_blank">沈阳地震主播淡定沈阳地震主播淡定</a></li>
+					<li><a href="" target="_blank">沈阳地震主播淡定沈阳地震主播淡定</a></li>
+					<li><a href="" target="_blank">沈阳地震主播淡定沈阳地震主播淡定</a></li>			
+				</ul>
+				
+			</div>
+		</div>
+		<div class="clearfix"></div>
+	</div>
+	<div class="layout-area">
+		<div class="m-banner">
+			<img src="images/banner.jpg" width="960" height="100" />
+		</div>
+		<div class="col-left">
+			<div class="m-rcm left">
+				<div class="hd">
+					<span><a href="">推荐企业</a></span>
+					<a class="more">更多</a>
+					<div class="clearfix"></div>
+				</div>
+				<div class="bd">
+					<div class="scroll">
+					<ul>
+						<li>
+							<div class="img"><img src="images/img1.jpg" width="93" height="68" /></div>							
+							<div class="info">
+								<a>昆明汗信经贸有限公司</a>
+								<p>1分钟前更新 <span class="red">112条</span></p>
+								<p>主营：铝</p>
+							</div>
+						</li>
+						<li>
+							<div class="img"><img src="images/img1.jpg" width="93" height="68" /></div>							
+							<div class="info">
+								<a>昆明汗信经贸有限公司</a>
+								<p>1分钟前更新 <span class="red">112条</span></p>
+								<p>主营：铝</p>
+							</div>
+						</li>
+						<li class="last">
+							<div class="img"><img src="images/img1.jpg" width="93" height="68" /></div>							
+							<div class="info">
+								<a>昆明汗信经贸有限公司</a>
+								<p>1分钟前更新 <span class="red">112条</span></p>
+								<p>主营：铝</p>
+							</div>
+						</li>
+					</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="col-right">		
+			<div class="m-quot">
+				<div class="hd">
+				<span class="on"><a href="">价格行情</a></span>			
+				<a href="" class="more">更多</a>
+				</div>
+				<div class="bd">
+					<div class="col-l">
+					<p>
+						<select>
+							<option>交易所</option>
+						</select>
+						<select>
+							<option>沪铝</option>
+						</select>
+						<select>
+							<option>沪铝连续</option>
+						</select>
+					</p>
+					<div class="chart">
+						<img src="images/b.png" />
+					</div>
+					</div>
+					<div class="col-r">
+						<ul class="current">
+							<li class="b"><a href="">毛新宇谈毛泽东激动:国共抗日本质不同</a></li>
+							<li><a href="" target="_blank">沈阳地震主播淡定沈阳地震主播淡定</a></li>
+							<li><a href="" target="_blank">沈阳地震主播淡定沈阳地震主播淡定</a></li>
+							<li><a href="" target="_blank">沈阳地震主播淡定沈阳地震主播淡定</a></li>
+							<li><a href="" target="_blank">沈阳地震主播淡定沈阳地震主播淡定</a></li>
+							<li><a href="" target="_blank">沈阳地震主播淡定沈阳地震主播淡定</a></li>
+							<li><a href="" target="_blank">沈阳地震主播淡定沈阳地震主播淡定</a></li>
+							<li><a href="" target="_blank">沈阳地震主播淡定沈阳地震主播淡定</a></li>
+								
+				</ul>
+					</div>
+				</div>
+			</div>
+			<div class="m-case">
+				<div class="hd">
+				<span class="on"><a href="">成功案例</a></span>			
+				<a href="" class="more">更多</a>
+				</div>
+				<div class="bd">
+					<ul>
+						<li><a>沈阳地震主播淡定沈阳地震主播淡定</a></li>
+						<li><a>沈阳地震主播淡定沈阳地震主播淡定</a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+		<div class="clearfix"></div>
+	</div>
+	<div class="layout-area">
+		<div class="m-brand">
+			<a><img src="images/img2.jpg" /></a>
+			<a><img src="images/img2.jpg" /></a>
+			<a><img src="images/img2.jpg" /></a>
+			<a><img src="images/img2.jpg" /></a>
+			<a><img src="images/img2.jpg" /></a>
+			<a><img src="images/img2.jpg" /></a>
+			<a><img src="images/img2.jpg" /></a>
+			<a><img src="images/img2.jpg" /></a>
+			<a><img src="images/img2.jpg" /></a>
+			<a><img src="images/img2.jpg" /></a>
+			<a><img src="images/img2.jpg" /></a>
+			<a><img src="images/img2.jpg" /></a>
+		</div>
+	</div>
+	
+	<div class="layout-area">
+		<div class="m-link">
+			<div class="hd">
+				<span>友情链接</span>
+				<p>欢迎电子工业欢迎电子工业欢迎电子工业<em>(加QQ时请通过)</em></p>
+				<div class="clearfix"></div>
+				<div class="link-list">
+					<a>钢X网</a><a>钢X网</a><a>钢X网</a><a>钢X网</a><a>钢X网</a><a>钢X网</a><a>钢X网</a><a>钢X网</a><a>钢X网</a><a>钢X网</a><a>钢X网</a><a>钢X网</a><a>钢X网</a><a>钢X网</a><a>钢X网</a><a>钢X网</a><a>钢X网</a><a>钢X网</a><a>钢X网</a><a>钢X网</a><a>钢X网</a><a>钢X网</a><a>钢X网</a><a>钢X网</a><a>钢X网</a><a>钢X网</a><a>钢X网</a><a>钢X网</a>
+				</div>
+			</div>
+		</div>
+	</div>
+	
+	<div class="layout-area">
+		<div class="m-footer">
+		<p class="first"><a>公司简介</a> | <a>广告服务</a> | <a>联系方式</a> | <a>服务项目</a> | <a>公司动态</a> | <a>工作机会</a> | <a>网站地图</a> | <a>公司简介</a> | <a>公司简介</a> | <a>公司简介</a></p>
+		<p>客服热线：400-0000-0000  0766-1111111 / 11111111 / 1111111 传真：0443-11111111</p>
+		<p>Copyright &copy; 1998 - 2012 中国XX网 All Rights Reserved</p>
+		<p>全国咨询/投诉电话：400-000-0000   E-mail:kf@xxxx.com ICP证1003232</p>
+		
+		<p class="fa"> <a class="fa1" target="_blank" href="http://www.cqc.com.cn"></a> <a class="fa2" target="_blank" href="http://www.smestar.com/detail.credit?action=preLevel&amp;credcode=NO.20000001143"></a> <a class="fa3" target="_blank" href="http://www.e-315.org"></a> <a class="fa4" target="_blank" href="http://www.isc.org.cn"></a> <a class="fa5" target="_blank" href="http://www.ec.org.cn"></a> <a class="fa6" target="_blank" href="https://www.itrust.org.cn/yz/pjwx.asp?wm=1697657781"></a> 
+	</p>
+		
+		</div>
+	</div>
+	
+
+</div>
+
+
+
+
+</div>
+
+
+
+
+<script src="js/jquery.1.8.min.js"></script>
+<script src="js/config.js"></script>
+<script src="js/global.js"></script>
+<script src="js/init.js"></script>
+</body>
+</html>

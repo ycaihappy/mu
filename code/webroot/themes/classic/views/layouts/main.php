6a4ec48d7bba316d26da04e488a74556b620bf51
ycaihappy <ycaihappy@163.com>
2013-01-26 21:01:58 +0800

    widget sample

--- code/webroot/themes/classic/views/layouts/main.php
+++ code/webroot/themes/classic/views/layouts/main.php
@@ -38,6 +38,7 @@
 	<div class="m-logo">
 		<a target="_self" href="" class="logo"><img title="xxx.com - xxxx" alt="zzz" src="images/logo.jpg"></a>
 	</div>
+
 	<div class="m-search">
 		<div class="search-triggers">
 			<ul class="switchable-nav">
@@ -90,10 +91,7 @@
 <div class="layout main">
 
 	<div class="layout-area">
-		<div class="m-index-ad">
-			<div class="pics"><img src="images/ad.jpg" /></div>
-			<div class="btns"><a href="javascript:void(0)"></a><a href="javascript:void(0)"></a><a href="javascript:void(0)"></a><a href="javascript:void(0)" class="selected"></a></div>
-		</div>
+        <?php $this->widget("BannerWidget");?>
 		<div class="m-tab-list middle">
 			
 			<div class="hd">

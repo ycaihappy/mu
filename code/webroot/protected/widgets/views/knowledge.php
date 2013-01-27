37908170293ff7a721975afc6c8a8f9dfe7bfbb8
ycaihappy <ycaihappy@163.com>
2013-01-27 18:07:52 +0800

    new module for mu knowledge

---
+++ code/webroot/protected/widgets/views/knowledge.php
@@ -0,0 +1,18 @@
+			<div class="m-nous right">
+				<div class="hd">
+				<span class="on"><a href="">钼常识</a></span>			
+				<a href="" class="more">更多</a>
+				</div>
+				<div class="bd">
+					<ul>
+						<li><a>钼常识钼常识钼常识钼常识钼常识</a></li>
+						<li><a>钼常识钼常识钼常识钼常识钼常识</a></li>
+						<li><a>沈阳地震主播淡定沈阳地震主播淡定</a></li>
+						<li><a>沈阳地震主播淡定沈阳地震主播淡定</a></li>
+						<li><a>沈阳地震主播淡定沈阳地震主播淡定</a></li>
+						<li><a>沈阳地震主播淡定沈阳地震主播淡定</a></li>
+						<li><a>沈阳地震主播淡定沈阳地震主播淡定</a></li>
+						
+					</ul>
+				</div>
+			</div>

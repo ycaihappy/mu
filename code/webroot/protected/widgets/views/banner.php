77c9bd0b79617ef5855407e9d22bd6a5118a8de3
ycaihappy <ycaihappy@163.com>
2013-01-28 02:27:12 +0800

    new pic

--- code/webroot/protected/widgets/views/banner.php
+++ code/webroot/protected/widgets/views/banner.php
@@ -1,9 +1,9 @@
 		<div class="m-index-ad" id="J_IndexAd">
 			<div class="pic">
-				<a href=""><img src="images/ad.jpg" /></a>
-				<a href=""><img src="images/ad.jpg" /></a>
-				<a href=""><img src="images/ad.jpg" /></a>
-				<a href=""><img src="images/ad.jpg" /></a>
+				<a href=""><img src="images/banner01.jpg" /></a>
+				<a href=""><img src="images/banner02.jpg" /></a>
+				<a href=""><img src="images/banner03.jpg" /></a>
+				<a href=""><img src="images/banner04.jpg" /></a>
 				
 			</div>
 			<div class="btns"><a href="javascript:void(0)"></a><a href="javascript:void(0)"></a><a href="javascript:void(0)"></a><a href="javascript:void(0)" class="on"></a></div>

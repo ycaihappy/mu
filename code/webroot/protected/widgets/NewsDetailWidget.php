177bc62009fb92f18b4534efbe0729ba8958cf81
ycaihappy <ycaihappy@163.com>
2013-01-29 03:13:33 +0800

    real data

--- code/webroot/protected/widgets/NewsDetailWidget.php
+++ code/webroot/protected/widgets/NewsDetailWidget.php
@@ -3,11 +3,11 @@ class NewsDetailWidget extends CWidget
 {
     public function init()
     {
-    	
     }
 
     public function run()
     {
-        $this->render('news_detail',array('name'=>'lizhli'));
+        $art_detail = Article::model()->findByPk($_GET['art_id']);
+        $this->render('news_detail',array('art_detail'=>$art_detail));
     }
 }

1f0a557cc519d4266a34c8ced80d31c6b1e50189
ycaihappy <ycaihappy@163.com>
2013-01-28 23:57:22 +0800

    add the layou

---
+++ code/webroot/themes/classic/views/layouts/main.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php $this->widget("CommonHeaderWidget");?>
+
+<body>
+
+<div id="p_index" class="pg-layout">
+<?php $this->widget('TopWidget');?>
+
+    <div class="layout head">
+        <?php $this->widget('SearchWidget');?>
+        <?php $this->widget('NavigationWidget');?>
+    </div>
+    
+    <div class="layout main">
+    
+        <?php echo $content;?>
+    
+    	<div class="layout-area">
+            <?php $this->widget("BottomBannerWidget");?>
+    	</div>
+    	
+    	<div class="layout-area">
+            <?php $this->widget("FriendLinkWidget");?>
+    	</div>
+    	
+    	<div class="layout-area">
+        <?php $this->widget("FooterWidget");?>
+    	</div>
+    
+    </div>
+    
+</div>
+
+<?php $this->widget("CommonFooterWidget");?>
+
+</body>
+</html>

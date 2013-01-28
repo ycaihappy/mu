1adc4b8c96f39e507ccdf8ce7058bb7632fc4482
ycaihappy <ycaihappy@163.com>
2013-01-29 01:37:18 +0800

    product detail widget

---
+++ code/webroot/protected/widgets/views/product_detail.php
@@ -0,0 +1,51 @@
+	<div class="m-detail">
+    <div class="sellinfo clearfix">
+        <div class="fl">
+            <div class="img">
+                <span><img alt="/￥%&mdash;【16mn方钢&mdash;&mdash;【百华】&mdash;&mdash;65mn方钢-20#方钢" style="width:140px;height:114px;margin-top:93px;margin-left:80px" src="http://img1.bmlink.com/big/default/2012/2/25/0/0673446879104454.jpg"></span>
+             
+            </div>
+
+        </div>
+        <div class="fr">
+            <a title="/￥%&mdash;【16mn方钢&mdash;&mdash;【百华】&mdash;&mdash;65mn方钢-20#方钢" href="/tjbhgg/supply-3983588.html"><h1>/￥%&mdash;【16mn方钢&mdash;&mdash;【百华】&mdash;&mdash;65mn方钢-20#方钢</h1></a>
+            
+	      <p>产品价格：<span class="orange">电议</span></p>
+          <p>最小起订：<span>10</span>吨</p>
+	      <p>供货总量：<span></span>吨</p>
+	      <p>发&nbsp;货&nbsp;期：3天</p>
+          
+            <p>
+                所&nbsp;在&nbsp;地：天津 天津</p>
+            <p>
+                有效期至：<span>2012-06-04</span></p>
+ 
+            <p class="btn">
+                <a title="立即询价" class="btn_lx" href="javascript:;">立即询价</a></p>
+            <div class="vipinfo">
+                <ul>
+                    <li class="btn"><a title="查看联系电话" class="btn_phone" href="#">查看联系电话</a>
+                        <ul style="display: none;" class="allinfo">
+                            <li>联系人：<span>安广虎</span>()</li>
+                            <li><a title="加为商友" class="btn_xp" href="javascript:addFriend(517145)">加为商友</a><a title="在线QQ" class="qqonline" href="http://wpa.qq.com/msgrd?V=1&amp;Site=中国建材网&amp;Uin=2510906890&amp;Menu=yes"><img align="absmiddle" src="http://meta.bmlink.com/2012/images/trade/icon_qq.jpg"></a></li>
+                            <li>联系电话：<span class="orange">13502153395&nbsp;&nbsp;86-022-60909034</span></li>
+                            <li>拨打电话时您可以说：您好，我在中国建材网看到您发布的……</li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+<p class="btn"><a title="加入收藏" href="javascript:addCollect(3983588,1,1,406856,'%2f%ef%bf%a5%25%e2%80%94%e3%80%9016mn%e6%96%b9%e9%92%a2%e2%80%94%e2%80%94%e3%80%90%e7%99%be%e5%8d%8e%e3%80%91%e2%80%94%e2%80%9465mn%e6%96%b9%e9%92%a2-20%23%e6%96%b9%e9%92%a2');">加入收藏</a></p>
+ 
+        </div>
+    </div>
+
+    <div class="title clearfix">
+        <h2>方钢-20#方钢详细描述</h2>
+    </div>
+
+	<div class="proInfo"><ul><li>产品规格:20# </li><li>形状:方钢 </li><li>仓库:天津 </li><li>制作工艺:冷轧 </li><li>产地/厂家:唐钢 </li><li>材质:Q235B </li></ul></div>
+    <div class="info">
+		
+		<p>内容内容内容内容内容</p>
+		
+	</div>

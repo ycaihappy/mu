a5f5b9689e852736b38d5b2c2a6e85366e1b912b
ycaihappy <ycaihappy@163.com>
2013-01-29 04:08:26 +0800

    build data

--- code/webroot/protected/config/console.php
+++ code/webroot/protected/config/console.php
@@ -11,19 +11,20 @@ return array(
 
 	// application components
 	'components'=>array(
-		'db'=>array(
-			'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
-		),
+	#	'db'=>array(
+	#		'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
+	#	),
 		// uncomment the following to use a MySQL database
-		/*
+
 		'db'=>array(
-			'connectionString' => 'mysql:host=localhost;dbname=testdrive',
+			'connectionString' => 'mysql:host=localhost;dbname=mu',
 			'emulatePrepare' => true,
 			'username' => 'root',
 			'password' => '',
 			'charset' => 'utf8',
+            'tablePrefix'=> 'mu'
 		),
-		*/
+
 		'log'=>array(
 			'class'=>'CLogRouter',
 			'routes'=>array(

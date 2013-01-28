163fb479dc19a507cd8f1f74478269b13eb4b06a
ycaihappy <ycaihappy@163.com>
2013-01-29 06:16:33 +0800

    dd

---
+++ code/webroot/protected/widgets/views/supply_detail.php
@@ -0,0 +1,59 @@
+		<div class="m-supplier-detail" id="contentInfo">
+            <div class="msg_time">
+                <span></span>发布于 2013/1/28 11:41:01
+            </div>
+            <div id="com_info" class="cmp_info">
+            	<ul>
+                	<li>
+                    	<span>发布公司</span>
+                        <em>邯郸市西丁贸易有限公司</em>
+                   	</li>
+                    <li>
+                    	<span>联系人</span>
+                        <em>张经理</em>
+                        <a href="http://i.steelcn.com/member/contacto/contactoAdd.aspx?user=xidingmaoyi" class="ci_mail" target="_blank">站内信</a>
+                    </li>
+                    <li class="ci_li01">
+                    	<span>品种</span>
+                        供应板材类/普碳中板
+                    </li>
+                    <li class="ci_li">
+                    	<span>规格</span>
+                        70
+                    </li>
+                    <li class="ci_li">
+                    	<span>材质</span>
+                        Q235B
+                    </li>
+                    <li class="ci_li">
+                    	<span>宽</span>
+                        2200
+                    </li>
+                    <li class="ci_li">
+                    	<span>长</span>
+                        10800
+                    </li>
+                    <li class="ci_li">
+                    	<span>交货地</span>
+                        邯郸市
+                    </li>
+                    <li class="ci_li">
+                    	<span>数量</span>
+                        14吨
+                    </li>
+                    <li class="ci_li">
+                    	<span>联系电话</span>
+                        <em>0310-7878266</em>
+                    </li>
+                    <li class="ci_li">
+                    	<span>价格</span>
+                        3800
+                    </li>
+                </ul>
+            </div>
+           
+            <div class="am_note">
+                现货
+            </div>
+            <p class="art_note">·联系我时，请注明您是在中国钢材网上看到了我的采购信息</p>
+      </div>

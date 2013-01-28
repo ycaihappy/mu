9da2a3e5ad255a2152c4f728cec42062cab325d3
ycaihappy <ycaihappy@163.com>
2013-01-29 01:48:22 +0800

    product controller

--- code/webroot/protected/controllers/ProductController.php
+++ code/webroot/protected/controllers/ProductController.php
@@ -8,16 +8,6 @@ class ProductController extends Controller
 	public function actions()
 	{
 		return array(
-			// captcha action renders the CAPTCHA image displayed on the contact page
-			'captcha'=>array(
-				'class'=>'CCaptchaAction',
-				'backColor'=>0xFFFFFF,
-			),
-			// page action renders "static" pages stored under 'protected/views/site/pages'
-			// They can be accessed via: index.php?r=site/page&view=FileName
-			'page'=>array(
-				'class'=>'CViewAction',
-			),
 		);
 	}
 
@@ -30,6 +20,13 @@ class ProductController extends Controller
 		// renders the view file 'protected/views/site/index.php'
 		// using the default layout 'protected/views/layouts/main.php'
 		$this->render('index');
+	}
+
+	public function actionView()
+	{
+		// renders the view file 'protected/views/site/index.php'
+		// using the default layout 'protected/views/layouts/main.php'
+		$this->render('view');
 	}
 
 	/**

082ef60261c4b532c95855447dc13a2737c5c2c7
ycaihappy <ycaihappy@163.com>
2013-01-29 06:00:24 +0800

    add link

--- code/webroot/protected/widgets/views/product_top.php
+++ code/webroot/protected/widgets/views/product_top.php
@@ -14,7 +14,6 @@ $title = '现货';
 			
 			<div class="hd">
             <span class="on"><a href=""><?php echo $title;?></a></span>
-				<!--<span><a href="">资讯</a></span> -->
 				<a href="" class="more">更多</a>
 			</div>
 			<div class="bd">
@@ -22,24 +21,11 @@ $title = '现货';
 			<?php for($index=0;$index<count($data);$index++):
 							if($index==0):
 						?>
-							<li class="b"><a href=""><?php echo $data[$index]['product_name']; ?></a></li>
+                            <li class="b"><a href="<?php echo "index.php?r=product/view&product_id=".$data[$index]['product_id']; ?>"><?php echo $data[$index]['product_name']; ?></a></li>
 							<?php else :?>
-							<li><a href="" target="_blank"><?php echo $data[$index]['product_name']; ?></a></li>
+                            <li><a href="<?php echo "index.php?r=product/view&product_id=".$data[$index]['product_id']; ?>" target="_blank"><?php echo $data[$index]['product_name']; ?></a></li>
 							<?php endif;?>
 					<?php endfor;?>			
-				</ul>
-				<ul>
-					<li class="b"><a href="">xxxxxxxxxxxxxxxxxxxx</a></li>
-					<li><a href="" target="_blank">女子裸身泡温泉被偷窥女子裸身泡温泉被偷窥</a></li>
-					<li><a href="" target="_blank">女子裸身泡温泉被偷窥女子裸身泡温泉被偷窥</a></li>
-					<li><a href="" target="_blank">沈阳地震主播淡定沈阳地震主播淡定</a></li>
-					<li><a href="" target="_blank">沈阳地震主播淡定沈阳地震主播淡定</a></li>
-					<li><a href="" target="_blank">沈阳地震主播淡定沈阳地震主播淡定</a></li>
-					<li><a href="" target="_blank">沈阳地震主播淡定沈阳地震主播淡定</a></li>
-					<li><a href="" target="_blank">沈阳地震主播淡定沈阳地震主播淡定</a></li>
-					<li><a href="" target="_blank">沈阳地震主播淡定沈阳地震主播淡定</a></li>
-					<li><a href="" target="_blank">沈阳地震主播淡定沈阳地震主播淡定</a></li>
-			
 				</ul>
 			</div>
 		

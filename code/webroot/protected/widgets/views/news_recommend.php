7a48725356abc8123328bb3cb2be2b546e0ba37e
ycaihappy <ycaihappy@163.com>
2013-01-29 01:29:16 +0800

    news views

---
+++ code/webroot/protected/widgets/views/news_recommend.php
@@ -0,0 +1,18 @@
+		<div class="m-rcm-list">
+			
+			<dl class="last">
+			  <dt><strong>推荐报道</strong></dt>
+			  <dd>				
+				<ul>
+					<li><a title="中国建材网" href="http://www.bmlink.com">美国钢材市场价格盘整运行</a></li>
+					<li><a title="中国建材网" href="http://www.bmlink.com">美国钢材市场价格盘整运行</a></li>
+					<li><a title="中国建材网" href="http://www.bmlink.com">美国钢材市场价格盘整运行</a></li>
+					<li><a title="中国建材网" href="http://www.bmlink.com">美国钢材市场价格盘整运行</a></li>
+					<li><a title="中国建材网" href="http://www.bmlink.com">美国钢材市场价格盘整运行</a></li>
+					<li><a title="中国建材网" href="http://www.bmlink.com">美国钢材市场价格盘整运行</a></li>
+					<li><a title="中国建材网" href="http://www.bmlink.com">美国钢材市场价格盘整运行</a></li>
+
+				</ul>
+			  </dd>
+			</dl>
+		</div>

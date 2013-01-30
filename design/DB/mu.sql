75113ab8b052ad788b480a439ce279456da31c5a
xiaofuqian <xiao2008abc@163.com>
2013-01-30 08:50:05 +0800

    修改部分数据表结构

--- design/DB/mu.sql
+++ design/DB/mu.sql
@@ -3,7 +3,7 @@
 # Server version:               5.1.28-rc-community
 # Server OS:                    Win32
 # HeidiSQL version:             6.0.0.3603
-# Date/time:                    2013-01-27 15:02:03
+# Date/time:                    2013-01-30 08:49:21
 # --------------------------------------------------------
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -75,20 +75,24 @@ DROP TABLE IF EXISTS `mu_city`;
 CREATE TABLE IF NOT EXISTS `mu_city` (
   `city_id` int(11) NOT NULL AUTO_INCREMENT,
   `city_name` varchar(128) NOT NULL,
-  `city_parent` int(11) NOT NULL,
+  `city_parent` int(11) NOT NULL DEFAULT '0',
   `city_level` tinyint(4) DEFAULT NULL,
-  `city_order` tinyint(4) DEFAULT NULL,
-  `city_open` varchar(45) DEFAULT NULL,
+  `city_order` tinyint(4) DEFAULT '0',
+  `city_open` tinyint(4) DEFAULT '1',
   PRIMARY KEY (`city_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COMMENT='省市联动表';
+) ENGINE=MyISAM AUTO_INCREMENT=9 DEFAULT CHARSET=utf8 COMMENT='省市联动表';
 
-# Dumping data for table mu.mu_city: 4 rows
+# Dumping data for table mu.mu_city: 8 rows
 /*!40000 ALTER TABLE `mu_city` DISABLE KEYS */;
 INSERT INTO `mu_city` (`city_id`, `city_name`, `city_parent`, `city_level`, `city_order`, `city_open`) VALUES
-	(1, '西南', 0, 1, 1, NULL),
-	(2, '重庆', 1, 2, 1, NULL),
-	(3, '大足', 2, 3, 1, NULL),
-	(4, '渝中区', 2, 3, 2, NULL);
+	(1, '西南', 0, 1, 1, 1),
+	(2, '重庆', 1, 2, 1, 1),
+	(3, '大足', 2, 3, 1, 1),
+	(4, '渝中区', 2, 3, 2, 1),
+	(5, '大渡口区', 2, NULL, 2, 1),
+	(6, '华北', 0, NULL, 0, 1),
+	(7, '河南', 6, NULL, 0, 1),
+	(8, '平顶山', 7, NULL, 0, 1);
 /*!40000 ALTER TABLE `mu_city` ENABLE KEYS */;
 
 
@@ -172,28 +176,33 @@ CREATE TABLE IF NOT EXISTS `mu_point_rule` (
 # Dumping structure for table mu.mu_product
 DROP TABLE IF EXISTS `mu_product`;
 CREATE TABLE IF NOT EXISTS `mu_product` (
-  `product_id` bigint(20) NOT NULL,
+  `product_id` bigint(20) NOT NULL AUTO_INCREMENT,
   `product_user_id` int(11) NOT NULL COMMENT '用户ID',
   `product_keyword` varchar(128) DEFAULT NULL COMMENT '用户ID',
   `product_name` varchar(45) NOT NULL,
   `product_quanity` int(11) DEFAULT NULL COMMENT '数量',
-  `product_unit` tinyint(11) DEFAULT NULL COMMENT '数量单位:吨,公斤等',
-  `product_type_id` tinyint(4) DEFAULT NULL COMMENT '产品分类',
+  `product_unit` int(11) DEFAULT NULL COMMENT '数量单位:吨,公斤等',
+  `product_type_id` int(4) DEFAULT NULL COMMENT '产品分类',
   `product_price` decimal(10,0) DEFAULT NULL COMMENT '0表示电议',
-  `product_status` tinyint(4) DEFAULT NULL COMMENT '在售',
-  `product_city_id` tinyint(4) DEFAULT NULL COMMENT '在售',
+  `product_status` int(4) DEFAULT NULL COMMENT '在售',
+  `product_city_id` int(4) DEFAULT NULL COMMENT '在售',
+  `product_content` text COMMENT '在售',
   `product_location` varchar(100) DEFAULT NULL COMMENT '''存货地''',
-  `product_special` tinyint(1) DEFAULT NULL COMMENT '是否特价',
+  `product_special` int(1) DEFAULT NULL COMMENT '是否特价',
   `product_join_date` datetime DEFAULT NULL,
   `product_image_src` varchar(128) DEFAULT NULL,
+  `product_check_by` varchar(128) DEFAULT NULL,
   PRIMARY KEY (`product_id`),
   KEY `product_user_id` (`product_user_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COMMENT='现货';
+) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COMMENT='现货';
 
-# Dumping data for table mu.mu_product: 1 rows
+# Dumping data for table mu.mu_product: 4 rows
 /*!40000 ALTER TABLE `mu_product` DISABLE KEYS */;
-INSERT INTO `mu_product` (`product_id`, `product_user_id`, `product_keyword`, `product_name`, `product_quanity`, `product_unit`, `product_type_id`, `product_price`, `product_status`, `product_city_id`, `product_location`, `product_special`, `product_join_date`, `product_image_src`) VALUES
-	(1, 1, NULL, '特价钼铁', 2000, 9, 1, 23435, 1, NULL, '1', 0, '2013-01-21 22:05:09', NULL);
+INSERT INTO `mu_product` (`product_id`, `product_user_id`, `product_keyword`, `product_name`, `product_quanity`, `product_unit`, `product_type_id`, `product_price`, `product_status`, `product_city_id`, `product_content`, `product_location`, `product_special`, `product_join_date`, `product_image_src`, `product_check_by`) VALUES
+	(1, 1, 'test', '特价钼铁1111', 2000, 3, 23, 23435, 1, 8, 'sdfsdf', '1', 1, '2013-01-21 22:05:09', NULL, NULL),
+	(2, 1, 'test', '特价氧化钼', 2000, 3, 22, 23232, 1, 8, 'sdfsdf', '1', 1, '2013-01-21 22:05:09', NULL, NULL),
+	(3, 1, 'test', '现货氧化钼', 2000, 3, 23, 23232, 1, 8, 'dsfsdfsdf', '1', 0, '2013-01-21 22:05:09', NULL, NULL),
+	(4, 1, 'tes6t', '现货钼铁', 2000, 3, 22, 23435, 2, 8, 'sdfsdfsdfdsf', '1', 0, '2013-01-21 22:05:09', NULL, NULL);
 /*!40000 ALTER TABLE `mu_product` ENABLE KEYS */;
 
 
@@ -350,28 +359,33 @@ CREATE TABLE IF NOT EXISTS `mu_success_case` (
 DROP TABLE IF EXISTS `mu_supply`;
 CREATE TABLE IF NOT EXISTS `mu_supply` (
   `supply_id` int(11) NOT NULL AUTO_INCREMENT,
+  `supply_name` varchar(218) NOT NULL,
   `supply_user_id` int(11) NOT NULL,
   `supply_type` int(11) NOT NULL COMMENT '供求类型',
   `supply_keyword` varchar(128) DEFAULT NULL COMMENT '供求类型',
-  `supply_category_id` tinyint(4) NOT NULL COMMENT '供应产品的品类',
+  `supply_category_id` int(4) NOT NULL COMMENT '供应产品的品类',
   `supply_contractor` varchar(32) DEFAULT NULL COMMENT '''发布者''',
-  `supply_content` varchar(128) DEFAULT NULL COMMENT '''发布内容''',
+  `supply_content` text COMMENT '''发布内容''',
   `supply_address` varchar(100) DEFAULT NULL COMMENT '供货地址',
-  `supply_city_id` tinyint(4) DEFAULT NULL COMMENT '供货地址',
+  `supply_city_id` int(4) DEFAULT NULL COMMENT '供货地址',
   `supply_status` int(11) DEFAULT NULL COMMENT '起草 待审核 已发布',
   `supply_phone` char(32) DEFAULT NULL COMMENT '''联系电话''',
+  `supply_unit` int(11) DEFAULT NULL COMMENT '''联系电话''',
   `supply_price` decimal(8,0) DEFAULT NULL COMMENT '价钱',
   `supply_valid_date` datetime DEFAULT NULL COMMENT '''0表示长期有效''',
-  `supply_check_by` int(11) DEFAULT NULL COMMENT '审核人',
-  `supply_recommend` tinyint(4) DEFAULT NULL COMMENT '是否推荐',
+  `supply_check_by` varchar(128) DEFAULT NULL COMMENT '审核人',
+  `supply_recommend` tinyint(4) DEFAULT '0' COMMENT '是否推荐',
   `supply_image_src` varchar(218) DEFAULT NULL COMMENT '是否推荐',
   `supply_join_date` datetime DEFAULT NULL COMMENT '''发表日期''',
   PRIMARY KEY (`supply_id`),
   KEY `supply_user_id` (`supply_user_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
-# Dumping data for table mu.mu_supply: 0 rows
+# Dumping data for table mu.mu_supply: 2 rows
 /*!40000 ALTER TABLE `mu_supply` DISABLE KEYS */;
+INSERT INTO `mu_supply` (`supply_id`, `supply_name`, `supply_user_id`, `supply_type`, `supply_keyword`, `supply_category_id`, `supply_contractor`, `supply_content`, `supply_address`, `supply_city_id`, `supply_status`, `supply_phone`, `supply_unit`, `supply_price`, `supply_valid_date`, `supply_check_by`, `supply_recommend`, `supply_image_src`, `supply_join_date`) VALUES
+	(1, '供应钼铁矿', 1, 18, '供应钼铁矿', 23, NULL, '但是发生大法发撒旦发生大法斯蒂芬', '', 8, 1, '1233444545', NULL, 23232, '2013-01-29 23:02:30', 'xiaofuqian', 0, NULL, '2013-01-29 23:02:53'),
+	(2, '求购钼铁矿', 1, 19, '求购钼铁矿', 22, NULL, '求购一批刚出炉的包子', NULL, 8, 1, '1233444545', NULL, 23232, '2013-01-29 23:02:30', NULL, 0, NULL, '2013-01-29 23:02:53');
 /*!40000 ALTER TABLE `mu_supply` ENABLE KEYS */;
 
 
@@ -379,19 +393,19 @@ CREATE TABLE IF NOT EXISTS `mu_supply` (
 DROP TABLE IF EXISTS `mu_term`;
 CREATE TABLE IF NOT EXISTS `mu_term` (
   `term_id` int(11) NOT NULL AUTO_INCREMENT,
-  `term_parent_id` tinyint(4) DEFAULT '0',
+  `term_parent_id` int(4) DEFAULT '0',
   `term_name` varchar(128) NOT NULL,
   `term_slug` varchar(200) DEFAULT NULL COMMENT '''描述''',
-  `term_group_id` tinyint(4) NOT NULL COMMENT '比如产品分类 职位 ',
-  `term_order` tinyint(4) DEFAULT '0',
+  `term_group_id` int(4) NOT NULL COMMENT '比如产品分类 职位 ',
+  `term_order` int(4) DEFAULT '0',
   `term_create_time` datetime DEFAULT NULL,
   PRIMARY KEY (`term_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=20 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=24 DEFAULT CHARSET=utf8;
 
-# Dumping data for table mu.mu_term: 19 rows
+# Dumping data for table mu.mu_term: 23 rows
 /*!40000 ALTER TABLE `mu_term` DISABLE KEYS */;
 INSERT INTO `mu_term` (`term_id`, `term_parent_id`, `term_name`, `term_slug`, `term_group_id`, `term_order`, `term_create_time`) VALUES
-	(1, 0, '有效', NULL, 1, 0, '2013-01-20 10:27:00'),
+	(1, 0, '有效', 'sdfsdfasdfasdf', 1, 0, '2013-01-20 10:27:00'),
 	(2, 0, '无效', NULL, 1, 0, '2013-01-20 17:56:41'),
 	(3, 0, '吨', NULL, 2, 0, '2013-01-22 22:31:17'),
 	(4, 0, '国有企业', NULL, 4, 0, '2013-01-23 20:59:01'),
@@ -409,7 +423,11 @@ INSERT INTO `mu_term` (`term_id`, `term_parent_id`, `term_name`, `term_slug`, `t
 	(16, 0, '行情', NULL, 10, 0, '2013-01-26 23:41:46'),
 	(17, 0, '新闻', NULL, 10, 0, '2013-01-26 23:41:59'),
 	(18, 0, '供应', NULL, 11, 0, '2013-01-27 14:37:56'),
-	(19, 0, '求购', NULL, 11, 0, '2013-01-27 14:37:54');
+	(19, 0, '求购', NULL, 11, 0, '2013-01-27 14:37:54'),
+	(20, 0, '未审核', NULL, 1, 0, '2013-01-27 15:09:39'),
+	(21, 0, '千克', 'test kg', 2, 0, NULL),
+	(22, 0, '钼铁', NULL, 12, 0, '2013-01-29 03:04:41'),
+	(23, 0, '氧化钼', NULL, 12, 0, '2013-01-29 03:05:05');
 /*!40000 ALTER TABLE `mu_term` ENABLE KEYS */;
 
 
@@ -420,9 +438,9 @@ CREATE TABLE IF NOT EXISTS `mu_term_group` (
   `group_name` varchar(100) DEFAULT NULL,
   `group_desc` varchar(128) DEFAULT NULL,
   PRIMARY KEY (`group_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
 
-# Dumping data for table mu.mu_term_group: 11 rows
+# Dumping data for table mu.mu_term_group: 12 rows
 /*!40000 ALTER TABLE `mu_term_group` DISABLE KEYS */;
 INSERT INTO `mu_term_group` (`group_id`, `group_name`, `group_desc`) VALUES
 	(1, '状态', NULL),
@@ -435,7 +453,8 @@ INSERT INTO `mu_term_group` (`group_id`, `group_name`, `group_desc`) VALUES
 	(8, '推荐信息类型', NULL),
 	(9, '推荐位置', NULL),
 	(10, '文章类型', NULL),
-	(11, '供求类型', NULL);
+	(11, '供求类型', NULL),
+	(12, '钼分类', NULL);
 /*!40000 ALTER TABLE `mu_term_group` ENABLE KEYS */;
 
 
@@ -447,14 +466,14 @@ CREATE TABLE IF NOT EXISTS `mu_user` (
   `user_pwd` varchar(40) NOT NULL,
   `user_email` char(100) DEFAULT NULL,
   `user_nickname` varchar(20) DEFAULT NULL,
-  `user_type` tinyint(4) DEFAULT NULL COMMENT '用户类型',
+  `user_type` int(4) DEFAULT NULL COMMENT '用户类型',
   `user_mobile_no` varchar(30) DEFAULT NULL,
   `user_first_name` varchar(50) DEFAULT NULL,
   `user_last_name` varchar(50) DEFAULT NULL,
-  `user_status` tinyint(4) DEFAULT NULL,
-  `user_province_id` tinyint(4) DEFAULT NULL COMMENT '所在省市',
-  `user_city_id` tinyint(4) DEFAULT NULL COMMENT '所在城市',
-  `user_subscribe` tinyint(4) DEFAULT NULL COMMENT '用户是否订阅钼市网行情',
+  `user_status` int(4) DEFAULT NULL,
+  `user_province_id` int(4) DEFAULT NULL COMMENT '所在省市',
+  `user_city_id` int(4) DEFAULT NULL COMMENT '所在城市',
+  `user_subscribe` int(4) DEFAULT NULL COMMENT '用户是否订阅钼市网行情',
   `user_point` bigint(20) DEFAULT NULL COMMENT '用户积分',
   `user_join_date` datetime DEFAULT NULL COMMENT '''注册时间''',
   `user_confirm_date` datetime DEFAULT NULL COMMENT '''验证时间''',
@@ -492,30 +511,31 @@ CREATE TABLE IF NOT EXISTS `mu_user_enterprise` (
   `ent_id` int(11) NOT NULL AUTO_INCREMENT,
   `ent_user_id` int(11) NOT NULL COMMENT '企业对应的网站用户',
   `ent_name` varchar(256) DEFAULT NULL COMMENT '企业对应的网站用户',
-  `ent_type` tinyint(4) DEFAULT NULL COMMENT '企业类型，如国有，私营',
+  `ent_type` int(4) DEFAULT NULL COMMENT '企业类型，如国有，私营',
   `ent_website` varchar(512) DEFAULT NULL COMMENT '''网站地址''',
-  `ent_business_model` tinyint(4) unsigned DEFAULT NULL COMMENT '经营模式，生产，贸易等',
+  `ent_business_model` int(4) unsigned DEFAULT NULL COMMENT '经营模式，生产，贸易等',
   `ent_zipcode` char(32) DEFAULT NULL COMMENT '''邮编''',
   `ent_introduce` text COMMENT '''企业介绍''',
   `ent_location` varchar(218) DEFAULT NULL COMMENT '''企业详细地址''',
-  `ent_city` tinyint(4) DEFAULT NULL COMMENT '所在市地区',
-  `ent_status` tinyint(4) DEFAULT NULL COMMENT '企业状态',
+  `ent_city` int(4) DEFAULT NULL COMMENT '所在市地区',
+  `ent_status` int(4) NOT NULL DEFAULT '0' COMMENT '企业状态',
   `ent_chief` varchar(128) NOT NULL COMMENT '''负责人''',
   `ent_create_time` datetime NOT NULL COMMENT '添加时间',
-  `ent_chief_postion` tinyint(11) DEFAULT NULL COMMENT '负责人职位',
+  `ent_chief_postion` int(11) DEFAULT NULL COMMENT '负责人职位',
   `ent_business_scope` varchar(512) DEFAULT NULL COMMENT '''经营范围''',
   `ent_registered_capital` decimal(10,0) DEFAULT NULL COMMENT '注册资金',
   `ent_recommend` tinyint(1) DEFAULT NULL COMMENT '是否推荐企业',
   `ent_logo` varchar(128) DEFAULT NULL COMMENT '公司LOGO',
   `ent_update_time` datetime DEFAULT NULL COMMENT '修改时间',
+  `ent_check_by` varchar(50) DEFAULT NULL COMMENT '审核人',
   PRIMARY KEY (`ent_id`),
   KEY `ent_user_id` (`ent_user_id`)
 ) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
 # Dumping data for table mu.mu_user_enterprise: 1 rows
 /*!40000 ALTER TABLE `mu_user_enterprise` DISABLE KEYS */;
-INSERT INTO `mu_user_enterprise` (`ent_id`, `ent_user_id`, `ent_name`, `ent_type`, `ent_website`, `ent_business_model`, `ent_zipcode`, `ent_introduce`, `ent_location`, `ent_city`, `ent_status`, `ent_chief`, `ent_create_time`, `ent_chief_postion`, `ent_business_scope`, `ent_registered_capital`, `ent_recommend`, `ent_logo`, `ent_update_time`) VALUES
-	(2, 1, '平顶山钼铁集团', 4, NULL, 6, NULL, NULL, NULL, 4, NULL, '肖富乾', '0000-00-00 00:00:00', 8, NULL, 2000, NULL, NULL, NULL);
+INSERT INTO `mu_user_enterprise` (`ent_id`, `ent_user_id`, `ent_name`, `ent_type`, `ent_website`, `ent_business_model`, `ent_zipcode`, `ent_introduce`, `ent_location`, `ent_city`, `ent_status`, `ent_chief`, `ent_create_time`, `ent_chief_postion`, `ent_business_scope`, `ent_registered_capital`, `ent_recommend`, `ent_logo`, `ent_update_time`, `ent_check_by`) VALUES
+	(2, 1, '平顶山钼铁集团', 4, NULL, 6, NULL, NULL, NULL, 4, 1, '肖富乾', '2012-12-02 00:00:00', 8, NULL, 2000, NULL, NULL, NULL, NULL);
 /*!40000 ALTER TABLE `mu_user_enterprise` ENABLE KEYS */;
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;

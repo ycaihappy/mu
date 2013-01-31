075894600b30d46d53c307dff0fdc5551383d7d3
ycaihappy <ycaihappy@163.com>
2013-01-31 14:48:41 +0800

    case

--- code/webroot/protected/widgets/CaseWidget.php
+++ code/webroot/protected/widgets/CaseWidget.php
@@ -4,7 +4,9 @@ class CaseWidget extends CWidget
 	private $recenltyCase=array();
     public function init()
     {
-    	$this->recenltyCase=SuccessCase::model()->recenltyCase()->findAll();
+        $sql = 'select ent_name,ent_id,purchase_amount,supply_unit from mu_success_case sc,mu_user_enterprise ent,mu_supply sup
+            where sc.supply_id=sup.supply_id and sc.purchase_user_id=ent.ent_user_id';
+        $this->recenltyCase = $connection->createCommand($sql)->queryAll();
     }
 
     public function run()

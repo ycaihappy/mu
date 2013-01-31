5aa48f2464f86e0fb35209189a7661277160d0d1
xiaofuqian <xiao2008abc@163.com>
2013-01-31 11:54:07 +0800

    管理后台菜单选择效果

--- code/webroot/protected/modules/admin/widgets/views/adminLeftMenu.php
+++ code/webroot/protected/modules/admin/widgets/views/adminLeftMenu.php
@@ -6,7 +6,7 @@ case 1:
 	  $this->widget('zii.widgets.CMenu',array(
 			'activeCssClass'=>'on',
 			'items'=>array(
-				array('label'=>'网站基本信息设置', 'url'=>array('/admin/site/index'),'linkOptions'=>array('target'=>'mainFrame')),
+				array('label'=>'网站基本信息设置', 'url'=>array('site/manageTerm'),'linkOptions'=>array('target'=>'mainFrame',),'active'=>true),
 				array('label'=>'地区管理', 'url'=>array('site/manageCity'),'linkOptions'=>array('target'=>'mainFrame')),
 				array('label'=>'基本类别管理', 'url'=>array('site/manageTerm'),'linkOptions'=>array('target'=>'mainFrame')),
 				
@@ -17,7 +17,7 @@ case 2:
   $this->widget('zii.widgets.CMenu',array(
 			'activeCssClass'=>'on',
 			'items'=>array(
-				array('label'=>'会员管理', 'url'=>array('user/manageProduct'),'linkOptions'=>array('target'=>'mainFrame')),
+				array('label'=>'会员管理', 'url'=>array('user/manageProduct'),'linkOptions'=>array('target'=>'mainFrame',),'active'=>true),
 				array('label'=>'管理员管理', 'url'=>array('user/manageSpecial'),'linkOptions'=>array('target'=>'mainFrame')),
 				array('label'=>'角色管理', 'url'=>array('user/manageSupply'),'linkOptions'=>array('target'=>'mainFrame')),
 				array('label'=>'角色管理', 'url'=>array('user/manageSupply'),'linkOptions'=>array('target'=>'mainFrame')),
@@ -29,7 +29,7 @@ case 4:
 		  $this->widget('zii.widgets.CMenu',array(
 			'activeCssClass'=>'on',
 			'items'=>array(
-				array('label'=>'现货管理', 'url'=>array('product/manageProduct'),'linkOptions'=>array('target'=>'mainFrame')),
+				array('label'=>'现货管理', 'url'=>array('product/manageProduct'),'linkOptions'=>array('target'=>'mainFrame'),'active'=>true),
 				array('label'=>'特价管理', 'url'=>array('product/manageSpecial'),'linkOptions'=>array('target'=>'mainFrame')),
 				array('label'=>'供应管理', 'url'=>array('product/manageSupply'),'linkOptions'=>array('target'=>'mainFrame')),
 				array('label'=>'求购管理', 'url'=>array('product/manageBuy'),'linkOptions'=>array('target'=>'mainFrame')),
@@ -42,7 +42,7 @@ case 5:
  $this->widget('zii.widgets.CMenu',array(
 			'activeCssClass'=>'on',
 			'items'=>array(
-				array('label'=>'新闻管理', 'url'=>array('article/manageNews'),'linkOptions'=>array('target'=>'mainFrame')),
+				array('label'=>'新闻管理', 'url'=>array('article/manageNews'),'linkOptions'=>array('target'=>'mainFrame',),'active'=>true),
 				array('label'=>'行情管理', 'url'=>array('article/managePrice'),'linkOptions'=>array('target'=>'mainFrame')),
 				
 				),
@@ -52,7 +52,7 @@ case 6:
  $this->widget('zii.widgets.CMenu',array(
 			'activeCssClass'=>'on',
 			'items'=>array(
-				array('label'=>'广告管理', 'url'=>array('advertisementRecommend/manageAdvertisement'),'linkOptions'=>array('target'=>'mainFrame')),
+				array('label'=>'广告管理', 'url'=>array('advertisementRecommend/manageAdvertisement'),'linkOptions'=>array('target'=>'mainFrame',),'active'=>true),
 				array('label'=>'推荐管理', 'url'=>array('advertisementRecommend/manageRecommend'),'linkOptions'=>array('target'=>'mainFrame')),
 				),
 		)); 

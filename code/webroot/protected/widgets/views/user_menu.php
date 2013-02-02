e6801088420d8f6f14ea684c00c93555da772f2e
ycaihappy <ycaihappy@163.com>
2013-02-02 10:53:15 +0800

    user center view

---
+++ code/webroot/protected/widgets/views/user_menu.php
@@ -0,0 +1,51 @@
+	<div class="m-left-panel" id="J_LeftPanel">
+	<h3><a>基本信息</a></h3>
+	<ul>
+		<li class="on"><a href="member.html" >企业资料</a></li>
+		<li><a href="#" >修改企业资料</a></li>
+		<li><a href="#" >修改登陆密码</a></li>
+		<li><a href="#" >找回密码</a></li>
+	</ul>
+	<h3><a>发布供求信息</a></h3>
+	<ul>
+		<li class="on"><a href="member.html" >企业资料</a></li>
+		<li><a href="#" >修改企业资料</a></li>
+		<li><a href="#" >修改登陆密码</a></li>
+		<li><a href="#" >找回密码</a></li>
+	</ul>
+	<h3><a>发布现货信息</a></h3>
+	<ul>
+		<li class="on"><a href="member.html" >企业资料</a></li>
+		<li><a href="#" >修改企业资料</a></li>
+		<li><a href="#" >修改登陆密码</a></li>
+		<li><a href="#" >找回密码</a></li>
+	</ul>
+	<h3><a>个人资料</a></h3>
+	<ul>
+		<li class="on"><a href="member.html" >企业资料</a></li>
+		<li><a href="#" >修改企业资料</a></li>
+		<li><a href="#" >修改登陆密码</a></li>
+		<li><a href="#" >找回密码</a></li>
+	</ul>
+	<h3><a>供求管理</a></h3>
+	<ul>
+		<li class="on"><a href="member.html" >企业资料</a></li>
+		<li><a href="#" >修改企业资料</a></li>
+		<li><a href="#" >修改登陆密码</a></li>
+		<li><a href="#" >找回密码</a></li>
+	</ul>
+	<h3><a>企业动态</a></h3>
+	<ul>
+		<li class="on"><a href="member.html" >企业资料</a></li>
+		<li><a href="#" >修改企业资料</a></li>
+		<li><a href="#" >修改登陆密码</a></li>
+		<li><a href="#" >找回密码</a></li>
+	</ul>
+	<h3><a>企业相册</a></h3>
+	<ul>
+		<li class="on"><a href="member.html" >企业资料</a></li>
+		<li><a href="#" >修改企业资料</a></li>
+		<li><a href="#" >修改登陆密码</a></li>
+		<li><a href="#" >找回密码</a></li>
+	</ul>
+	</div>

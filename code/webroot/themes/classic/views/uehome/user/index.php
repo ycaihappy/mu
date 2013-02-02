4bb16dd31aef6ce00704357034c91819b3e7d7a3
ycaihappy <ycaihappy@163.com>
2013-02-02 10:50:24 +0800

    new view for user view

---
+++ code/webroot/themes/classic/views/uehome/user/index.php
@@ -0,0 +1,20 @@
+	<div class="m-form">
+	
+	<form>
+	<table border="0" cellpadding="0" cellspacing="0" class="table-field">
+		<tr>
+			<td class="label">用户名：</td><td><input type="text" name="username" value="admin" class="cmp-input" /></td>
+		</tr>
+		<tr>
+			<td class="label">密码：</td><td><input type="text" name="username" value="admin" class="cmp-input" /><span class="msg">不想改，不用填写</span></td>
+		</tr>
+		<tr>
+			<td class="label">地址：</td><td><input type="text" name="address" value="深圳市" class="cmp-input w245" /></td>
+		</tr>
+		<tr>
+			<td></td><td><button type="submit" class="btn-save">保存</button></td>
+		</tr>
+		
+	</table>
+	</form>
+</div>

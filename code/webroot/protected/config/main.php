729daae6f6aefac871d4c9e60b13096f673a7c6b
xiaofuqian <xiao2008abc@163.com>
2013-02-03 18:35:16 +0800

    编辑器

--- code/webroot/protected/config/main.php
+++ code/webroot/protected/config/main.php
@@ -22,7 +22,7 @@ return array(
 		'application.models.*',
 		'application.components.*',
 		'application.widgets.*',
-		//'application.extensions.*',
+		'application.extensions.*',
 		//'packages.solr.*',
 	),
 

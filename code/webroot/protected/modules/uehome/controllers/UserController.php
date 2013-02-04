13a110a7ba96b1d4917eaaf86db50877260f6747
ycaihappy <ycaihappy@163.com>
2013-02-04 12:17:40 +0800

    user center

--- code/webroot/protected/modules/uehome/controllers/UserController.php
+++ code/webroot/protected/modules/uehome/controllers/UserController.php
@@ -7,4 +7,32 @@ class UserController extends Controller
 	{
 		$this->render('index');
 	}
+    public function actionDetail()
+    {
+        $this->render('detail');
+    }
+    public function actionCompany()
+    {
+        $this->render('company');
+    }
+    public function actionPassword()
+    {
+        $this->render('password');
+    }
+    public function actionNews()
+    {
+        $this->render('news');
+    }
+    public function actionSupply()
+    {
+        $this->render('supply');
+    }
+    public function actionGoods()
+    {
+        $this->render('goods');
+    }
+    public function actionCert()
+    {
+        $this->render('cert');
+    }
 }

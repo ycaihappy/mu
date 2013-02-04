78ee2e58e00fd798d73957f5b0fdbd820587913c
ycaihappy <ycaihappy@163.com>
2013-02-04 12:18:48 +0800

    user center

--- code/webroot/protected/widgets/views/user_menu.php
+++ code/webroot/protected/widgets/views/user_menu.php
@@ -1,51 +1,27 @@
 	<div class="m-left-panel" id="J_LeftPanel">
-	<h3><a>基本信息</a></h3>
+	<h3><a>用户管理</a></h3>
 	<ul>
-		<li class="on"><a href="member.html" >企业资料</a></li>
-		<li><a href="#" >修改企业资料</a></li>
-		<li><a href="#" >修改登陆密码</a></li>
-		<li><a href="#" >找回密码</a></li>
+		<li class="on"><a href="index.php?r=uehome/user/detail" >基本信息</a></li>
+		<li><a href="index.php?r=uehome/user/password" >修改密码</a></li>
+		<!-- <li><a href="#" >账号升级</a></li> -->
 	</ul>
-	<h3><a>发布供求信息</a></h3>
+	<h3><a>企业管理</a></h3>
 	<ul>
-		<li class="on"><a href="member.html" >企业资料</a></li>
-		<li><a href="#" >修改企业资料</a></li>
-		<li><a href="#" >修改登陆密码</a></li>
-		<li><a href="#" >找回密码</a></li>
-	</ul>
-	<h3><a>发布现货信息</a></h3>
-	<ul>
-		<li class="on"><a href="member.html" >企业资料</a></li>
-		<li><a href="#" >修改企业资料</a></li>
-		<li><a href="#" >修改登陆密码</a></li>
-		<li><a href="#" >找回密码</a></li>
-	</ul>
-	<h3><a>个人资料</a></h3>
-	<ul>
-		<li class="on"><a href="member.html" >企业资料</a></li>
-		<li><a href="#" >修改企业资料</a></li>
-		<li><a href="#" >修改登陆密码</a></li>
-		<li><a href="#" >找回密码</a></li>
+		<li class="on"><a href="index.php?r=uehome/user/company" >基本信息</a></li>
+		<li><a href="index.php?r=uehome/user/cert" >企业资质</a></li>
+		<li><a href="index.php?r=uehome/user/news" >新闻中心</a></li>
+		<li><a href="index.php?r=uehome/user/ad" >广告中心</a></li>
+		<li><a href="index.php?r=uehome/user/job" >人才招聘</a></li>
 	</ul>
 	<h3><a>供求管理</a></h3>
 	<ul>
-		<li class="on"><a href="member.html" >企业资料</a></li>
-		<li><a href="#" >修改企业资料</a></li>
-		<li><a href="#" >修改登陆密码</a></li>
-		<li><a href="#" >找回密码</a></li>
+		<li class="on"><a href="index.php?r=uehome/user/supply" >供求管理</a></li>
+		<li><a href="index.php?r=uehome/user/supply" >发布供求</a></li>
+		<li><a href="index.php?r=uehome/user/case" >成功供求</a></li>
 	</ul>
-	<h3><a>企业动态</a></h3>
+	<h3><a>现货管理</a></h3>
 	<ul>
-		<li class="on"><a href="member.html" >企业资料</a></li>
-		<li><a href="#" >修改企业资料</a></li>
-		<li><a href="#" >修改登陆密码</a></li>
-		<li><a href="#" >找回密码</a></li>
-	</ul>
-	<h3><a>企业相册</a></h3>
-	<ul>
-		<li class="on"><a href="member.html" >企业资料</a></li>
-		<li><a href="#" >修改企业资料</a></li>
-		<li><a href="#" >修改登陆密码</a></li>
-		<li><a href="#" >找回密码</a></li>
+		<li class="on"><a href="index.php?r=uehome/user/goods" >添加现货</a></li>
+		<li><a href="index.php?r=uehome/user/goods" >特价处理</a></li>
 	</ul>
 	</div>

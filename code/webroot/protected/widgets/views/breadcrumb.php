4b46c688167dd7ff60d4c290926aa55340bea9e8
ycaihappy <ycaihappy@163.com>
2013-02-05 15:45:42 +0800

    tidy up breadcrumb

--- code/webroot/protected/widgets/views/breadcrumb.php
+++ code/webroot/protected/widgets/views/breadcrumb.php
@@ -1,9 +1,14 @@
-	<div class="m-breadcrumb">
-
-		<a title="天津市百华钢铁贸易有限公司" href="http://www.bmlink.com/tjbhgg">天津市百华钢铁贸易有限公司</a> &gt; 
-
-		<a title="金属材料" href="http://www.bmlink.com/tjbhgg/supply/c_12.html">金属材料</a> &gt; 
-
-		<a title="金属型材" href="http://www.bmlink.com/tjbhgg/supply/c_96.html">金属型材</a> &gt; 
-
-	</div>
+     <div class="m-breadcrumb">
+     <p>
+<?php
+$i = 0;
+foreach($this->crumbs as $crumb) {
+    if ( $i == 0)
+        echo '<b class="crumb"></b>'.$crumb['name'];
+    else
+        echo '<i></i>'.$crumb['name'];
+    $i++;
+}
+?>
+     </p>
+     </div> 

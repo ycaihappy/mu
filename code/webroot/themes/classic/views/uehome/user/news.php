038448d10d06f0cc54d4ca88693ccc3576260d4b
ycaihappy <ycaihappy@163.com>
2013-02-05 16:30:39 +0800

    require editor

--- code/webroot/themes/classic/views/uehome/user/news.php
+++ code/webroot/themes/classic/views/uehome/user/news.php
@@ -6,6 +6,22 @@
 			<td class="label">标题：</td><td><input type="text" name="title" value="" class="cmp-input" /></td>
 		</tr>
 		<tr>
+<?php
+$this->widget('application.extensions.ckeditor.CKEditor',array(
+
+    "model"=>$model,
+
+    "attribute"=>'siteDescription',
+
+    "height"=>'400px',
+
+    "width"=>'600px',
+
+    'editorTemplate'=>'advanced',
+
+)
+                                                                );
+?>
 			<td class="label">详细内容：</td><td><textarea name="description" rows="10" class="cmp-text"  ></textarea></td>
 		</tr>
 		<tr>

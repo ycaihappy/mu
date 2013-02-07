87d699b85f8297eed53e4b033640d1ef450342bc
ycaihappy <ycaihappy@163.com>
2013-02-07 15:02:51 +0800

    add news for enterprise

--- code/webroot/protected/modules/uehome/models/NewsForm.php
+++ code/webroot/protected/modules/uehome/models/NewsForm.php
@@ -2,17 +2,9 @@
 
 class NewsForm extends CFormModel
 {
-	public $product_name;
-	public $product_keyword;
-	public $product_type_id;
-	public $district;
-    public $province;
-    public $product_city_id;
-    public $product_unit;
-    public $product_quanity;
-    public $product_price;
-    public $product_content;
-    public $product_join_date;
+	public $art_title;
+	public $art_content;
+	public $art_user_id;
 
 
 	/**
@@ -21,11 +13,9 @@ class NewsForm extends CFormModel
 	public function rules()
     {
         return array(
-            array('supply_category,category', 'required'),		
-            array('keywords,supply_name', 'required'),		
-            array('address', 'required'),		
-            array('district,province,city', 'required'),		
-            array('tel,price,description,effective_time', 'required'),		
+            array('art_title', 'required'),		
+            array('art_content', 'required'),		
+            array('art_user_id', 'safe'),		
 		);
 	}
 
@@ -40,25 +30,15 @@ class NewsForm extends CFormModel
 
 	public function draft()
 	{
-        $addsql = "insert into mu_supply(supply_name, supply_user_id,supply_type,supply_keyword,
-            supply_category_id,supply_content,supply_address,supply_status,supply_phone,supply_unit,supply_price,supply_valid_date)
-            values(:supply_name,:supply_user_id,:supply_type,:supply_keyword,:supply_category_id,
-            :supply_content,:supply_address,:supply_status,:supply_phone,:supply_unit,:supply_price,:supply_valid_date)";
+        $addsql = "insert into mu_article(art_title, art_content, art_user_id)
+            values(:art_title,:art_content,:art_user_id)";
 
         $commd = Yii::app()->db->createCommand($addsql);
 
-        $commd->bindValue(":supply_name", $this->supply_name, PDO::PARAM_STR);
-        $commd->bindValue(":supply_user_id", yii::app()->user->getID(), PDO::PARAM_STR);
-        $commd->bindValue(":supply_type", $this->supply_category);
-        $commd->bindValue(":supply_keyword", $this->keywords);
-        $commd->bindValue(":supply_category_id", $this->category);
-        $commd->bindValue(":supply_content", $this->description);
-        $commd->bindValue(":supply_address", $this->address);
-        $commd->bindValue(":supply_phone", $this->tel);
-        $commd->bindValue(":supply_price", $this->price);
-        $commd->bindValue(":supply_status", 0);
-        $commd->bindValue(":supply_unit", 0);
-        $commd->bindValue(":supply_valid_date", $this->effective_time);
+        $commd->bindValue(":art_title", $this->art_title, PDO::PARAM_STR);
+        $commd->bindValue(":art_content", $this->art_content, PDO::PARAM_STR);
+        $commd->bindValue(":art_user_id", yii::app()->user->getID(), PDO::PARAM_STR);
+
         $commd->execute();
 	}
 }

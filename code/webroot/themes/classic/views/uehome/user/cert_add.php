bd8e90d247448037bdcf6d7ee77c388c470a593f
ycaihappy <ycaihappy@163.com>
2013-02-07 15:22:11 +0800

    add cert

---
+++ code/webroot/themes/classic/views/uehome/user/cert_add.php
@@ -0,0 +1,33 @@
+		<div class="m-breadcrumb">
+	<p><b class="crumb"></b>会员中心<i></i>添加图片</p>
+</div>
+
+	<div class="m-form">
+	
+	<form>
+	<table border="0" cellpadding="0" cellspacing="0" class="table-field">
+		
+		<tr>
+			<td class="label">标题：</td><td><input type="text" name="title" value="" class="cmp-input" /></td>
+		</tr>
+			
+		<tr>
+			<td class="label">图片品类：</td><td><select name="category"><option value="0">1111</option></select></td>
+		</tr>	
+		<tr>
+			<td class="label">图片描述：</td><td><textarea name="description" rows="10" class="cmp-text"  ></textarea></td>
+		</tr>
+		<tr>
+			<td class="label">图片上传：</td><td><input type="file" name="photo" value="浏览" />
+			<p>(图片大小不要超过200K，格式GIF,JPG,PNG图片宽度最大为220像素效果最佳！)</p>
+			
+			<br /><img src="images/thumb.gif" class="thumb"></td>
+		</tr>
+		
+		<tr>
+			<td></td><td><button type="submit" class="btn-save">发布/保存</button></td>
+		</tr>
+		
+	</table>
+	</form>
+</div>

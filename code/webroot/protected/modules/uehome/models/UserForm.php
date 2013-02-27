010dbeb588898347cf01d717b9bfdda05f03d5c8
ycaihappy <ycaihappy@163.com>
2013-02-27 12:29:22 +0800

    modify user base information

--- code/webroot/protected/modules/uehome/models/UserForm.php
+++ code/webroot/protected/modules/uehome/models/UserForm.php
@@ -2,9 +2,13 @@
 
 class UserForm extends CFormModel
 {
-	public $district;
-    public $province;
-    public $city;
+    public $user_province_id;
+    public $user_city_id;
+    public $user_name;
+    public $user_email;
+    public $user_nickname;
+    public $user_mobile_no;
+    public $user_subscribe;
 
 
 	/**
@@ -13,7 +17,12 @@ class UserForm extends CFormModel
 	public function rules()
     {
         return array(
-            array('district,province,city', 'required'),		
+            #array('district,province,city', 'required'),		
+            array('user_nickname', 'required'),		
+            array('user_email', 'required'),		
+            array('user_mobile_no', 'required'),		
+            array('user_subscribe', 'required'),		
+            array('user_name', 'required'),		
 		);
 	}
 
@@ -26,7 +35,23 @@ class UserForm extends CFormModel
 		);
 	}
 
-	public function draft()
+	public function update()
+	{
+        $addsql = "update mu_user set user_name=:user_name,user_nickname=:user_nickname,user_mobile_no=:user_mobile_no,user_email=:user_email,user_subscribe=:user_subscribe where user_id=:user_id";
+
+        $commd = Yii::app()->db->createCommand($addsql);
+
+        $commd->bindValue(":user_name", $this->user_name, PDO::PARAM_STR);
+        $commd->bindValue(":user_nickname", $this->user_nickname, PDO::PARAM_STR);
+        $commd->bindValue(":user_mobile_no", $this->user_mobile_no, PDO::PARAM_STR);
+        $commd->bindValue(":user_email", $this->user_email, PDO::PARAM_STR);
+        $commd->bindValue(":user_subscribe", $this->user_subscribe, PDO::PARAM_STR);
+        $commd->bindValue(":user_id", yii::app()->user->getID(), PDO::PARAM_STR);
+
+        $commd->execute();
+	}
+
+	public function register()
 	{
         $addsql = "insert into mu_user(user_name,user_mobile_no,user_email)
             values(:user_name,:user_mobile_no,:user_email)";
@@ -38,5 +63,8 @@ class UserForm extends CFormModel
         $commd->bindValue(":user_email", yii::app()->user->getID(), PDO::PARAM_STR);
 
         $commd->execute();
+
+        echo json_encode(array('status'=>1,'data'=>array()));
+
 	}
 }

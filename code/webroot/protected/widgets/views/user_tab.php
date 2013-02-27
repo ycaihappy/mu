41454f8c8fe3eab0417a48eb90167198c7a87833
ycaihappy <ycaihappy@163.com>
2013-02-27 14:33:52 +0800

    user center

--- code/webroot/protected/widgets/views/user_tab.php
+++ code/webroot/protected/widgets/views/user_tab.php
@@ -1,3 +1,3 @@
 	<div class="m-menubar">
-		<p class="bar"><span class="on"><a href="">会员中心</a></span><span><a href="">业务管理</a></span></p>
+		<p class="bar"><span class="on"><a href="">会员中心</a></span><!--<span><a href="">业务管理</a></span>--></p>
 	</div>

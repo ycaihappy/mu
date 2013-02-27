89bd3a9097dc19223ad63c304ab4e356346f90e8
ycaihappy <ycaihappy@163.com>
2013-02-27 14:34:04 +0800

    user center

--- code/webroot/protected/widgets/views/user_menu.php
+++ code/webroot/protected/widgets/views/user_menu.php
@@ -16,11 +16,11 @@
 	<h3><a>供求管理</a></h3>
 	<ul>
 		<li><a href="index.php?r=uehome/user/supply" >发布供求</a></li>
-		<li><a href="index.php?r=uehome/user/case" >成功案例</a></li>
+		<!--<li><a href="index.php?r=uehome/user/case" >成功案例</a></li> -->
 	</ul>
 	<h3><a>现货管理</a></h3>
 	<ul>
 		<li><a href="index.php?r=uehome/user/goods" >添加现货</a></li>
-		<li><a href="index.php?r=uehome/user/special" >特价产品</a></li>
+		<!-- <li><a href="index.php?r=uehome/user/special" >特价产品</a></li> -->
 	</ul>
 	</div>

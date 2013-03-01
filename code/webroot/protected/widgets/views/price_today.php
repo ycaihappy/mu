87945f69a5204bcadad4bdf3c76bdbba7497aa50
jackie.lee <ueelife@163.com>
2013-03-02 00:18:24 +0800

    new price index

---
+++ code/webroot/protected/widgets/views/price_today.php
@@ -0,0 +1,136 @@
+		<div class="m-jrbj">
+            <h1>
+                <a href="#">今日报价</a></h1>
+            
+    <table width="93%" cellspacing="0" cellpadding="0" style=" line-height:29px;">
+        
+        <tbody><tr>
+            <th align="left">
+                品名/价格
+            </th>
+            
+            <th>
+                上海
+            </th>
+            
+            <th>
+                北京
+            </th>
+            
+            <th>
+                广州
+            </th>
+            </tr>
+        
+        <tr>
+            <td style="text-align: left; width: 28%">
+                4.75mm热轧
+            </td>
+            
+            <td>
+                4140
+                
+            </td>
+            
+            <td>
+                4050
+                
+            </td>
+            
+            <td>
+                4120
+                
+            </td>
+            </tr>
+        
+        <tr>
+            <td style="text-align: left; width: 28%">
+                1.0mm冷轧
+            </td>
+            
+            <td>
+                4910
+                
+            </td>
+            
+            <td>
+                4990
+                
+            </td>
+            
+            <td>
+                
+                -
+                
+            </td>
+            </tr>
+        
+        <tr>
+            <td style="text-align: left; width: 28%">
+                20mm普板
+            </td>
+            
+            <td>
+                4010
+                
+            </td>
+            
+            <td>
+                
+                -
+                
+            </td>
+            
+            <td>
+                4180
+                
+            </td>
+            </tr>
+        
+        <tr>
+            <td style="text-align: left; width: 28%">
+                20mm三级螺
+            </td>
+            
+            <td>
+                3830
+                
+            </td>
+            
+            <td>
+                3730
+                
+            </td>
+            
+            <td>
+                4060
+                
+            </td>
+            </tr>
+        
+        <tr>
+            <td style="text-align: left; width: 28%">
+                5#角钢
+            </td>
+            
+            <td>
+                3950
+                
+            </td>
+            
+            <td>
+                3600
+                
+            </td>
+            
+            <td>
+                
+                -
+                
+            </td>
+            </tr>
+        
+    </tbody></table>
+ 
+
+        </div>

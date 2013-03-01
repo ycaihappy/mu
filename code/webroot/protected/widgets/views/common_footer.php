b1a05c3e21808ecd4eccf9c8b0be771f3bb83c0d
xiaofuqian <xiao2008abc@163.com>
2013-03-01 09:45:49 +0800

    旺铺模板设置

--- code/webroot/protected/widgets/views/common_footer.php
+++ code/webroot/protected/widgets/views/common_footer.php
@@ -1,4 +1,5 @@
 <script src="js/jquery.1.8.min.js"></script>
+<script src="js/jquery.uploadify.min.js"></script>
 <script src="js/config.js"></script>
 <script src="js/global.js"></script>
 <script src="js/init.js"></script>

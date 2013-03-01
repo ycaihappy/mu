4af22f4ec74fbe56589121685ad8a7821fa41e8c
jackie.lee <ueelife@163.com>
2013-03-01 23:51:21 +0800

    new index

--- code/webroot/themes/classic/views/site/index.php
+++ code/webroot/themes/classic/views/site/index.php
@@ -7,9 +7,54 @@
 		<div class="clearfix"></div>
 	</div>
 	<div class="layout-area">
-        <?php $this->widget("ProductTopWidget", array('type'=>'special'));?>
-        <?php $this->widget("TabListWidget", array('type'=>'supply'));?>
-        <?php $this->widget("ProductTopWidget", array('type'=>'product'));?>
+        <?php //$this->widget("ProductTopWidget", array('type'=>'special'));?>
+        <?php //$this->widget("TabListWidget", array('type'=>'supply'));?>
+        <?php //$this->widget("ProductTopWidget", array('type'=>'product'));?>
+<div class="m-tab-list left">
+			<div class="hd">
+				<span class="on"><a href="">供求</a></span>			
+				<a href="" class="more">更多</a>
+			</div>
+			<div class="bd">
+				<ul class="current">
+					<li class="b"><a href="">毛新宇谈毛泽东激动:国共抗日本质不同</a></li>
+					<li><a href="" target="_blank">沈阳地震主播淡定沈阳地震主播淡定</a></li>
+					<li><a href="" target="_blank">沈阳地震主播淡定沈阳地震主播淡定</a></li>
+					<li><a href="" target="_blank">沈阳地震主播淡定沈阳地震主播淡定</a></li>
+					<li><a href="" target="_blank">沈阳地震主播淡定沈阳地震主播淡定</a></li>
+					<li><a href="" target="_blank">沈阳地震主播淡定沈阳地震主播淡定</a></li>
+					<li><a href="" target="_blank">沈阳地震主播淡定沈阳地震主播淡定</a></li>
+					<li><a href="" target="_blank">沈阳地震主播淡定沈阳地震主播淡定</a></li>
+					<li><a href="" target="_blank">沈阳地震主播淡定沈阳地震主播淡定</a></li>
+			
+				</ul>
+				
+			</div>
+		</div>
+		<div class="m-tab-list home-xh">
+			<div class="hd">
+				<span class="on"><a href="">现货中心</a></span>			
+				<a href="" class="more">更多</a>
+			</div>
+			<div class="fbs">
+			<ul><li><em class="gc01"></em><span>低合金板：</span><i>26895</i></li><li><em class="gc02"></em><span>锅炉容器板：</span><i>26895</i></li><li><em class="gc03"></em><span>普碳中板：</span><i>26895</i></li><li><em class="gc04"></em><span>无缝管：</span><i>888</i></li><li><em class="gc05"></em><span>容器板：</span><i>344</i></li><li><em class="gc06"></em><span>耐磨钢板：</span>7527</li><li><em class="gc07"></em><span>高强板：</span><i>876</i></li><li><em class="gc08"></em><span>螺纹钢：</span><i>222</i></li></ul></div>
+			<div class="bd">
+				
+				<ul class="current">
+					<li class="b"><a href="">毛新宇谈毛泽东激动:国共抗日本质不同</a></li>
+					<li><a href="" target="_blank">沈阳地震主播淡定沈阳地震主播淡定</a></li>
+					<li><a href="" target="_blank">沈阳地震主播淡定沈阳地震主播淡定</a></li>
+					<li><a href="" target="_blank">沈阳地震主播淡定沈阳地震主播淡定</a></li>
+					<li><a href="" target="_blank">沈阳地震主播淡定沈阳地震主播淡定</a></li>
+					<li><a href="" target="_blank">沈阳地震主播淡定沈阳地震主播淡定</a></li>
+					<li><a href="" target="_blank">沈阳地震主播淡定沈阳地震主播淡定</a></li>
+					<li><a href="" target="_blank">沈阳地震主播淡定沈阳地震主播淡定</a></li>
+					<li><a href="" target="_blank">沈阳地震主播淡定沈阳地震主播淡定</a></li>
+					<li><a href="" target="_blank">沈阳地震主播淡定沈阳地震主播淡定</a></li>			
+				</ul>
+				
+			</div>
+		</div>
 
 		<div class="clearfix"></div>
 	</div>
@@ -17,15 +62,200 @@
 		<div class="m-banner">
 			<img src="images/banner.jpg" width="960" height="100" />
 		</div>
-		<div class="col-left">
-        <?php $this->widget("RecommedWidget",array('type'=>1));?>
+		<!--<div class="col-left">
+        <?php //$this->widget("RecommedWidget",array('type'=>1));?>
 		</div>
         <div class="col-right">		
-        <?php $this->widget("PriceWidget");?>
+        <?php //$this->widget("PriceWidget");?>
         <div class="m-mid-ad"><a href=""><img src="images/ad6.jpg" /></a></div>
         <?php #$this->widget("CaseWidget");?>
         <?php $this->widget("KnowledgeWidget");?>
+		</div>-->
+		<div class="col-left">
+			<div class="m-tab-list home-special left">
+			<div class="hd">
+				<span class="on"><a href="">特价</a></span>			
+				<a href="" class="more">更多</a>
+			</div>
+			<div class="bd">
+				<ul class="current">
+                        
+                <li><a target="_blank" href="http://cg.steelcn.com/specinfo.aspx?t=1&amp;id=41859">特价：30普碳中板 邯郸市西丁贸易有限公司</a><em>02-28</em></li>
+    
+                <li><a target="_blank" href="http://cg.steelcn.com/specinfo.aspx?t=1&amp;id=41252">特价：25普碳中板 邯郸市西丁贸易有限公司</a><em>02-28</em></li>
+    
+                <li><a target="_blank" href="http://cg.steelcn.com/specinfo.aspx?t=1&amp;id=45339">特价：14/16/18/20/25/30/40/50耐磨钢板 舞钢市鑫涛钢铁有限公司</a><em>02-28</em></li>
+    
+                <li><a target="_blank" href="http://cg.steelcn.com/specinfo.aspx?t=1&amp;id=45337">特价：100-260mm优碳板 舞钢市鑫涛钢铁有限公司</a><em>02-28</em></li>
+    
+                <li><a target="_blank" href="http://cg.steelcn.com/specinfo.aspx?t=1&amp;id=45336">特价：12-60mm容器板 舞钢市鑫涛钢铁有限公司</a><em>02-28</em></li>
+    
+                <li><a target="_blank" href="http://cg.steelcn.com/specinfo.aspx?t=1&amp;id=45335">特价：18普碳中板 舞钢市鑫涛钢铁有限公司</a><em>02-28</em></li>
+    
+                <li><a target="_blank" href="http://cg.steelcn.com/specinfo.aspx?t=1&amp;id=45331">特价：M3-M24合金板 上海底特精密紧固件有限公司</a><em>02-28</em></li>
+    
+                <li><a target="_blank" href="http://cg.steelcn.com/specinfo.aspx?t=1&amp;id=45295">特价：Φ8~400模具钢 上海浍南实业有限公司</a><em>02-27</em></li>
+    
+
+                    </ul>
+				
+			</div>
 		</div>
+			
+			
+			<div class="m-rcm left" id="J_Rcm">
+				<div class="hd">
+					<span class="on"><a href="">优质推荐</a></span>
+					<a class="more">更多</a>
+					<div class="clearfix"></div>
+				</div>
+				<div class="bd">
+					<div class="scroll">
+					<ul>
+						<li class="on">
+							<div class="img"><img src="images/img1.jpg" width="93" height="68" /></div>							
+							<div class="info">
+								<a>昆明汗信经贸有限公司</a>
+								<p>1分钟前更新 <span class="red">112条</span></p>
+								<p>主营：铝</p>
+							</div>
+						</li>
+						<li>
+							<div class="img"><img src="images/img1.jpg" width="93" height="68" /></div>							
+							<div class="info">
+								<a>昆明汗信经贸有限公司</a>
+								<p>1分钟前更新 <span class="red">112条</span></p>
+								<p>主营：铝</p>
+							</div>
+						</li>
+						<li>
+							<div class="img"><img src="images/img1.jpg" width="93" height="68" /></div>							
+							<div class="info">
+								<a>昆明汗信经贸有限公司</a>
+								<p>1分钟前更新 <span class="red">112条</span></p>
+								<p>主营：铝</p>
+							</div>
+						</li>
+						<li>
+							<div class="img"><img src="images/img1.jpg" width="93" height="68" /></div>							
+							<div class="info">
+								<a>昆明汗信经贸有限公司</a>
+								<p>1分钟前更新 <span class="red">112条</span></p>
+								<p>主营：铝</p>
+							</div>
+						</li>
+						<li>
+							<div class="img"><img src="images/img1.jpg" width="93" height="68" /></div>							
+							<div class="info">
+								<a>昆明汗信经贸有限公司</a>
+								<p>1分钟前更新 <span class="red">112条</span></p>
+								<p>主营：铝</p>
+							</div>
+						</li>
+						<li>
+							<div class="img"><img src="images/img1.jpg" width="93" height="68" /></div>							
+							<div class="info">
+								<a>昆明汗信经贸有限公司</a>
+								<p>1分钟前更新 <span class="red">112条</span></p>
+								<p>主营：铝</p>
+							</div>
+						</li>
+						<li>
+							<div class="img"><img src="images/img1.jpg" width="93" height="68" /></div>							
+							<div class="info">
+								<a>昆明汗信经贸有限公司</a>
+								<p>1分钟前更新 <span class="red">112条</span></p>
+								<p>主营：铝</p>
+							</div>
+						</li>
+						<li class="last">
+							<div class="img"><img src="images/img1.jpg" width="93" height="68" /></div>							
+							<div class="info">
+								<a>昆明汗信经贸有限公司</a>
+								<p>1分钟前更新 <span class="red">112条</span></p>
+								<p>主营：铝</p>
+							</div>
+						</li>
+					</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="col-right">		
+			<div class="m-quot">
+				<div class="hd">
+				<span class="on"><a href="">价格行情</a></span>			
+				<a href="" class="more">更多</a>
+				</div>
+				<div class="bd">
+					<div class="col-l">
+					<p>
+						<select>
+							<option>交易所</option>
+						</select>
+						<select>
+							<option>沪铝</option>
+						</select>
+						<select>
+							<option>沪铝连续</option>
+						</select>
+					</p>
+					<div class="chart">
+						<img src="images/b.png" />
+					</div>
+					</div>
+					<div class="col-r">
+						<ul class="current">
+							<li class="b"><a href="">毛新宇谈毛泽东激动:国共抗日本质不同</a></li>
+							<li><a href="" target="_blank">沈阳地震主播淡定沈阳地震主播淡定</a></li>
+							<li><a href="" target="_blank">沈阳地震主播淡定沈阳地震主播淡定</a></li>
+							<li><a href="" target="_blank">沈阳地震主播淡定沈阳地震主播淡定</a></li>
+							<li><a href="" target="_blank">沈阳地震主播淡定沈阳地震主播淡定</a></li>
+							<li><a href="" target="_blank">沈阳地震主播淡定沈阳地震主播淡定</a></li>
+							<li><a href="" target="_blank">沈阳地震主播淡定沈阳地震主播淡定</a></li>
+							<li><a href="" target="_blank">沈阳地震主播淡定沈阳地震主播淡定</a></li>
+								
+				</ul>
+					</div>
+				</div>
+			</div>
+			<div class="m-mid-ad"><a href=""><img src="images/ad6.jpg" /></a></div>
+			<div class="m-nous">
+				<div class="hd">
+				<span class="on"><a href="">钼百科</a></span>			
+				<a href="" class="more">更多</a>
+				</div>
+				<div class="bd">
+				
+					<dl class="aside">
+						<dt><span><b>生成工艺</b></span></dt>
+						<dd style="height:140px;">
+						<ul class="list" style="display: block;"><li><a target="_blank"  href="http://nj.focus.cn/news/2013-02-28/2909803.html">万科深圳总部或迁至福田 牵手福田开展6大合作</a></li><li><a target="_blank"  href="http://life.gmw.cn/2013-02/28/content_6842987.htm">深圳两男子移动警车前厮打 血溅街头[图]</a></li><li><a target="_blank"  href="http://life.gmw.cn/2013-02/28/content_6841788.htm">男子欲强奸求职女遭反抗 僵持8小时未得逞</a></li><li><a target="_blank"  href="http://district.ce.cn/newarea/roll/201302/28/t20130228_24152054.shtml">南山社保局员工回答问题不耐烦 深圳市民痛哭</a></li><li><a target="_blank"  href="http://politics.people.com.cn/n/2013/0228/c70731-20627923.html">广东省政府部署严查入粤重金属镉超标大米</a></li></ul>
+						</dd>
+					</dl>
+					<dl class="aside">
+						<dt><span><b>选矿技术</b></span></dt>
+						<dd style="height:140px;">
+						<ul class="list"  style="display: block;"><li><a target="_blank"  href="http://nj.focus.cn/news/2013-02-28/2909803.html">万科深圳总部或迁至福田 牵手福田开展6大合作</a></li><li><a target="_blank"  href="http://life.gmw.cn/2013-02/28/content_6842987.htm">深圳两男子移动警车前厮打 血溅街头[图]</a></li><li><a target="_blank"  href="http://life.gmw.cn/2013-02/28/content_6841788.htm">男子欲强奸求职女遭反抗 僵持8小时未得逞</a></li><li><a target="_blank"  href="http://district.ce.cn/newarea/roll/201302/28/t20130228_24152054.shtml">南山社保局员工回答问题不耐烦 深圳市民痛哭</a></li><li><a target="_blank"  href="http://politics.people.com.cn/n/2013/0228/c70731-20627923.html">广东省政府部署严查入粤重金属镉超标大米</a></li></ul>
+						</dd>
+					</dl>
+					<dl class="aside">
+						<dt><span><b>国内标准</b></span></dt>
+						<dd style="height:140px;">
+						<ul class="list"  style="display: block;"><li><a target="_blank"  href="http://nj.focus.cn/news/2013-02-28/2909803.html">万科深圳总部或迁至福田 牵手福田开展6大合作</a></li><li><a target="_blank"  href="http://life.gmw.cn/2013-02/28/content_6842987.htm">深圳两男子移动警车前厮打 血溅街头[图]</a></li><li><a target="_blank"  href="http://life.gmw.cn/2013-02/28/content_6841788.htm">男子欲强奸求职女遭反抗 僵持8小时未得逞</a></li><li><a target="_blank"  href="http://district.ce.cn/newarea/roll/201302/28/t20130228_24152054.shtml">南山社保局员工回答问题不耐烦 深圳市民痛哭</a></li><li><a target="_blank"  href="http://politics.people.com.cn/n/2013/0228/c70731-20627923.html">广东省政府部署严查入粤重金属镉超标大米</a></li></ul>
+						</dd>
+					</dl>
+					<dl class="aside">
+						<dt><span><b>国际标准</b></span></dt>
+						<dd style="height:140px;">
+						<ul class="list"  style="display: block;"><li><a target="_blank"  href="http://nj.focus.cn/news/2013-02-28/2909803.html">万科深圳总部或迁至福田 牵手福田开展6大合作</a></li><li><a target="_blank"  href="http://life.gmw.cn/2013-02/28/content_6842987.htm">深圳两男子移动警车前厮打 血溅街头[图]</a></li><li><a target="_blank"  href="http://life.gmw.cn/2013-02/28/content_6841788.htm">男子欲强奸求职女遭反抗 僵持8小时未得逞</a></li><li><a target="_blank"  href="http://district.ce.cn/newarea/roll/201302/28/t20130228_24152054.shtml">南山社保局员工回答问题不耐烦 深圳市民痛哭</a></li><li><a target="_blank"  href="http://politics.people.com.cn/n/2013/0228/c70731-20627923.html">广东省政府部署严查入粤重金属镉超标大米</a></li></ul>
+						</dd>
+					</dl>
+					
+				</div>
+			</div>
+		</div>
+		<div class="clearfix"></div>
 		<div class="clearfix"></div>
 	</div>
 

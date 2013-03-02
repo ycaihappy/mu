f3129f1008b2aa9fb08d6711047bd520046bcd30
ycaihappy <ycaihappy@163.com>
2013-03-02 20:09:52 +0800

    supply index

---
+++ code/webroot/protected/widgets/SupplyNewsWidget.php
@@ -0,0 +1,16 @@
+<?php
+class SupplyNewsWidget extends CWidget
+{
+    public $top_news;
+    public $top_mu_news;
+    public function init()
+    {
+    	$this->top_news = Article::model()->topNews()->findAll();
+    	$this->top_mu_news = Article::model()->topMuNews()->findAll();
+    }
+
+    public function run()
+    {
+        $this->render('supply_news',array('data'=>$this->top_news,'mu_news'=>$this->top_mu_news));
+    }
+}

e65fc9a71a3deb6e640f484f6a853fcedf65e60c
ycaihappy <ycaihappy@163.com>
2013-03-02 20:10:24 +0800

    supply index

---
+++ code/webroot/protected/widgets/views/supply_ad.php
@@ -0,0 +1,10 @@
+		<div class="m-supply-ad" id="J_IndexAd">
+			<div class="pic">
+				<a href=""><img src="images/290x287_1.gif" /></a>
+				<a href=""><img src="images/290x287_2.gif" /></a>
+				<a href=""><img src="images/290x287_1.gif" /></a>
+				<a href=""><img src="images/290x287_2.gif" /></a>
+				
+			</div>
+			<div class="btns"><a href="javascript:void(0)"></a><a href="javascript:void(0)"></a><a href="javascript:void(0)"></a><a href="javascript:void(0)" class="on"></a></div>
+		</div>
---
+++ code/webroot/protected/widgets/views/supply_mu.php
@@ -0,0 +1,43 @@
+			<div class="m-supply-mt">
+				<div class="hd">
+					<h5>钼材料</h5>
+					<a class="ad"><img src="images/191x100.gif" width="191" height="100" /></a>
+				</div>
+				<div class="bd">
+					<ul>
+						<li class="top"><em>01</em><a>钼材料钼材料钼材料钼材料</a></li>
+						<li class="top"><em>02</em><a>钼材料钼材料钼材料钼材料</a></li>
+						<li class="top"><em>03</em><a>钼材料钼材料钼材料钼材料</a></li>
+						<li><em>04</em><a>钼材料钼材料钼材料钼材料</a></li>
+						<li><em>05</em><a>钼材料钼材料钼材料钼材料</a></li>
+						<li><em>06</em><a>钼材料钼材料钼材料钼材料</a></li>
+						<li><em>07</em><a>钼材料钼材料钼材料钼材料</a></li>
+						<li><em>08</em><a>钼材料钼材料钼材料钼材料</a></li>
+						<li><em>09</em><a>钼材料钼材料钼材料钼材料</a></li>
+						<li><em>10</em><a>钼材料钼材料钼材料钼材料</a></li>
+					</ul>
+					<a class="ad"><img src="images/193x60.gif" width="191" height="60" /></a>
+				</div>
+				<div class="ft">
+					<div class="item">						
+						<a class="ad"><img src="images/73x62_1.gif" width="73" height="62" /></a>
+						<p>
+							<i></i>
+							<a>原材料采购信息</a>
+							<span>气质功国，美丽，大方。</span>
+						</p>
+					</div>
+					<div class="item">
+						
+						<a class="ad"><img src="images/73x62_2.gif" width="73" height="62" /></a>
+						<p>
+							<i></i>
+							<a>原材料采购信息</a>
+							<span>气质功国，美丽，大方。</span>
+						</p>
+					</div>
+					
+				</div>
+				
+			
+			</div>

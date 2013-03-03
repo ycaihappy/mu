9284e9f08edc75db46371f072e7cecd98dae9c73
ycaihappy <ycaihappy@163.com>
2013-03-03 11:49:19 +0800

    new common

---
+++ code/webroot/themes/classic/views/layouts/price_main.php
@@ -0,0 +1,143 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=Edge,chrome=1">
+    <title>行情首页</title>
+    <link rel="stylesheet" href="css/global.css">	
+	<link rel="shortcut icon" type="image/png" href="/img/favicon.png">
+
+
+</head>
+
+<body>
+
+
+<div id="p_hq_index" class="pg-layout">
+
+<div class="m-top">
+	<div class="layout">
+	<p class="user-info">
+		<a href="">请登陆</a>
+		<a href="">免费注册 </a>
+	</p>
+	<p class="site-tool">
+		<span>|</span>
+		<a href="">找回密码</a>
+		<span>|</span>
+		<a href="">网站地图</a>
+		<span>|</span>
+		<a href="">联系客服</a>
+		<span>|</span>
+		<a href="">收藏本站</a>
+	</p>
+	</div>
+</div>
+
+<div class="layout head">
+	<div class="m-logo">
+		<a target="_self" href="" class="logo"><img title="xxx.com - xxxx" alt="zzz" src="images/logo.jpg"></a>
+	</div>
+	<div class="m-search" id="J_SearchForm">
+		<div class="search-triggers">
+			<ul class="switchable-nav">
+			  <li class="selected" data-type="1"><a href="javascript:void(0);">现货</a></li>
+			  <li data-type="2"><a href="javascript:void(0);">新闻</a></li>
+			  <li data-type="3"><a href="javascript:void(0);">行情</a></li>
+			  <li data-type="4"><a href="javascript:void(0);">行情</a></li>
+			  <li data-type="5"><a href="javascript:void(0);">求购</a></li>
+			</ul>
+		 </div>
+		 <div class="search-box">
+			<form name="search" action="" target="_top">
+			  <input type="hidden" name="type" value=""/>
+			  <div class="search-panel-fields">       
+				<input autocomplete="off" autofocus="true" accesskey="s" name="q" id="q">
+				<s></s>
+			  </div>
+			  <button type="submit">搜 索</button>			 
+			 </form>
+		 </div>
+		<div class="clearfix"></div>
+	</div>
+	
+	
+	<!--news nav-->
+	
+	
+	<div class="m-news-nav">
+		<div class="nav">
+		<ul>
+                	<li class="bgnone"><a href="http://www.gtxh.com" title="首页">行情首页</a></li>
+                    <li><a href="http://steel.gtxh.com" title="中国钢铁资源库">钢材行情</a></li>
+                    <li><a href="http://market.gtxh.com" title="中国钢铁现货超市">钢材行情</a></li>
+                    <li><a href="http://news.gtxh.com" title="中国钢铁信息库">特钢行情</a></li>
+                    <li><a href="http://news.gtxh.com/yanjiuzhongxin/" title="研究中心">价格汇总</a></li>
+                    <li><a href="http://bbs.gtxh.com" title="钢铁社区">当日报价</a></li>
+                    <li><a href="http://union.gtxh.com" target="_blank">市场评论</a></li>
+                    <li><a href="http://hyjy.gtxh.com" title="行业视频">分析预测</a></li>
+                    <li><a href="http://www.gtxh.com/go/g.aspx?g=https://www.gopay.com.cn/index.jsp?source=0000008061" target="_blank">走势图</a></li>
+                </ul>
+		</div>
+	</div>
+	
+	
+	
+	<!--news nav-->
+	
+		<div class="m-banner">
+			<img src="images/960x100.gif" width="960" height="80" />
+		</div>
+	
+</div>
+
+<div class="layout main">
+
+<?php echo $content;?>
+
+</div>
+	
+	
+	<div class="layout-area">
+		<div class="m-link">
+			<div class="hd">
+				<span>友情链接</span>
+				<p>欢迎电子工业欢迎电子工业欢迎电子工业<em>(加QQ时请通过)</em></p>
+				<div class="clearfix"></div>
+				<div class="link-list">
+					<a>钢X网</a><a>钢X网</a><a>钢X网</a><a>钢X网</a><a>钢X网</a><a>钢X网</a><a>钢X网</a><a>钢X网</a><a>钢X网</a><a>钢X网</a><a>钢X网</a><a>钢X网</a><a>钢X网</a><a>钢X网</a><a>钢X网</a><a>钢X网</a><a>钢X网</a><a>钢X网</a><a>钢X网</a><a>钢X网</a><a>钢X网</a><a>钢X网</a><a>钢X网</a><a>钢X网</a><a>钢X网</a><a>钢X网</a><a>钢X网</a><a>钢X网</a>
+				</div>
+			</div>
+		</div>
+	</div>
+	
+	<div class="layout-area">
+		<div class="m-footer">
+		<p class="first"><a>公司简介</a> | <a>广告服务</a> | <a>联系方式</a> | <a>服务项目</a> | <a>公司动态</a> | <a>工作机会</a> | <a>网站地图</a> | <a>公司简介</a> | <a>公司简介</a> | <a>公司简介</a></p>
+		<p>客服热线：400-0000-0000  0766-1111111 / 11111111 / 1111111 传真：0443-11111111</p>
+		<p>Copyright &copy; 1998 - 2012 中国XX网 All Rights Reserved</p>
+		<p>全国咨询/投诉电话：400-000-0000   E-mail:kf@xxxx.com ICP证1003232</p>
+		
+		<p class="fa"> <a class="fa1" target="_blank" href="http://www.cqc.com.cn"></a> <a class="fa2" target="_blank" href="http://www.smestar.com/detail.credit?action=preLevel&amp;credcode=NO.20000001143"></a> <a class="fa3" target="_blank" href="http://www.e-315.org"></a> <a class="fa4" target="_blank" href="http://www.isc.org.cn"></a> <a class="fa5" target="_blank" href="http://www.ec.org.cn"></a> <a class="fa6" target="_blank" href="https://www.itrust.org.cn/yz/pjwx.asp?wm=1697657781"></a> 
+	</p>
+		
+		</div>
+	</div>
+	
+
+</div>
+
+
+
+
+</div>
+
+
+
+
+<script src="js/jquery.1.8.min.js"></script>
+<script src="js/config.js"></script>
+<script src="js/global.js"></script>
+<script src="js/init.js"></script>
+</body>
+</html>

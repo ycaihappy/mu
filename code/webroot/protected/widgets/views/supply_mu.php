bce4b9e91147a9a9c7bd9d8b868ad80047d60cfb
ycaihappy <ycaihappy@163.com>
2013-03-03 16:38:00 +0800

    supply top 2

--- code/webroot/protected/widgets/views/supply_mu.php
+++ code/webroot/protected/widgets/views/supply_mu.php
@@ -1,20 +1,15 @@
 			<div class="m-supply-mt">
 				<div class="hd">
-					<h5>钼材料</h5>
+					<h5>推荐供求</h5>
 					<a class="ad"><img src="images/191x100.gif" width="191" height="100" /></a>
 				</div>
 				<div class="bd">
 					<ul>
-						<li class="top"><em>01</em><a>钼材料钼材料钼材料钼材料</a></li>
-						<li class="top"><em>02</em><a>钼材料钼材料钼材料钼材料</a></li>
-						<li class="top"><em>03</em><a>钼材料钼材料钼材料钼材料</a></li>
-						<li><em>04</em><a>钼材料钼材料钼材料钼材料</a></li>
-						<li><em>05</em><a>钼材料钼材料钼材料钼材料</a></li>
-						<li><em>06</em><a>钼材料钼材料钼材料钼材料</a></li>
-						<li><em>07</em><a>钼材料钼材料钼材料钼材料</a></li>
-						<li><em>08</em><a>钼材料钼材料钼材料钼材料</a></li>
-						<li><em>09</em><a>钼材料钼材料钼材料钼材料</a></li>
-						<li><em>10</em><a>钼材料钼材料钼材料钼材料</a></li>
+			<?php for($index=0;$index<count($data);$index++):
+            if ( $index < 3) $class='top';
+						?>
+                            <li class="<?php echo $class;?>"><em><?php echo $index;?></em<a href="<?php echo Yii::app()->controller->createUrl('supply/view',array('supply_id'=>$data[$index]['supply_id']));?>" target="_blank"><?php echo $data[$index]['supply_name']; ?></a></li>
+					<?php endfor;?>			
 					</ul>
 					<a class="ad"><img src="images/193x60.gif" width="191" height="60" /></a>
 				</div>
@@ -23,8 +18,8 @@
 						<a class="ad"><img src="images/73x62_1.gif" width="73" height="62" /></a>
 						<p>
 							<i></i>
-							<a>原材料采购信息</a>
-							<span>气质功国，美丽，大方。</span>
+                            <a><?php echo $data[3]['supply_name'];?></a>
+                            <span><?php echo $data[3]['supply_keyword'];?></span>
 						</p>
 					</div>
 					<div class="item">
@@ -32,8 +27,8 @@
 						<a class="ad"><img src="images/73x62_2.gif" width="73" height="62" /></a>
 						<p>
 							<i></i>
-							<a>原材料采购信息</a>
-							<span>气质功国，美丽，大方。</span>
+                            <a><?php echo $data[6]['supply_name'];?></a>
+                            <span><?php echo $data[6]['supply_keyword'];?></span>
 						</p>
 					</div>
 					

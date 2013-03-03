0e76b75776c3c749173311a72031ffba53820081
ycaihappy <ycaihappy@163.com>
2013-03-03 16:22:16 +0800

    supply index

--- code/webroot/protected/widgets/views/supply_module.php
+++ code/webroot/protected/widgets/views/supply_module.php
@@ -4,15 +4,10 @@
 			</div>
 			<div class="bd">
 				<ul>
-					<li><a>最新资讯最新资讯最新资讯最新资讯最新资讯</a></li>
-					<li><a>最新资讯最新资讯最新资讯最新资讯最新资讯</a></li>
-					<li><a>最新资讯最新资讯最新资讯最新资讯最新资讯</a></li>
-					<li><a>最新资讯最新资讯最新资讯最新资讯最新资讯</a></li>
-					<li><a>最新资讯最新资讯最新资讯最新资讯最新资讯</a></li>
-					<li><a>最新资讯最新资讯最新资讯最新资讯最新资讯</a></li>
-					<li><a>最新资讯最新资讯最新资讯最新资讯最新资讯</a></li>
-					<li><a>最新资讯最新资讯最新资讯最新资讯最新资讯</a></li>
-					<li><a>最新资讯最新资讯最新资讯最新资讯最新资讯</a></li>
+			<?php for($index=0;$index<count($data);$index++):
+						?>
+                            <li><a href="<?php echo Yii::app()->controller->createUrl('news/view',array('art_id'=>$data[$index]['art_id']));?>" target="_blank"><?php echo $data[$index]['art_title']; ?></a></li>
+					<?php endfor;?>			
 				</ul>
 			</div>
 		

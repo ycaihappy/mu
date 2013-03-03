eb555958dfb89d3cddf6bc0b018ee5054a9b2c30
ycaihappy <ycaihappy@163.com>
2013-03-03 17:13:57 +0800

    good data

--- code/webroot/protected/modules/uehome/controllers/UserController.php
+++ code/webroot/protected/modules/uehome/controllers/UserController.php
@@ -102,7 +102,9 @@ class UserController extends Controller {
             $model = Enterprise::model()->find("ent_user_id=:ent_user_id", array('ent_user_id'=>yii::app()->user->getID()));
         }
         $city = City::model()->getAllCity();
-		$this->render ( 'company' ,array('model'=>$model,'city'=>$city));
+
+        $ent_type = Term::model()->getTermsByGroupId(5);
+		$this->render ( 'company' ,array('model'=>$model,'city'=>$city,'ent_type'=>$ent_type));
 	}
 	public function actionPassword() {
 		$this->render ( 'password' );
@@ -133,7 +135,10 @@ class UserController extends Controller {
                 $model->draft();
             }
         }
-		$this->render ( 'supply' ,array('model'=>$model));
+        $supply_type = Term::model()->getTermsByGroupId(11);
+        $product_type= Term::model()->getTermsByGroupId(14);
+        $city = City::model()->getAllCity();
+		$this->render ( 'supply' ,array('model'=>$model,'supply_type'=>$supply_type,'product_type'=>$product_type,'city'=>$city));
 	}
 	public function actionGoods() {
         $model = new ProductForm();
@@ -146,7 +151,10 @@ class UserController extends Controller {
                 $model->draft();
             }
         }
-		$this->render ( 'goods' , array('model'=>$model));
+        $product_type= Term::model()->getTermsByGroupId(14);
+        $unit_type= Term::model()->getTermsByGroupId(2);
+        $city = City::model()->getAllCity();
+		$this->render ( 'goods' , array('model'=>$model,'product_type'=>$product_type,'city'=>$city,'unit_type'=>$unit_type));
 	}
 	public function actionCert() {
         $model = new FileForm;

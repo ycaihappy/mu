c3aa1f118df0e85604c5296cc03c2454ad4f0ed5
ycaihappy <ycaihappy@163.com>
2013-03-03 16:21:41 +0800

    commit supply

--- code/webroot/protected/widgets/SupplyModuleWidget.php
+++ code/webroot/protected/widgets/SupplyModuleWidget.php
@@ -2,13 +2,14 @@
 class SupplyModuleWidget extends CWidget
 {
     public $type;
+    public $top_mu_news;
     public function init()
     {
-    	#$this->top_mu_news = Article::model()->topMuNews()->findAll();
+    	$this->top_mu_news = Article::model()->topMuNews()->findAll();
     }
 
     public function run()
     {
-        $this->render('supply_module',array('type'=>$this->type));
+        $this->render('supply_module',array('type'=>$this->type,'data'=>$this->top_mu_news));
     }
 }

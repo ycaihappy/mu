67385bdadb9e18f21a953d732a8ac15f0f15cda0
ycaihappy <ycaihappy@163.com>
2013-03-05 18:09:44 +0800

    supply list

--- code/webroot/themes/classic/views/uehome/user/slist.php
+++ code/webroot/themes/classic/views/uehome/user/slist.php
@@ -7,25 +7,28 @@
 
 	<div class="repeatbg search">
 			<form>
-				<select name="category"><option>选择栏目</option></select>
+<?php echo CHtml::dropDownList('supply_status','', $status,array());?>
+				<!--<select name="category"><option>选择栏目</option></select>
 				<label>关键字：</label>
 				<input type="text" name="keyword" class="cmp-input" />
 				<select name="rank"><option>排序</option></select>
-				<input type="submit" class="cmp-btn" value="搜索"/>
+				<input type="submit" class="cmp-btn" value="搜索"/>-->
 			<a class="cmp-btn">添加供求信息</a>
 			</form>
 		</div>
 	
 	<table border="0" cellpadding="0" cellspacing="0" class="table-list" width="100%">
 		<tr class="repeatbg">
-			<th width="58">ID</th><th width="35">选择</th><th width="314">标题</th><th width="107">到期时间</th><th width="106">价格</th><th>操作</th>
+			<th width="58">状态</th><th width="35">选择</th><th width="314">标题</th><th width="107">品类</th><th width="106">品位</th><th width="106">存货地</th><th width="106">有效时间</th><th>操作</th>
 		</tr>
 		<tr>
 			<td>001</td>
 			<td><input type="checkbox" /></td>
 			<td>文字文字文字文字文字文字</td>
 			<td>2013-1-1</td>
-			<td>面议</td>
+            <td>面议</td>
+            <td>2013-1-1</td>
+            <td>面议</td>
 			<td><a href="" class="ico-edit">编辑</a><a href="" class="ico-del">编辑</a></td>
 		</tr>
 		<tr class="even">

ab24945f44719e57dd02cf296812032f14771c22
ycaihappy <ycaihappy@163.com>
2013-03-05 15:16:48 +0800

    add link

--- code/webroot/protected/widgets/views/price_enterprise.php
+++ code/webroot/protected/widgets/views/price_enterprise.php
@@ -4,9 +4,8 @@
                 <div class="jxs-con">
                     <div>
                         <ul>
-			<?php for($index=0;$index<count($data);$index++):
-						?>
-							<li><a target="_blank" href=""><?php echo $data[$index]['ent_name'];?></a></li>
+			<?php for($index=0;$index<count($data);$index++):?>
+                            <li><a target="_blank" href="<?php echo Yii::app()->controller->createUrl('/storeFront/default/index',array('username'=>$data[$index]['user']['user_name']));?>"><?php echo $data[$index]['ent_name'];?></a></li>
 					<?php endfor;?>			
 						</ul>
                         <div class="clear">

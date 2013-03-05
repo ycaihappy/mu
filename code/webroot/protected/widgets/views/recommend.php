5444e4988c7a8df89bf4116b78a0d51013295aef
ycaihappy <ycaihappy@163.com>
2013-03-05 15:20:37 +0800

    recommend enterprise for index

--- code/webroot/protected/widgets/views/recommend.php
+++ code/webroot/protected/widgets/views/recommend.php
@@ -1,7 +1,7 @@
 			<div class="m-rcm left" id="J_Rcm">
 				<div class="hd">
 					<span class="on"><a href="">推荐企业</a></span>
-					<a class="more">更多</a>
+					<!--<a class="more">更多</a>-->
 					<div class="clearfix"></div>
 				</div>
 				<div class="bd">
@@ -9,10 +9,10 @@
 					<ul>
 			<?php for($index=0;$index<count($data);$index++):
 						?>
-						<li <?php echo $index==0 ? ' class="on"' : '';?>>
+						<li>
 							<div class="img"><img src="images/img1.jpg" width="93" height="68" /></div>							
 							<div class="info">
-                            <a><?php echo $data[$index]['ent_name'];?></a>
+                            <a href="<?php echo Yii::app()->controller->createUrl('/storeFront/default/index',array('username'=>$data[$index]['user']['user_name']));?>"><?php echo $data[$index]['ent_name'];?></a>
                             <p>1分钟前更新 <span class="red"><?php echo rand(19,200);?>条</span></p>
                                 <p><?php echo $data[$index]['ent_business_scope'];?></p>
 							</div>

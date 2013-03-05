3d0488fdae1763acc90997f39a8a29307b96ff5b
ycaihappy <ycaihappy@163.com>
2013-03-05 14:23:06 +0800

    price enterprise

--- code/webroot/protected/widgets/views/price_enterprise.php
+++ code/webroot/protected/widgets/views/price_enterprise.php
@@ -1,12 +1,14 @@
 	<div class="m-jxs">
                 <h1>
-                    <a class="bt" href="javascript:;">钼经销商</a></h1>
+                    <a class="bt" href="javascript:;">推荐经销商</a></h1>
                 <div class="jxs-con">
                     <div>
-                        <ul><li><a target="_blank" href="">郑州市金润钢铁有限公司</a></li><li><a target="_blank" href="">河南济源钢铁（集团）有限公司</a></li><li><a target="_blank" href="">河南济源钢铁（集团）有限公司</a></li><li><a target="_blank" href="">郑州同乐物资有限公司</a></li><li><a target="_blank" href="">郑州万花钢管有限公司</a></li><li><a target="_blank" href="http://www.hndwsteel.com/">河南大万钢铁有限公司 </a></li></ul>
-						<ul class=""><li><a target="_blank" href="">郑州隆晟物资有限公司</a></li><li><a target="_blank" href="">河南新达森贸易有限公司</a></li><li><a target="_blank" href="">河南光兴钢铁有限公司</a></li><li><a target="_blank" href="">郑州瑞隆物资有限公司</a></li><li><a target="_blank" href="">河南省展程实业有限公司</a></li><li><a target="_blank" href="http://www.aypxwz.com/">安阳市平信物资有限公司</a></li></ul>
-						<ul class=""><li><a target="_blank" href="">郑州隆晟物资有限公司</a></li><li><a target="_blank" href="">河南新达森贸易有限公司</a></li><li><a target="_blank" href="">河南光兴钢铁有限公司</a></li><li><a target="_blank" href="">郑州瑞隆物资有限公司</a></li><li><a target="_blank" href="">河南省展程实业有限公司</a></li><li><a target="_blank" href="http://www.aypxwz.com/">安阳市平信物资有限公司</a></li></ul>
-						<ul class="jxs-ul03"><li><a target="_blank" href="http://www.hnjhgt.com/">河南金亨钢铁有限公司</a></li><li><a target="_blank" href="http://www.455000.cn/">河南中天钢铁有限公司</a></li><li><a target="_blank" href="http://www.hnshgt.com/">河南双海钢铁贸易有限公司</a></li><li><a target="_blank" href="">河南正亚贸易有限公司</a></li><li><a target="_blank" href="">河南安信钢铁销售有限公司</a></li><li><a target="_blank" href="">郑州银泽物资有限公司</a></li></ul>
+                        <ul>
+			<?php for($index=0;$index<count($data);$index++):
+						?>
+							<li><a target="_blank" href=""><?php echo $data[$index]['ent_name'];?></a></li>
+					<?php endfor;?>			
+						</ul>
                         <div class="clear">
                         </div>
                     </div>

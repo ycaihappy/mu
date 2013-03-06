ae7bc308dcaf6acc27ad96667d5c429c59f8a777
ycaihappy <ycaihappy@163.com>
2013-03-06 22:23:30 +0800

    news relate

--- code/webroot/themes/classic/views/news/view.php
+++ code/webroot/themes/classic/views/news/view.php
@@ -3,10 +3,12 @@
 
 <div class="layout main">
 	
-    <?php $this->widget("BreadCrumbWidget", array('crumbs'=>array(
-    array('name'=>'新闻中心','link'=>'#'),
-    array('name'=>'新闻详细', 'link'=>'#')
-    )));?>
+<?php 
+#$this->widget("BreadCrumbWidget", array('crumbs'=>array(
+#    array('name'=>'新闻中心','link'=>'#'),
+#    array('name'=>'新闻详细', 'link'=>'#')
+#)));
+?>
 
 	<div class="layout-area">
 	

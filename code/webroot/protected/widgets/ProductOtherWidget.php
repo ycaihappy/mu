b81d30ee051c2e086bd24ccf0e51eff24be95335
ycaihappy <ycaihappy@163.com>
2013-03-06 23:47:39 +0800

    product detail

--- code/webroot/protected/widgets/ProductOtherWidget.php
+++ code/webroot/protected/widgets/ProductOtherWidget.php
@@ -1,13 +1,16 @@
 <?php
 class ProductOtherWidget extends CWidget
 {
+    public $ent_info;
     public function init()
     {
+        $product_info = Product::model()->find("product_id=:product_id", array('product_id'=>$_REQUEST['product_id']));
     	
+        $this->ent_info = User::model()->with(array('enterprise'=>array('select'=>'*')))->find('user_id=:user_id', array('user_id'=>$product_info->product_user_id));
     }
 
     public function run()
     {
-        $this->render('product_other',array('name'=>'lizhli'));
+        $this->render('product_other',array('data'=>$this->ent_info));
     }
 }

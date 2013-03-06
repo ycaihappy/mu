4fa1c90f95fad03912401dce4b0322fb20d5fb74
ycaihappy <ycaihappy@163.com>
2013-03-06 22:21:53 +0800

    news relate

--- code/webroot/protected/widgets/views/news_relate.php
+++ code/webroot/protected/widgets/views/news_relate.php
@@ -3,25 +3,10 @@
 		<div class="bd">
 			<h4><a target="_blank" href="news_list.html">相关新闻</a></h4>
 		<ul>
-		<li><a target="_blank" href="http://news.xinhuanet.com/video/2013lh/2013-03/05/c_124415760.htm">直播：第十二届全国人大一次会议开幕</a></li>
-
-		<li><a target="_blank" href="content_7771727.htm"><strong>深圳今年启动地铁四期建设 2016地铁基本覆盖全境</strong></a></li>
-
-		<li><a target="_blank" href="content_7771714.htm"></a><a target="_blank" href="../../../../zhuanti/node_137346.htm" class="bt_link"><strong>两会</strong></a> <strong></strong><a target="_blank" href="content_7771697.htm" class="bt_link"><strong>特区一体化如鲠在喉 深代表联名提撤二线关</strong></a></li>
-
-		<li><a target="_blank" href="content_7771682.htm"></a><a target="_blank" href="../../../../zhuanti/node_137429.htm" class="bt_link">海婷跑两会</a> <a target="_blank" href="content_7771623.htm" class="bt_link">连提2年"放宽二胎"未获答 代表要求倒查</a></li>
-
-		<li><a target="_blank" href="content_7772920.htm"></a><a target="_blank" class="bt_link" href="http://news.sznews.com/content/2013-03/05/content_7772788.htm">在粤全国政协委员热议政协报告</a> <a target="_blank" class="bt_link" href="http://news.sznews.com/content/2013-03/05/content_7772842.htm">珠海横琴引领改革创新</a></li>
-
-		<li><a target="_blank" href="content_7771672.htm"></a><a target="_blank" class="bt_link" href="content_7771655.htm">蒋开儒：我是草丛里面“长”出来的词作家</a> <a target="_blank" class="bt_link" href="../../../../photo/content/2013-03/05/content_7771666.htm">高清图</a></li>
-
-		<li><a target="_blank" href="content_7771631.htm">深圳67块闲置土地信息 最大28万平米最长闲置17年</a></li>
-
-		<li><a target="_blank" href="content_7771691.htm"></a><a target="_blank" href="content_7771619.htm" class="bt_link"><strong>保障房福保桂花苑建成2年无人住 到底为谁空置</strong></a> <strong></strong><a target="_blank" href="../../../../photo/content/2013-03/05/content_7771667.htm" class="bt_link"><strong>图</strong></a></li>
-
-		<li><a target="_blank" href="content_7771898.htm">华润集团确定将营运总部2015年从珠海全部迁移到深圳</a></li>
-
-		<li><a target="_blank" href="content_7771772.htm">医院专家号预约挂号一号难求 商业网上为何却随便挂</a></li>
+	<?php for($index=0;$index<count($data);$index++):
+						?>
+                            <li><a href="<?php echo Yii::app()->controller->createUrl('news/view',array('art_id'=>$data[$index]['art_id']));?>" target="_blank"><?php echo $data[$index]['art_title'] ?></a></li>
+					<?php endfor;?>			
 		</ul>
 
 		</div>

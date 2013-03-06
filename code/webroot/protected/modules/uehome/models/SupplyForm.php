2f5d8ae1b7178e1fe80948a0b3cec1e23033b0ab
ycaihappy <ycaihappy@163.com>
2013-03-06 10:27:22 +0800

    handle supply

--- code/webroot/protected/modules/uehome/models/SupplyForm.php
+++ code/webroot/protected/modules/uehome/models/SupplyForm.php
@@ -2,6 +2,7 @@
 
 class SupplyForm extends CFormModel
 {
+    public $supply_id;
 	public $supply_category;
 	public $supply_name;
 	public $supply_keyword;
@@ -60,4 +61,29 @@ class SupplyForm extends CFormModel
         $commd->bindValue(":supply_valid_date", date('Y-m-d H:i:s'));
         $commd->execute();
 	}
+
+    public function update()
+    {
+    $addsql = "update mu_supply set supply_mu_content=:supply_mu_content,supply_name=:supply_name, supply_user_id=:supply_user_id,supply_type=:supply_type,supply_keyword=:supply_keyword,
+            supply_category_id=:supply_category_id,supply_content=:supply_content,supply_address=:supply_address,supply_status=:supply_status,supply_phone=:supply_phone,supply_unit=:supply_unit,supply_price=:supply_price,supply_valid_date=:supply_valid_date
+            where supply_id=:supply_id";
+
+        $commd = Yii::app()->db->createCommand($addsql);
+
+        $commd->bindValue(":supply_id", $this->supply_id, PDO::PARAM_STR);
+        $commd->bindValue(":supply_name", $this->supply_name, PDO::PARAM_STR);
+        $commd->bindValue(":supply_user_id", yii::app()->user->getID(), PDO::PARAM_STR);
+        $commd->bindValue(":supply_type", $this->supply_category);
+        $commd->bindValue(":supply_keyword", $this->supply_keyword);
+        $commd->bindValue(":supply_category_id", $this->category);
+        $commd->bindValue(":supply_content", $this->description);
+        $commd->bindValue(":supply_address", $this->address);
+        $commd->bindValue(":muContent", $this->muContent);
+        $commd->bindValue(":supply_phone", $this->tel);
+        $commd->bindValue(":supply_price", $this->price);
+        $commd->bindValue(":supply_status", 1);
+        $commd->bindValue(":supply_unit", 0);
+        $commd->bindValue(":supply_valid_date", date('Y-m-d H:i:s'));
+        $commd->execute();
+    }
 }

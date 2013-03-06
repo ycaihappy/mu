42e2c065fe8973b8ff743e9989e4ad3305b52209
ycaihappy <ycaihappy@163.com>
2013-03-06 22:17:53 +0800

    news recommend

--- code/webroot/protected/widgets/NewsRecommendWidget.php
+++ code/webroot/protected/widgets/NewsRecommendWidget.php
@@ -1,13 +1,16 @@
 <?php
 class NewsRecommendWidget extends CWidget
 {
+    public $top_news;
+    public $top_mu_news;
     public function init()
     {
-    	
+     	$this->top_news = Article::model()->topNews()->findAll();
+    	$this->top_mu_news = Article::model()->topMuNews()->findAll();   	
     }
 
     public function run()
     {
-        $this->render('news_recommend',array('name'=>'lizhli'));
+        $this->render('news_recommend',array('data'=>$this->top_news,'mu_news'=>$this->top_mu_news));
     }
 }

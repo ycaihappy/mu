b12624e419fb227fde681f1a4424950b94c7d1b1
ycaihappy <ycaihappy@163.com>
2013-03-06 22:09:41 +0800

    relate news

---
+++ code/webroot/protected/widgets/NewsRelateWidget.php
@@ -0,0 +1,13 @@
+<?php
+class NewsRelateWidget extends CWidget
+{
+    public function init()
+    {
+    }
+
+    public function run()
+    {
+        $art_detail = Article::model()->findByPk($_GET['art_id']);
+        $this->render('news_relate',array('art_detail'=>$art_detail));
+    }
+}

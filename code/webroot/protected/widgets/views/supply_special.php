94e554a3fa32801021567ad8a639e5853353f12f
ycaihappy <ycaihappy@163.com>
2013-03-07 15:21:46 +0800

    commit

--- code/webroot/protected/widgets/views/supply_special.php
+++ code/webroot/protected/widgets/views/supply_special.php
@@ -1,13 +1,12 @@
 		<div class="m-tjxx">
         	<h2><span><a href="/special.aspx">更多</a></span>特价信息</h2>
             <ul>
+        <?php if($data):
+        		foreach ($data as $product):
+        ?>       
             	<li style="cursor:pointer;" href=""><span class="tj01"><a target="_blank" href="specinfo.aspx?t=1&amp;id=41252">普碳中板</a></span><span class="tj02">25</span><span class="tj03">118吨</span><span class="tj04">邯郸市</span></li>
-				<li style="cursor:pointer;" href=""><span class="tj01"><a target="_blank" href="specinfo.aspx?t=1&amp;id=41252">普碳中板</a></span><span class="tj02">25</span><span class="tj03">118吨</span><span class="tj04">邯郸市</span></li>
-				<li style="cursor:pointer;" href=""><span class="tj01"><a target="_blank" href="specinfo.aspx?t=1&amp;id=41252">普碳中板</a></span><span class="tj02">25</span><span class="tj03">118吨</span><span class="tj04">邯郸市</span></li>
-				<li style="cursor:pointer;" href=""><span class="tj01"><a target="_blank" href="specinfo.aspx?t=1&amp;id=41252">普碳中板</a></span><span class="tj02">25</span><span class="tj03">118吨</span><span class="tj04">邯郸市</span></li>
-				<li style="cursor:pointer;" href=""><span class="tj01"><a target="_blank" href="specinfo.aspx?t=1&amp;id=41252">普碳中板</a></span><span class="tj02">25</span><span class="tj03">118吨</span><span class="tj04">邯郸市</span></li>
-				<li style="cursor:pointer;" href=""><span class="tj01"><a target="_blank" href="specinfo.aspx?t=1&amp;id=41252">普碳中板</a></span><span class="tj02">25</span><span class="tj03">118吨</span><span class="tj04">邯郸市</span></li>
-				<li style="cursor:pointer;" href=""><span class="tj01"><a target="_blank" href="specinfo.aspx?t=1&amp;id=41252">普碳中板</a></span><span class="tj02">25</span><span class="tj03">118吨</span><span class="tj04">邯郸市</span></li>
-				<li style="cursor:pointer;" href=""><span class="tj01"><a target="_blank" href="specinfo.aspx?t=1&amp;id=41252">普碳中板</a></span><span class="tj02">25</span><span class="tj03">118吨</span><span class="tj04">邯郸市</span></li>
+        <?php endforeach;
+        endif;
+        ?>
             </ul>
         </div>

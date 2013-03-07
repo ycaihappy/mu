d8ad37431e4165b46eff108c98069f0b99376cc6
ycaihappy <ycaihappy@163.com>
2013-03-07 15:19:25 +0800

    commit

--- code/webroot/protected/widgets/SupplySpecialWidget.php
+++ code/webroot/protected/widgets/SupplySpecialWidget.php
@@ -7,21 +7,8 @@ class SupplySpecialWidget extends CWidget
 
     public function run()
     {
-		$entCriteria=new CDbCriteria();
-		$entCriteria->select='ent_name,ent_website,ent_business_scope,ent_location';
-		$entCriteria->join='inner join mu_recommend b on t.ent_id=b.recommend_object_id and b.recommend_status=1 and b.recommend_type=24 and b.recommend_position=53';
-		$entCriteria->condition='ent_status=1';
-		$entCriteria->with=array('user'=>array('select'=>'user_telephone,user_mobile_no,user_first_name'));
-		$entCriteria->limit=10;
-		$advEnt=Enterprise::model()->findAll($entCriteria);
-		if($advEnt)
-		{
-			foreach ($advEnt as &$ent)
-			{
-				$ent->ent_id=$ent->ent_website?$ent->ent_website:$this->getController()->createUrl('/storeFront/default/default',array('username'=>$ent->user->user_name));
-			}
-		}
-		$data=compact('advEnt');
-        $this->render('supply_special',array('data'=>$data));
+
+        $special_list = Product::model()->topSpecial()->findAll();
+        $this->render('supply_special',array('data'=>$special_list));
     }
 }

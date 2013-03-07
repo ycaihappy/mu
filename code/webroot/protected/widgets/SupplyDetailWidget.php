58aaf5bea130e22fc4ea7676fc1c2a8c2cd1943d
ycaihappy <ycaihappy@163.com>
2013-03-07 16:39:34 +0800

    commit

--- code/webroot/protected/widgets/SupplyDetailWidget.php
+++ code/webroot/protected/widgets/SupplyDetailWidget.php
@@ -9,7 +9,7 @@ class SupplyDetailWidget extends CWidget
     {
         $supply_detail = Supply::model()->find("supply_id=:supply_id", array('supply_id'=>$_REQUEST['supply_id']));
         $user_info     = User::model()->with(array('enterprise'=>array('select'=>'*')))->find('user_id=:user_id', array('user_id'=>$supply_detail->supply_user_id));
-        $city  = city::getAllCity();
+        $city  = City::getAllCity();
         $category = Term::model()->getTermsByGroupId(14);
 
         $this->render('supply_detail',array('supply'=>$supply_detail,'user_info'=>$user_info,'category'=>$category, 'city'=>$city));

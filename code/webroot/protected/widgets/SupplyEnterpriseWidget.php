64efc1c9a5ec54f301c02b0abe7a71722a7ca4e6
ycaihappy <ycaihappy@163.com>
2013-03-08 16:43:49 +0800

    commit

--- code/webroot/protected/widgets/SupplyEnterpriseWidget.php
+++ code/webroot/protected/widgets/SupplyEnterpriseWidget.php
@@ -1,12 +1,12 @@
 <?php
 class SupplyEnterpriseWidget extends CWidget
 {
-    public function init()
-    {
-    }
+    public $type = 0;
 
     public function run()
     {
+        if ( $this->type != 1)
+        {
 		$entCriteria=new CDbCriteria();
 		$entCriteria->select='ent_name,ent_website,ent_business_scope,ent_location';
 		$entCriteria->join='inner join mu_recommend b on t.ent_id=b.recommend_object_id and b.recommend_status=1 and b.recommend_type=24 and b.recommend_position=53';
@@ -21,6 +21,11 @@ class SupplyEnterpriseWidget extends CWidget
 				$ent->ent_id=$ent->ent_website?$ent->ent_website:$this->getController()->createUrl('/storeFront/default/default',array('username'=>$ent->user->user_name));
 			}
 		}
-        $this->render('supply_enterprise',array('advEnt'=>$advEnt));
+        }
+        else
+        {
+            $advEnt = Supply::model()->topsupplyIndex()->findAll();
+        }
+        $this->render('supply_enterprise',array('advEnt'=>$advEnt,'type'=>$this->type));
     }
 }

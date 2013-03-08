ea800726ecb29054da4a095f4f2b7fd5ceb7ce3a
ycaihappy <ycaihappy@163.com>
2013-03-09 00:48:54 +0800

    ss

--- code/webroot/themes/classic/views/uehome/user/company.php
+++ code/webroot/themes/classic/views/uehome/user/company.php
@@ -1,6 +1,26 @@
-    		<div class="m-breadcrumb">
+<?php
+$error = $model->getErrors();
+?>
+            <div class="m-breadcrumb">
 	<p><b class="crumb"></b>会员中心<i></i>查看企业信息</p>
 </div>
+<?php
+if ( !empty($error))
+{
+?>
+<div class="block-error">
+<?php
+    foreach ($error as $one_error)
+    {
+?>
+    <p><?php echo $one_error[0];?></p>
+<?php
+    }
+?>
+</div>
+<?php
+}
+?>
     <div class="m-form">
 	
 <?php $form = $this->beginWidget('CActiveForm', array(
@@ -15,20 +35,20 @@
 			<td colspan="2"><h3 class="b-title">企业资料</h3></td>
 		</tr>
 		<tr>
-			<td class="label">企业名称：</td><td><?php echo $form->textField($model, 'ent_name', array('class'=>'cmp-input','value'=>$model->ent_name));?>
+			<td class="label">企业名称：</td><td><?php echo $form->textField($model, 'ent_name', array('class'=>'cmp-input'));?>
 		</td>
 		</tr>
 		<!--<tr>
 			<td class="label">工商注册号：</td><td><input type="text" name="company_no" value="" class="cmp-input" /></td>
+		</tr> -->
+		<tr>
+			<td class="label">企业类型：</td><td><?php echo $form->dropDownList($model, 'ent_type', $ent_type);?></td>
 		</tr>
 		<tr>
-			<td class="label">企业类型：</td><td><select name="company_category"><option value="0">国有企业</option></select></td>
-		</tr>-->
-		<tr>
-			<td class="label">经营模式：</td><td><?php echo $form->dropDownList($model, 'ent_type', $ent_type);?></td>
+			<td class="label">经营方式：</td><td><?php echo $form->dropDownList($model, 'ent_business_model', $business_type);?></td>
 		</tr>
 		<tr>
-			<td class="label">主营产品：</td><td><?php echo $form->textField($model, 'ent_business_scope', array('class'=>'cmp-input','value'=>$model->ent_business_scope));?></td>
+			<td class="label">经营范围：</td><td><?php echo $form->textField($model, 'ent_business_scope', array('class'=>'cmp-input'));?></td>
 		</tr>
 		<tr>
            <td class="label">经营地点：</td><td><?php echo CHtml::dropDownList( 'province',$province,$allProvince,array(
@@ -42,13 +62,13 @@
            ));?> <?php echo $form->dropDownList($model, 'ent_city',$allCity,array('empty'=>'选择城市'));?></td>
         </tr>
 		<tr>
-			<td class="label">详细地址：</td><td><?php echo $form->textField($model, 'ent_location', array('class'=>'cmp-input','value'=>$model->ent_location));?></td>
+			<td class="label">详细地址：</td><td><?php echo $form->textField($model, 'ent_location', array('class'=>'cmp-input'));?></td>
 		</tr>
 		<tr>
-			<td class="label">公司网址：</td><td><?php echo $form->textField($model, 'ent_website', array('class'=>'cmp-input','value'=>$model->ent_website));?></td>
+			<td class="label">公司网址：</td><td><?php echo $form->textField($model, 'ent_website', array('class'=>'cmp-input'));?></td>
 		</tr>
 		<tr>
-			<td class="label">邮编：</td><td><?php echo $form->textField($model, 'ent_zipcode', array('class'=>'cmp-input','value'=>$model->ent_zipcode));?></td>
+			<td class="label">邮编：</td><td><?php echo $form->textField($model, 'ent_zipcode', array('class'=>'cmp-input'));?></td>
 		</tr>		
 		
 		<tr>
@@ -64,13 +84,13 @@
 			<td class="label">品牌名称：</td><td><input type="text" name="brand" value="" class="cmp-input" /></td>
 		</tr> -->
 		<tr>
-			<td class="label">注册资本：</td><td><?php echo $form->textField($model, 'ent_registered_capital', array('class'=>'cmp-input','value'=>$model->ent_registered_capital));?><span class="msg">(万元)</span></td>
+			<td class="label">注册资本：</td><td><?php echo $form->textField($model, 'ent_registered_capital', array('class'=>'cmp-input'));?><span class="msg">(万元)</span></td>
 		</tr>
 		<!--<tr>
 			<td class="label">公司成立年份：</td><td><select name="company_date"><option value="0">2013</option></select></td>
 		</tr>-->
 		<tr>
-			<td class="label">法人代表：</td><td><?php echo $form->textField($model, 'ent_chief', array('class'=>'cmp-input','value'=>$model->ent_chief));?></td>
+			<td class="label">法人代表：</td><td><?php echo $form->textField($model, 'ent_chief', array('class'=>'cmp-input'));?></td>
 		</tr>
 		<tr>
 			<td></td><td><button type="submit" class="btn-save">保存</button></td>

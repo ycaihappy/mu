9980263a8c8a190701b2de39bfb34a7db20e58f1
ycaihappy <ycaihappy@163.com>
2013-03-09 11:02:41 +0800

    handle data

--- code/webroot/protected/widgets/PriceTodayWidget.php
+++ code/webroot/protected/widgets/PriceTodayWidget.php
@@ -5,12 +5,15 @@ class PriceTodayWidget extends CWidget
     public $area;
     public function init()
     {
+        $unit_type= Term::model()->getTermsListByGroupId(2);
+
         $connection = Yii::app()->db;
-        $sql = 'select sum_product_type,sum_product_zone,sum_price from mu_price_summary group by sum_product_type,sum_product_zone';
+        $sql = 'select sum_unit,sum_product_type,sum_product_zone,sum_price from mu_price_summary 
+            group by sum_product_type,sum_product_zone';
         $day_price = $connection->createCommand($sql)->queryAll();
         foreach ($day_price as $price)
         {
-            $this->today_price[$price['sum_product_type']][] = $price['sum_price'];
+            $this->today_price[$price['sum_product_type']][] = $price['sum_price'].'('.$unit_type[$price['sum_unit']].")";
             $this->area[$price['sum_product_zone']]= $price['sum_product_zone'];
         }
     }

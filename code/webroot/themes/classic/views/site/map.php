2ce774911e293609627d418b53644aafb1124dd1
ycaihappy <ycaihappy@163.com>
2013-03-10 11:47:05 +0800

    map

--- code/webroot/themes/classic/views/site/map.php
+++ code/webroot/themes/classic/views/site/map.php
@@ -1,38 +1,12 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="utf-8">
-    <title>网站地图</title>
-    <link rel="stylesheet" href="css/global.css">
-	<meta http-equiv="X-UA-Compatible" content="IE=Edge,chrome=1">
-	<link rel="shortcut icon" type="image/png" href="/img/favicon.png">
-
-
-</head>
-
+<?php $this->widget("CommonHeaderWidget");?>  
 <body>
 
 
 <div id="p_map" class="pg-layout">
+<?php $this->widget("TopWidget");?>  
 
-<div class="m-top">
-	<div class="layout">
-	<p class="user-info">
-		<a href="">请登陆</a>
-		<a href="">免费注册 </a>
-	</p>
-	<p class="site-tool">
-		<span>|</span>
-		<a href="">找回密码</a>
-		<span>|</span>
-		<a href="">网站地图</a>
-		<span>|</span>
-		<a href="">联系客服</a>
-		<span>|</span>
-		<a href="">收藏本站</a>
-	</p>
-	</div>
-</div>
 
 <div class="layout head">
 	<div class="m-logo">
@@ -44,65 +18,13 @@
 </div>
 
 <div class="layout main">
-	
-	
-	
-	
-	
 	<div class="m-map">
 	<div class="hd"></div>
 	<div class="bd">
 		<table width="960" cellspacing="0" cellpadding="0" border="0">
       <tbody>
 	  
-	  
-    <tr>
-    <td width="109" valign="middle" align="left" class="line1"><div class="tab2"><span class="tab2font">分站导航</span></div></td>
-    <td width="881" class="line2">
-        <ul>
-            <li> <a target="_blank" href="http://bj.steelcn.com">北京站</a></li>
-            <li> <a target="_blank" href="http://sh.steelcn.com">上海站</a> </li>
-            <li> <a target="_blank" href="http://tj.steelcn.com">天津站</a> </li>
-            <li> <a target="_blank" href="http://sd.steelcn.com">山东站</a></li>
-             <li> <a target="_blank" href="http://nj.steelcn.com">南京站</a> </li>
-             <li> <a target="_blank" href="http://wh.steelcn.com">武汉站</a> </li>
-             <li> <a target="_blank" href="http://zz.steelcn.com">郑州站</a> </li>
-             <li> <a target="_blank" href="http://ay.steelcn.com">安阳站</a> </li>
-             <li> <a target="_blank" href="http://xa.steelcn.com">西安站</a></li>
-              <li> <a target="_blank" href="http://wg.steelcn.com">舞钢站</a> </li>
-              <li> <a target="_blank" href="http://lc.steelcn.com">聊城站</a> </li>
-               <li><a target="_blank" href="http://hd.steelcn.com">邯郸站</a> </li>
-               <li><a target="_blank" href="http://sjz.steelcn.com">石家庄</a> </li>
-               <li><a target="_blank" href="http://ty.steelcn.com">太原站</a> </li>
-               <li><a target="_blank" href="http://jc.steelcn.com">晋城站</a></li>
-        </ul>
-    </td>
-  </tr>
-   <tr>
-    <td width="109" valign="middle" align="left" class="line1"><div class="tab2"><span class="tab2font">分站导航</span></div></td>
-    <td width="881" class="line2">
-        <ul>
-            <li> <a target="_blank" href="http://bj.steelcn.com">北京站</a></li>
-            <li> <a target="_blank" href="http://sh.steelcn.com">上海站</a> </li>
-            <li> <a target="_blank" href="http://tj.steelcn.com">天津站</a> </li>
-            <li> <a target="_blank" href="http://sd.steelcn.com">山东站</a></li>
-             <li> <a target="_blank" href="http://nj.steelcn.com">南京站</a> </li>
-             <li> <a target="_blank" href="http://wh.steelcn.com">武汉站</a> </li>
-             <li> <a target="_blank" href="http://zz.steelcn.com">郑州站</a> </li>
-             <li> <a target="_blank" href="http://ay.steelcn.com">安阳站</a> </li>
-             <li> <a target="_blank" href="http://xa.steelcn.com">西安站</a></li>
-              <li> <a target="_blank" href="http://wg.steelcn.com">舞钢站</a> </li>
-              <li> <a target="_blank" href="http://lc.steelcn.com">聊城站</a> </li>
-               <li><a target="_blank" href="http://hd.steelcn.com">邯郸站</a> </li>
-               <li><a target="_blank" href="http://sjz.steelcn.com">石家庄</a> </li>
-               <li><a target="_blank" href="http://ty.steelcn.com">太原站</a> </li>
-               <li><a target="_blank" href="http://jc.steelcn.com">晋城站</a></li>
-        </ul>
-    </td>
-  </tr>
-
-
-
+	        <?php $this->widget("SiteMapWidget");?>  
 
 </tbody></table>
 	</div>
@@ -110,37 +32,16 @@
 	</div>
 		
 		
-		
-	
-	
-
 	<div class="layout-area">
-		<div class="m-footer">
-		<p class="first"><a>公司简介</a> | <a>广告服务</a> | <a>联系方式</a> | <a>服务项目</a> | <a>公司动态</a> | <a>工作机会</a> | <a>网站地图</a> | <a>公司简介</a> | <a>公司简介</a> | <a>公司简介</a></p>
-		<p>客服热线：400-0000-0000  0766-1111111 / 11111111 / 1111111 传真：0443-11111111</p>
-		<p>Copyright &copy; 1998 - 2012 中国XX网 All Rights Reserved</p>
-		<p>全国咨询/投诉电话：400-000-0000   E-mail:kf@xxxx.com ICP证1003232</p>
-		
-		<p class="fa"> <a class="fa1" target="_blank" href="http://www.cqc.com.cn"></a> <a class="fa2" target="_blank" href="http://www.smestar.com/detail.credit?action=preLevel&amp;credcode=NO.20000001143"></a> <a class="fa3" target="_blank" href="http://www.e-315.org"></a> <a class="fa4" target="_blank" href="http://www.isc.org.cn"></a> <a class="fa5" target="_blank" href="http://www.ec.org.cn"></a> <a class="fa6" target="_blank" href="https://www.itrust.org.cn/yz/pjwx.asp?wm=1697657781"></a> 
-	</p>
-		
-		</div>
+        <?php $this->widget("FooterWidget");?>  
+
 	</div>
 	
 
 </div>
-
-
-
-
 </div>
 
+        <?php $this->widget("CommonFooterWidget");?>  
 
-
-
-<script src="js/jquery.1.8.min.js"></script>
-<script src="js/config.js"></script>
-<script src="js/global.js"></script>
-<script src="js/init.js"></script>
 </body>
 </html>

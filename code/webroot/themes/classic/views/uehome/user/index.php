18fec1944ca57ceb80bf74c4a7bb5576a5bde4c4
ycaihappy <ycaihappy@163.com>
2013-03-11 21:10:32 +0800

    member index

--- code/webroot/themes/classic/views/uehome/user/index.php
+++ code/webroot/themes/classic/views/uehome/user/index.php
@@ -1,20 +1,58 @@
-	<div class="m-form">
+<div class="m-breadcrumb">
+	<p><b class="crumb"></b>会员中心<i></i>会员首页</p>
+</div>
+
+	<div class="m-info">
 	
-	<form>
+
 	<table border="0" cellpadding="0" cellspacing="0" class="table-field">
 		<tr>
-			<td class="label">用户名：</td><td><input type="text" name="username" value="admin" class="cmp-input" /></td>
+			<td class="label" width="70">用户名：</td><td width="300"></td><td class="label" width="70">邮箱：</td><td width="300"></td>
 		</tr>
 		<tr>
-			<td class="label">密码：</td><td><input type="text" name="username" value="admin" class="cmp-input" /><span class="msg">不想改，不用填写</span></td>
+			<td class="label" width="70">手　机：</td><td width="300"></td><td class="label" width="70">座机：</td><td width="300"></td>
+		</tr>
+	</table>
+	<table border="0" cellpadding="0" cellspacing="0" class="table-field">
+		<tr>
+			<td class="label" width="70">公司名称：</td>
 		</tr>
 		<tr>
-			<td class="label">地址：</td><td><input type="text" name="address" value="深圳市" class="cmp-input w245" /></td>
+			<td class="label" width="70">联系人：</td><td width="300"></td><td class="label" width="70">经营范围：</td><td width="300"></td>
 		</tr>
 		<tr>
-			<td></td><td><button type="submit" class="btn-save">保存</button></td>
+			<td class="label" width="70">注册资金：</td><td width="300"></td><td class="label" width="70">公司网址：</td><td width="300"></td>
+		</tr>
+		<tr>
+			<td class="label" width="70">经营范围：</td><td colspan="3"><img src="images/noimage.gif" width="100" height="100" /></td>
+		</tr>
+		<tr>
+			<td class="label" width="70" valign="top">公司简介：</td><td colspan="3">
+				<div class="info">
+				每天，我们为近5亿网民提供专业的搜索服务，响应来自138个国家和地区的数十亿次搜索请求。
+
+我们欣喜地看到，无论徜徉繁华都市，或是身处偏远边陲，
+
+不管人们在哪里，都可以通过百度，在2000亿网页组成的浩瀚信息海洋中获取新知。
+
+ 
+
+搜索引擎无法使每个人拥有平等的财富，但是搜索引擎可以使每个人与知识和信息保持平等的距离——这是百度回报社会的最好方式。
+
+ 
+
+我们致力于消除信息鸿沟、共享知识社会，使欠发达地区的民众也能受益于信息时代的便利。
+
+我们致力于扶助弱势群体、奉献爱与关怀，让更多善行义举叠加出越来越强劲的声音。
+
+我们致力于联合公益组织、倡导低碳环保，为共同的家园、共同的未来保留共同的希望。
+				</div>
+			</td>
 		</tr>
 		
 	</table>
-	</form>
+
 </div>
+		
+
+

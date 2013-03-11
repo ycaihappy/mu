75b4806e8fb0754b1830056ccafb09b30e73304e
ycaihappy <ycaihappy@163.com>
2013-03-11 21:55:57 +0800

    knowledge index

---
+++ code/webroot/themes/classic/views/knowledge/index.php
@@ -0,0 +1,410 @@
+	<div class="layout-area">
+		<div class="grid-290">
+		<!--module ad-->
+		<div class="m-supply-ad" id="J_IndexAd">
+			<div class="pic">
+				<a href=""><img src="images/290x287_1.gif" /></a>
+				<a href=""><img src="images/290x287_2.gif" /></a>
+				<a href=""><img src="images/290x287_1.gif" /></a>
+				<a href=""><img src="images/290x287_2.gif" /></a>
+				
+			</div>
+			<div class="btns"><a href="javascript:void(0)"></a><a href="javascript:void(0)"></a><a href="javascript:void(0)"></a><a href="javascript:void(0)" class="on"></a></div>
+		</div>
+		<!--module ad-->
+		<!--supply news-->
+		<div class="m-supply-news">
+			<div class="hd clearfix">
+				<span class="on"><a href="">最新资讯</a><i></i></span>			
+			</div>
+			<div class="bd">
+				<ul>
+					<li><a>最新资讯最新资讯最新资讯最新资讯最新资讯</a></li>
+					<li><a>最新资讯最新资讯最新资讯最新资讯最新资讯</a></li>
+					<li><a>最新资讯最新资讯最新资讯最新资讯最新资讯</a></li>
+					<li><a>最新资讯最新资讯最新资讯最新资讯最新资讯</a></li>
+					<li><a>最新资讯最新资讯最新资讯最新资讯最新资讯</a></li>
+					<li><a>最新资讯最新资讯最新资讯最新资讯最新资讯</a></li>
+					<li><a>最新资讯最新资讯最新资讯最新资讯最新资讯</a></li>
+					<li><a>最新资讯最新资讯最新资讯最新资讯最新资讯</a></li>
+					<li><a>最新资讯最新资讯最新资讯最新资讯最新资讯</a></li>
+				</ul>
+			</div>
+			<div class="ft"><a><img src="images/290x80.gif" width="290" height="80" /></a></div>
+		</div>
+		<!--supply news-->
+		
+		</div>
+		<div class="grid-436">
+		<!--m-suppy-topnews-->
+		<div class="m-suppy-topnews">
+			<div class="topnews">
+            <h4><a target="_blank" href="http://a/104/20130204/621599E63AA32F.html">政策向好 钢价有望上攻4200</a></h4>
+            <p>[导读]近期,国内钢材现货市场价格小幅波动，由于春节渐近，贸易商积极性不高，部分商家提前离市，但是由于铁矿石、焦炭价格坚挺，成本支撑强劲，使得钢价保持高位盘整格局。螺纹钢期货价格走势整体强于现货，导致期现价差由负转正。自去年四季度以来，国家连续出台利好政策，使得市场对后市需求预期向好，节前螺纹钢主力合约有望上冲4200关口。总体来看，钢价易涨难跌，趋势多单可继续持有。</p>
+    
+			</div>
+			<div class="ad">
+				<a><img src="images/410x58.gif" /></a>
+			</div>
+			<div class="news">
+				<div class="img"><a><img src="images/108x112_1.gif" width="108" height="112" /></a></div>
+				<div class="list">
+					<ul>       
+					<li><a target="_blank" href="http://a/94/20130204/6218584DB79855.html"><em>江苏常熟重大杀人案 为2万块连杀5人</em></a></li>                    
+					<li><a target="_blank" href="http://a/94/20130204/62176610593223.html">春节的由来与传说</a></li>
+					<li><a target="_blank" href="http://a/94/20130204/62176610593223.html">春节的由来与传说</a></li> 
+					<li><a target="_blank" href="http://a/94/20130204/62176610593223.html">春节的由来与传说</a></li>
+					<li><a target="_blank" href="http://a/94/20130204/62171483F5C28C.html">哈佛大学丑闻 百名学生集体作弊</a></li>
+					<li><a target="_blank" href="http://a/94/20130204/62171483F5C28C.html">哈佛大学丑闻 百名学生集体作弊</a></li>
+				</ul>
+				</div>
+			</div>
+			<div class="news">
+				<div class="img"><a><img src="images/108x112_2.gif" width="108" height="112" /></a></div>
+				<div class="list">
+					<ul>       
+					<li><a target="_blank" href="http://a/94/20130204/6218584DB79855.html"><em>江苏常熟重大杀人案 为2万块连杀5人</em></a></li>                    
+					<li><a target="_blank" href="http://a/94/20130204/62176610593223.html">春节的由来与传说</a></li>
+					<li><a target="_blank" href="http://a/94/20130204/62176610593223.html">春节的由来与传说</a></li> 
+					<li><a target="_blank" href="http://a/94/20130204/62176610593223.html">春节的由来与传说</a></li>
+					<li><a target="_blank" href="http://a/94/20130204/62171483F5C28C.html">哈佛大学丑闻 百名学生集体作弊</a></li>
+					<li><a target="_blank" href="http://a/94/20130204/62171483F5C28C.html">哈佛大学丑闻 百名学生集体作弊</a></li>
+				</ul>
+				</div>
+			</div>
+			<div class="news last">
+				<div class="img"><a><img src="images/108x112_3.gif" width="108" height="112" /></a></div>
+				<div class="list">
+					<ul>       
+					<li><a target="_blank" href="http://a/94/20130204/6218584DB79855.html"><em>江苏常熟重大杀人案 为2万块连杀5人</em></a></li>                    
+					<li><a target="_blank" href="http://a/94/20130204/62176610593223.html">春节的由来与传说</a></li>
+					<li><a target="_blank" href="http://a/94/20130204/62176610593223.html">春节的由来与传说</a></li> 
+					<li><a target="_blank" href="http://a/94/20130204/62176610593223.html">春节的由来与传说</a></li>
+					<li><a target="_blank" href="http://a/94/20130204/62171483F5C28C.html">哈佛大学丑闻 百名学生集体作弊</a></li>
+					<li><a target="_blank" href="http://a/94/20130204/62171483F5C28C.html">哈佛大学丑闻 百名学生集体作弊</a></li>
+				</ul>
+				</div>
+			</div>
+		</div>
+		<!--m-suppy-topnews-->
+		</div>
+		<div class="grid-210">
+			<!--m-supply-mt-->
+			<div class="m-supply-mt">
+				<div class="hd">
+					<h5>钼材料</h5>
+					<a class="ad"><img src="images/191x100.gif" width="191" height="100" /></a>
+				</div>
+				<div class="bd">
+					<ul>
+						<li class="top"><em>01</em><a>钼材料钼材料钼材料钼材料</a></li>
+						<li class="top"><em>02</em><a>钼材料钼材料钼材料钼材料</a></li>
+						<li class="top"><em>03</em><a>钼材料钼材料钼材料钼材料</a></li>
+						<li><em>04</em><a>钼材料钼材料钼材料钼材料</a></li>
+						<li><em>05</em><a>钼材料钼材料钼材料钼材料</a></li>
+						<li><em>06</em><a>钼材料钼材料钼材料钼材料</a></li>
+						<li><em>07</em><a>钼材料钼材料钼材料钼材料</a></li>
+						<li><em>08</em><a>钼材料钼材料钼材料钼材料</a></li>
+						<li><em>09</em><a>钼材料钼材料钼材料钼材料</a></li>
+						<li><em>10</em><a>钼材料钼材料钼材料钼材料</a></li>
+					</ul>
+					<a class="ad"><img src="images/193x60.gif" width="191" height="60" /></a>
+				</div>
+				<div class="ft">
+					<div class="item">						
+						<a class="ad"><img src="images/73x62_1.gif" width="73" height="62" /></a>
+						<p>
+							<i></i>
+							<a>原材料采购信息</a>
+							<span>气质功国，美丽，大方。</span>
+						</p>
+					</div>
+					<div class="item">
+						
+						<a class="ad"><img src="images/73x62_2.gif" width="73" height="62" /></a>
+						<p>
+							<i></i>
+							<a>原材料采购信息</a>
+							<span>气质功国，美丽，大方。</span>
+						</p>
+					</div>
+					
+				</div>
+				
+			
+			</div>
+			<!--m-supply-mt-->
+		</div>
+		
+		
+	
+		<div class="clearfix"></div>
+	</div>
+	
+	<div class="layout-area">
+		<div class="m-banner">
+			<img src="images/banner.jpg" width="960" height="100" />
+		</div>
+		<div class="grid-290">
+			
+							<!--supply news-->
+		<div class="m-supply-news">
+			<div class="hd clearfix">
+				<span class="on"><a href="">最新资讯</a><i></i></span>			
+			</div>
+			<div class="bd">
+				<ul>
+					<li><a>最新资讯最新资讯最新资讯最新资讯最新资讯</a></li>
+					<li><a>最新资讯最新资讯最新资讯最新资讯最新资讯</a></li>
+					<li><a>最新资讯最新资讯最新资讯最新资讯最新资讯</a></li>
+					<li><a>最新资讯最新资讯最新资讯最新资讯最新资讯</a></li>
+					<li><a>最新资讯最新资讯最新资讯最新资讯最新资讯</a></li>
+					<li><a>最新资讯最新资讯最新资讯最新资讯最新资讯</a></li>
+					<li><a>最新资讯最新资讯最新资讯最新资讯最新资讯</a></li>
+					<li><a>最新资讯最新资讯最新资讯最新资讯最新资讯</a></li>
+					<li><a>最新资讯最新资讯最新资讯最新资讯最新资讯</a></li>
+				</ul>
+			</div>
+		
+		</div>
+		<!--supply news-->
+						<!--supply news-->
+		<div class="m-supply-news">
+			<div class="hd clearfix">
+				<span class="on"><a href="">最新资讯</a><i></i></span>			
+			</div>
+			<div class="bd">
+				<ul>
+					<li><a>最新资讯最新资讯最新资讯最新资讯最新资讯</a></li>
+					<li><a>最新资讯最新资讯最新资讯最新资讯最新资讯</a></li>
+					<li><a>最新资讯最新资讯最新资讯最新资讯最新资讯</a></li>
+					<li><a>最新资讯最新资讯最新资讯最新资讯最新资讯</a></li>
+					<li><a>最新资讯最新资讯最新资讯最新资讯最新资讯</a></li>
+					<li><a>最新资讯最新资讯最新资讯最新资讯最新资讯</a></li>
+					<li><a>最新资讯最新资讯最新资讯最新资讯最新资讯</a></li>
+					<li><a>最新资讯最新资讯最新资讯最新资讯最新资讯</a></li>
+					<li><a>最新资讯最新资讯最新资讯最新资讯最新资讯</a></li>
+				</ul>
+			</div>
+		
+		</div>
+		<!--supply news-->
+		</div>
+		<div class="grid-660">		
+
+			<!--module-->
+			
+			<div class="m-suppy-list">
+			
+
+   	  <h2 class="art_gyxx"><span class="on"><a href="">供应信息</a><i></i></span></h2>
+        <table width="100%" cellspacing="0" cellpadding="0" border="0" class="left_table">    
+            
+                    <tbody><tr>
+                      <th>状态</th>
+                      <th>品名</th>
+                      <th>规格</th>
+                      <th>数量</th>
+                      <th>存货地</th>
+                      <th>发布日期</th>
+                    </tr>
+                
+                    <tr>                     
+                      <td><font style="color:red;">供应中</font></td>
+                      <td class="td02"><a target="_blank" href="stockinfo.aspx?t=2&amp;id=45412">合金管</a></td>
+                      <td>5</td>
+                      <td>56000</td>
+                      <td>东莞市</td>
+                      <td>2013/03/01</td>                      
+                    </tr>
+                
+                    <tr>                     
+                      <td><font style="color:red;">供应中</font></td>
+                      <td class="td02"><a target="_blank" href="stockinfo.aspx?t=2&amp;id=44232">普碳中板</a></td>
+                      <td>6-100mm</td>
+                      <td>3000吨</td>
+                      <td>郑州市</td>
+                      <td>2013/03/01</td>                      
+                    </tr>
+                
+                    <tr>                     
+                      <td><font style="color:red;">供应中</font></td>
+                      <td class="td02"><a target="_blank" href="stockinfo.aspx?t=2&amp;id=43709">普碳中板</a></td>
+                      <td>6-80</td>
+                      <td>2000吨</td>
+                      <td>舞钢市</td>
+                      <td>2013/03/01</td>                      
+                    </tr>
+                
+                    <tr>                     
+                      <td><font style="color:red;">供应中</font></td>
+                      <td class="td02"><a target="_blank" href="stockinfo.aspx?t=2&amp;id=45340">不锈钢管</a></td>
+                      <td>1.5</td>
+                      <td>1000吨</td>
+                      <td>佛山市</td>
+                      <td>2013/02/28</td>                      
+                    </tr>
+                
+                    <tr>                     
+                      <td><font style="color:red;">供应中</font></td>
+                      <td class="td02"><a target="_blank" href="stockinfo.aspx?t=2&amp;id=20599">普碳中板</a></td>
+                      <td>0.8  1.0  1.2</td>
+                      <td>大量</td>
+                      <td>合肥市</td>
+                      <td>2013/02/28</td>                      
+                    </tr>
+                
+                    <tr>                     
+                      <td><font style="color:red;">供应中</font></td>
+                      <td class="td02"><a target="_blank" href="stockinfo.aspx?t=2&amp;id=20381">普碳中板</a></td>
+                      <td>0.8mm,1.0mm,1.2mm</td>
+                      <td>820000米</td>
+                      <td>合肥市</td>
+                      <td>2013/02/28</td>                      
+                    </tr>
+                
+                    <tr>                     
+                      <td><font style="color:red;">供应中</font></td>
+                      <td class="td02"><a target="_blank" href="stockinfo.aspx?t=2&amp;id=45333">H型钢</a></td>
+                      <td>150*150*4.5*6_500*25…</td>
+                      <td>2000吨</td>
+                      <td>天津市</td>
+                      <td>2013/02/28</td>                      
+                    </tr>
+                
+                    <tr>                     
+                      <td><font style="color:red;">供应中</font></td>
+                      <td class="td02"><a target="_blank" href="stockinfo.aspx?t=2&amp;id=45332">不锈钢管</a></td>
+                      <td>1.5</td>
+                      <td>1000吨</td>
+                      <td>佛山市</td>
+                      <td>2013/02/28</td>                      
+                    </tr>
+                
+                    <tr>                     
+                      <td><font style="color:red;">供应中</font></td>
+                      <td class="td02"><a target="_blank" href="stockinfo.aspx?t=2&amp;id=43337">合结钢</a></td>
+                      <td>M3-M24</td>
+                      <td>1吨</td>
+                      <td>上海市</td>
+                      <td>2013/02/28</td>                      
+                    </tr>
+                
+                    <tr>                     
+                      <td><font style="color:red;">供应中</font></td>
+                      <td class="td02"><a target="_blank" href="stockinfo.aspx?t=2&amp;id=45328">普碳中板</a></td>
+                      <td>70</td>
+                      <td>14.534</td>
+                      <td>舞钢市</td>
+                      <td>2013/02/28</td>                      
+                    </tr>
+                
+                    <tr>                     
+                      <td><font style="color:red;">供应中</font></td>
+                      <td class="td02"><a target="_blank" href="stockinfo.aspx?t=2&amp;id=45326">容器板</a></td>
+                      <td>10.12.20.22.30.40.45…</td>
+                      <td>220</td>
+                      <td>舞钢市</td>
+                      <td>2013/02/28</td>                      
+                    </tr>
+                
+                    <tr>                    
+                      <td><font style="color:red;">供应中</font></td>
+                      <td class="td02"><a target="_blank" href="stockinfo.aspx?t=2&amp;id=45325">高强板</a></td>
+                      <td>16.18.20.30.35.40.50…</td>
+                      <td>220</td>
+                      <td>舞钢市</td>
+                      <td>2013/02/28</td>                      
+                    </tr>
+                
+                    <tr>                     
+                      <td><font style="color:red;">供应中</font></td>
+                      <td class="td02"><a target="_blank" href="stockinfo.aspx?t=2&amp;id=45324">普碳中板</a></td>
+                      <td>8.10.12.14.16.20－80…</td>
+                      <td>120</td>
+                      <td>舞钢市</td>
+                      <td>2013/02/28</td>                      
+                    </tr>
+                
+                    <tr>                     
+                      <td><font style="color:red;">供应中</font></td>
+                      <td class="td02"><a target="_blank" href="stockinfo.aspx?t=2&amp;id=45244">普碳中板</a></td>
+                      <td>8--400</td>
+                      <td>大量</td>
+                      <td>无锡市</td>
+                      <td>2013/02/28</td>                      
+                    </tr>
+                
+                    <tr>                     
+                      <td><font style="color:red;">供应中</font></td>
+                      <td class="td02"><a target="_blank" href="stockinfo.aspx?t=2&amp;id=45320">轴承钢</a></td>
+                      <td>1.8</td>
+                      <td>51800kg</td>
+                      <td>上海市</td>
+                      <td>2013/02/27</td>                      
+                    </tr>
+                
+                    <tr>                     
+                      <td><font style="color:red;">供应中</font></td>
+                      <td class="td02"><a target="_blank" href="stockinfo.aspx?t=2&amp;id=45319">轴承钢</a></td>
+                      <td>1.8</td>
+                      <td>51800kg</td>
+                      <td>上海市</td>
+                      <td>2013/02/27</td>                      
+                    </tr>
+                
+                    <tr>                     
+                      <td><font style="color:red;">供应中</font></td>
+                      <td class="td02"><a target="_blank" href="stockinfo.aspx?t=2&amp;id=16850">槽钢</a></td>
+                      <td>16</td>
+                      <td>46.852</td>
+                      <td>邯郸市</td>
+                      <td>2013/02/27</td>                      
+                    </tr>
+                
+                    <tr>                     
+                      <td><font style="color:red;">供应中</font></td>
+                      <td class="td02"><a target="_blank" href="stockinfo.aspx?t=2&amp;id=45318">热轧带钢</a></td>
+                      <td>1.6</td>
+                      <td>51800kg</td>
+                      <td>上海市</td>
+                      <td>2013/02/27</td>                      
+                    </tr>
+                
+                    <tr>                     
+                      <td><font style="color:red;">供应中</font></td>
+                      <td class="td02"><a target="_blank" href="stockinfo.aspx?t=2&amp;id=45317">冷轧板</a></td>
+                      <td>1.5</td>
+                      <td>51800kg</td>
+                      <td>上海市</td>
+                      <td>2013/02/27</td>                      
+                    </tr>
+                
+                    <tr>                     
+                      <td><font style="color:red;">供应中</font></td>
+                      <td class="td02"><a target="_blank" href="stockinfo.aspx?t=2&amp;id=45314">容器板</a></td>
+                      <td>8.14.16.20.30.40.45.…</td>
+                      <td>480</td>
+                      <td>舞钢市</td>
+                      <td>2013/02/27</td>                      
+                    </tr>
+                
+        </tbody></table>
+	  	<div class="page" id="fenye">
+
+		<a href="javascript:void(0);">&lt;上一页</a>
+		<a class="page_cur">1</a>
+		<a href="?p=2">2</a>
+		<a href="?p=3">3</a>
+		<a href="?p=4">4</a>
+		<a>…</a>
+		<a title="下一页" href="?p=2">下一页&gt;</a>
+		<a title="尾页" href="?p=827">尾页</a></div>
+
+			
+			</div>
+			<!--module-->
+			
+		</div>
+		<div class="clearfix"></div>
+	</div>

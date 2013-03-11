f131697de73f65f34392a0f22983f0d270e1e1ce
ycaihappy <ycaihappy@163.com>
2013-03-11 22:50:53 +0800

    news detail

--- code/webroot/protected/widgets/views/news_detail.php
+++ code/webroot/protected/widgets/views/news_detail.php
@@ -4,7 +4,7 @@
             <h1><?php echo $art_detail['art_title'];?></h1>
                   <div class="tit-bar clearfix">
 					<div class="ll">
-                    <span class="color-a-0"><a target="_blank" accesskey="5" href="http://news.qq.com/newsgn/zhxw/shizhengxinwen.htm" title="更多时政新闻相关内容列表">时政新闻</a></span><span class="bor-tit"></span><span class="color-a-1" ><a target="_blank" href="http://www.xinhuanet.com"><?php echo $art_detail['art_source'];?></a></span><span class="infoMblog"><span class="color-a-2" bosszone="jgmblog"></span></span><span style="margin:0 8px;" class="color-a-3"><?php echo $art_detail['art_user_id'];?></span><span class="article-time"><?php echo $art_detail['art_post_date'];?></span><span class="bor-tit"></span></div>
+                    <span class="color-a-0"></span><span class="bor-tit"></span><span class="color-a-1" ><a target="_blank" href=""><?php echo $art_detail['art_source'];?></a></span><span class="infoMblog"><span class="color-a-2" bosszone="jgmblog"></span></span><span style="margin:0 8px;" class="color-a-3"><?php echo $art_detail['art_user_id'];?></span><span class="article-time"><?php echo $art_detail['art_post_date'];?></span><span class="bor-tit"></span></div>
 
 					</div>
                  

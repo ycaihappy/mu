791862ebca15948bd509d5a53457b3c99f0a50da
ycaihappy <ycaihappy@163.com>
2013-03-11 22:33:35 +0800

    user index

--- code/webroot/themes/classic/views/uehome/user/index.php
+++ code/webroot/themes/classic/views/uehome/user/index.php
@@ -4,55 +4,59 @@
 
 	<div class="m-info">
 	
-
-	<table border="0" cellpadding="0" cellspacing="0" class="table-field">
-		<tr>
-			<td class="label" width="70">用户名：</td><td width="300"></td><td class="label" width="70">邮箱：</td><td width="300"></td>
-		</tr>
-		<tr>
-			<td class="label" width="70">手　机：</td><td width="300"></td><td class="label" width="70">座机：</td><td width="300"></td>
-		</tr>
-	</table>
-	<table border="0" cellpadding="0" cellspacing="0" class="table-field">
-		<tr>
-			<td class="label" width="70">公司名称：</td>
-		</tr>
-		<tr>
-			<td class="label" width="70">联系人：</td><td width="300"></td><td class="label" width="70">经营范围：</td><td width="300"></td>
-		</tr>
-		<tr>
-			<td class="label" width="70">注册资金：</td><td width="300"></td><td class="label" width="70">公司网址：</td><td width="300"></td>
-		</tr>
-		<tr>
-			<td class="label" width="70">经营范围：</td><td colspan="3"><img src="images/noimage.gif" width="100" height="100" /></td>
-		</tr>
-		<tr>
-			<td class="label" width="70" valign="top">公司简介：</td><td colspan="3">
-				<div class="info">
-				每天，我们为近5亿网民提供专业的搜索服务，响应来自138个国家和地区的数十亿次搜索请求。
-
-我们欣喜地看到，无论徜徉繁华都市，或是身处偏远边陲，
-
-不管人们在哪里，都可以通过百度，在2000亿网页组成的浩瀚信息海洋中获取新知。
-
- 
-
-搜索引擎无法使每个人拥有平等的财富，但是搜索引擎可以使每个人与知识和信息保持平等的距离——这是百度回报社会的最好方式。
-
- 
-
-我们致力于消除信息鸿沟、共享知识社会，使欠发达地区的民众也能受益于信息时代的便利。
-
-我们致力于扶助弱势群体、奉献爱与关怀，让更多善行义举叠加出越来越强劲的声音。
-
-我们致力于联合公益组织、倡导低碳环保，为共同的家园、共同的未来保留共同的希望。
+		<ul>
+			<li>
+            <div class="item"><label>用户名：</label><?php echo $user->user_name;?></div><div class="item"><label>昵称：</label><?php echo $user->user_nickname;?></div>
+			</li>
+			<li>
+            <div class="item"><label>手机号：</label><?php echo $user->user_mobile_no;?></div><div class="item"><label>邮箱：</label><?php echo $user->user_email;?></div>
+			</li>
+			<a class="edit" href="<?php echo Yii::app()->controller->createUrl('/uehome/user/detail');?>">[编辑]</a>
+		</ul>
+		<ul>
+			<li>
+            <div class="item"><label>企业名称：</label><?php echo $enterprise->ent_name;?></div><div class="item"><label>联络人：</label><?php echo $enterprise->ent_chief;?></div>
+			</li>
+			<li>
+            <div class="item"><label>公司类型：</label><?php echo $ent_type[$enterprise->ent_type];?></div><div class="item"><label>业务类型：</label><?php echo $business_type[$enterprise->ent_business_model];?></div>
+			</li>
+			<li>
+            <div class="item"><label>注册资金：</label><?php echo $enterprise->ent_registered_capital."万元";?></div><div class="item"><label>网址：</label><?php echo $enterprise->ent_website;?></div>
+			</li>
+			<li>
+            <div class="item"><label>地址：</label><?php echo $enterprise->ent_location;?></div><div class="item"><label>传真：</label><?php echo $enterprise->ent_tax;?></div>
+			</li>
+			<li>
+				<div class="item">
+				<label>公司简介：</label>
+					<div class="info">
+<?php echo $enterprise->ent_introduce;?>
 				</div>
-			</td>
-		</tr>
+				</div>
+			</li>
+            <a class="edit" href="<?php echo Yii::app()->controller->createUrl('/uehome/user/company');?>">[编辑]</a>
+		</ul>
 		
-	</table>
-
+		<ul class="pic">
+			<li>
+<?php 
+if ( !empty($cert_list) ) 
+{
+foreach ($cert_list as $cert)
+{
+?>
+				<div class="item">
+                <p><?php echo $category[$cert['file_type_id']];?></p>
+                <img src="<?php echo $cert['file_url'];?>" width="125" height="125" />					
+				</div>
+<?php
+}
+?>
+				<div class="clearfix"></div>
+			</li>
+            <a class="edit" href="<?php echo Yii::app()->controller->createUrl('/uehome/user/cert');?>">[编辑]</a>
+<?php
+}
+?>
+		</ul>
 </div>
-		
-
-

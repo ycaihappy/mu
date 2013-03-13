db33aec01b22d6e396050091468406929a297162
ycaihappy <ycaihappy@163.com>
2013-03-13 10:49:11 +0800

    statistic

--- code/webroot/protected/widgets/views/footer.php
+++ code/webroot/protected/widgets/views/footer.php
@@ -1,7 +1,9 @@
 		<div class="m-footer">
 		<p class="first"><a href="<?php echo $this->getController()->createUrl('/about/index')?>">公司简介</a> | <a href="<?php echo $this->getController()->createUrl('/about/contact')?>">联系方式</a> | <a href="<?php echo $this->getController()->createUrl('/about/agrement')?>">使用协议</a> |<a href="<?php echo $this->getController()->createUrl('/about/copyright')?>">版权隐私</a> | <a href="<?php echo Yii::app()->controller->createUrl('site/map');?>">网站地图</a></p>
 		<p>客服热线：<?php echo $this->getController()->siteConfig->csHotline1?>  <?php echo $this->getController()->siteConfig->csHotline2?> 传真：<?php echo $this->getController()->siteConfig->fax?></p>
-		<p>Copyright &copy; 1998 - 2012  钼市网  All Rights Reserved</p>
+        <p>Copyright &copy; 1998 - 2012  钼市网  All Rights Reserved<script language="javascript" type="text/javascript" src="http://js.users.51.la/15606198.js"></script></p>
+
+<noscript><a href="http://www.51.la/?15606198" target="_blank"><img alt="&#x6211;&#x8981;&#x5566;&#x514D;&#x8D39;&#x7EDF;&#x8BA1;" src="http://img.users.51.la/15606198.asp" style="border:none" /></a></noscript>
 		<p>全国咨询/投诉电话：<?php echo $this->getController()->siteConfig->advisoryHotline ?>   E-mail:<?php echo $this->getController()->siteConfig->csEmail?> ICP证<?php echo  $this->getController()->siteConfig->siteIcpCode?></p>
 		
 		<p class="fa"> <a class="fa1" target="_blank" href="http://www.cqc.com.cn"></a> <a class="fa2" target="_blank" href="http://www.smestar.com/detail.credit?action=preLevel&amp;credcode=NO.20000001143"></a> <a class="fa3" target="_blank" href="http://www.e-315.org"></a> <a class="fa4" target="_blank" href="http://www.isc.org.cn"></a> <a class="fa5" target="_blank" href="http://www.ec.org.cn"></a> <a class="fa6" target="_blank" href="https://www.itrust.org.cn/yz/pjwx.asp?wm=1697657781"></a> 

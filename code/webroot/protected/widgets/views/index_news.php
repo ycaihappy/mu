a90ff802bd5de4efdf6a6c5e8813ca44ff9c4f45
ycaihappy <ycaihappy@163.com>
2013-03-14 11:29:43 +0800

    index news

--- code/webroot/protected/widgets/views/index_news.php
+++ code/webroot/protected/widgets/views/index_news.php
@@ -1,25 +1,19 @@
 		<div class="m-new-hx" id="J_New_Tab_List">
 			<div class="hd">
-				<span class="on"><a href="">推荐行情</a></span>
-				<span class=""><a href="">新闻政策</a></span>
+				<span class="on"><a href="">行业新闻</a></span>
+				<span class=""><a href="">国内新闻</a></span>
 				<div class="clearfix"></div>
 			</div>
 			<div class="bd">
 				<ul class="on">
-					<li><a href="">沈阳地震主播淡定沈阳地震主播淡1</a></li>
-					<li><a href="">沈阳地震主播淡定沈阳地震主播淡定</a></li>
-					<li><a href="">沈阳地震主播淡定沈阳地震主播淡定</a></li>
-					<li><a href="">沈阳地震主播淡定沈阳地震主播淡定</a></li>
-					<li><a href="">沈阳地震主播淡定沈阳地震主播淡定</a></li>
-					<li><a href="">沈阳地震主播淡定沈阳地震主播淡定</a></li>
+		<?php for($index=0;$index<6;$index++):?>
+                            <li><a href="<?php echo Yii::app()->controller->createUrl('news/view',array('art_id'=>$data[$index]['art_id']));?>" target="_blank"><?php echo $data[$index]['art_title'] ?></a></li>
+					<?php endfor;?>			
 				</ul>
 				<ul>
-					<li><a href="">沈阳地震主播淡2222</a></li>
-					<li><a href="">沈阳地震主播淡定沈阳地震主播淡定</a></li>
-					<li><a href="">沈阳地震主播淡定沈阳地震主播淡定</a></li>
-					<li><a href="">沈阳地震主播淡定沈阳地震主播淡定</a></li>
-					<li><a href="">沈阳地震主播淡定沈阳地震主播淡定</a></li>
-					<li><a href="">沈阳地震主播淡定沈阳地震主播淡定</a></li>
+        <?php for($index=0;$index<6;$index++):?>
+                            <li><a href="<?php echo Yii::app()->controller->createUrl('news/view',array('art_id'=>$mu_news[$index]['art_id']));?>" target="_blank"><?php echo $mu_news[$index]['art_title'] ?></a></li>
+                    <?php endfor;?>			
 				</ul>
 			</div>
 		</div>

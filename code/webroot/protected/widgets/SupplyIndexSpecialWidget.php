2570f8f8203bb70399b8b80e42dfe5021985a20c
ycaihappy <ycaihappy@163.com>
2013-03-17 23:28:01 +0800

    commit

--- code/webroot/protected/widgets/SupplyIndexSpecialWidget.php
+++ code/webroot/protected/widgets/SupplyIndexSpecialWidget.php
@@ -12,13 +12,17 @@ class SupplyIndexSpecialWidget extends CWidget {
 			}
 		}
 
-        $cur_supply = Supply::model()->find('supply_id=:supply_id', array(':supply_id'=>$_REQUEST['supply_id']));
-        $criteria=new CDbCriteria;
-        $criteria->order='supply_id DESC';
-        $criteria->addCondition('supply_category_id='.$cur_supply->supply_category_id);
-        $criteria->limit = 8;
+        $supply_category_list = array();
+        if ( isset($_REQUEST['supply_id']) )
+        {
+            $cur_supply = Supply::model()->find('supply_id=:supply_id', array(':supply_id'=>$_REQUEST['supply_id']));
+            $criteria=new CDbCriteria;
+            $criteria->order='supply_id DESC';
+            $criteria->addCondition('supply_category_id='.$cur_supply->supply_category_id);
+            $criteria->limit = 8;
 
-        $supply_category_list =Supply::model()->findAll($criteria);
+            $supply_category_list =Supply::model()->findAll($criteria);
+        }
 		$data=compact('topSpecial','supply_category_list');
 		$this->render('supply_index_special',$data);
 	}

8327b10f988f45dcb15e599ea39d73395e478e2d
ycaihappy <ycaihappy@163.com>
2013-03-17 23:23:22 +0800

    supply

--- code/webroot/protected/widgets/SupplyIndexSpecialWidget.php
+++ code/webroot/protected/widgets/SupplyIndexSpecialWidget.php
@@ -11,7 +11,15 @@ class SupplyIndexSpecialWidget extends CWidget {
 				$special->product_id=$this->getController()->createUrl('/product/view',array('product_id'=>$special->product_id));
 			}
 		}
-		$data=compact('topSpecial');
+
+        $cur_supply = Supply::model()->find('supply_id=:supply_id', array(':supply_id'=>$_REQUEST['supply_id']));
+        $criteria=new CDbCriteria;
+        $criteria->order='supply_id DESC';
+        $criteria->addCondition('supply_category_id='.$cur_supply->supply_category_id);
+        $criteria->limit = 8;
+
+        $supply_category_list =Supply::model()->findAll($criteria);
+		$data=compact('topSpecial','supply_category_list');
 		$this->render('supply_index_special',$data);
 	}
 }

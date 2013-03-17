f3880b0131320804e524b3434b556156745b6145
ycaihappy <ycaihappy@163.com>
2013-03-17 21:04:44 +0800

    product category

--- code/webroot/protected/widgets/ProductCategoryWidget.php
+++ code/webroot/protected/widgets/ProductCategoryWidget.php
@@ -2,12 +2,15 @@
 class ProductCategoryWidget extends CWidget
 {
 
-	public function init()
-	{
-	}
-
 	public function run()
 	{
-		$this->render('product_top',array('type'=>$this->type, 'data'=>$this->newlist,'proTypes'=>$this->proTypes));
+        $cur_supply = Product::model()->find('product_id=:product_id', array(':product_id'=>$_REQUEST['product_id']));
+        $criteria=new CDbCriteria;
+        $criteria->order='product_id DESC';
+        $criteria->addCondition('product_type_id='.$cur_supply->product_type_id);
+        $criteria->limit = 8;
+
+        $product_category_list = Product::model()->findAll($criteria);
+		$this->render('product_category',array('data'=>$product_category_list));
 	}
 }

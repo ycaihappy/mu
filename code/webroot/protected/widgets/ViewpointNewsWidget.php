fef8fb8e6640d4542852c660963620d97a199769
ycaihappy <ycaihappy@163.com>
2013-03-19 13:38:19 +0800

    handle

--- code/webroot/protected/widgets/ViewpointNewsWidget.php
+++ code/webroot/protected/widgets/ViewpointNewsWidget.php
@@ -13,6 +13,7 @@ class ViewpointNewsWidget extends CWidget {
 			{//用其他字段封装链接
 				$news->art_title=CStringHelper::truncate_utf8_string($news->art_title, 20);
 				$news->art_source=$this->getController()->createUrl('/news/view',array('art_id'=>$news->art_id));
+				$news->art_content=CStringHelper::truncate_utf8_string($news->art_content,180);
 			}
 		}
 		$viewpointOne=array_shift($viewpoint);

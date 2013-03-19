f6d2875e1669c134258fc9b21adac1c372f8b937
ycaihappy <ycaihappy@163.com>
2013-03-19 13:58:03 +0800

    view

--- code/webroot/themes/classic/views/service/view.php
+++ code/webroot/themes/classic/views/service/view.php
@@ -83,7 +83,6 @@
                 <h6 class="bigcolmfont"><?php echo $service['art_title'];?></h6>
 			</div>
 			<div>
-            <h1><?php echo $service['art_title'];?></h1>
                   <div class="tit-bar clearfix">
 					<div class="ll">
                     <span class="color-a-0"></span><span class="bor-tit"></span><span class="color-a-1" ><a target="_blank" href=""><?php echo $service['art_source'];?></a></span><span class="infoMblog"><span class="color-a-2"></span></span><span class="article-time">发布时间：<?php echo $service['art_post_date'];?></span><span class="bor-tit"></span></div>

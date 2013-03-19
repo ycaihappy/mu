9dda5af59fa84b98fdf343c937753003b955e56e
ycaihappy <ycaihappy@163.com>
2013-03-19 12:32:04 +0800

    commit

--- code/webroot/protected/widgets/views/top_4_news.php
+++ code/webroot/protected/widgets/views/top_4_news.php
@@ -4,16 +4,6 @@
 	<?php echo $headNews->art_content;?>
 	</p>
 </div>
-
-<div class="news ui-m-tab ui-m-border">
-<div class="hd">
-	<span class="on">主推新闻</span>
-</div>
-<div class="bd">
-<h4>
-<a target="_blank" href="<?php echo $topOne->art_source?>"><?php echo $topOne->art_title?></a>
-</h4>
-
 <ul>
 <?php 
 if ($top4News):
@@ -21,7 +11,6 @@ if ($top4News):
  <li><a target="_blank" href="<?php echo $news->art_source?>"><?php echo $news->art_title?></a></li>
 <?php endforeach;
 endif;
-?> 
+?>        
 </ul>
-</div>
-</div>
+

be603dd8544c55fae76c24b50a18a462fbc6d5cf
ycaihappy <ycaihappy@163.com>
2013-03-20 20:11:41 +0800

    limit

--- code/webroot/protected/widgets/views/top_trends_news.php
+++ code/webroot/protected/widgets/views/top_trends_news.php
@@ -15,9 +15,13 @@
     <ul class="m2_ul">
 <?php 
 	if ($trendsNews):
-	 foreach ($trendsNews as $news):?>
+        $i=0;
+    foreach ($trendsNews as $news):
+        if ( $i > 9) continue;
+?>
 	 <li><a target="_blank" href="<?php echo $news->art_source?>"><?php echo $news->art_title?></a></li>
-	<?php endforeach;
+<?php $i++;
+endforeach;
 	endif;
 	?>
 </ul>

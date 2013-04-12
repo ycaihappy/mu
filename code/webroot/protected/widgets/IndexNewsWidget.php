df3f6ae7bfaad7a9bed73d8fe43c5d2de0da6244
ycaihappy <ycaihappy@163.com>
2013-04-12 21:00:10 +0800

    limit 8

--- code/webroot/protected/widgets/IndexNewsWidget.php
+++ code/webroot/protected/widgets/IndexNewsWidget.php
@@ -1,16 +1,11 @@
 <?php
 class IndexNewsWidget extends CWidget
 {
-    public $top_news;
-    public $top_mu_news;
-    public function init()
-    {
-    	$this->top_news = Article::model()->topTrendsNews()->findAll();
-    	$this->top_mu_news = Article::model()->topHotSpotNews()->findAll();
-    }
 
     public function run()
     {
-        $this->render('index_news',array('data'=>$this->top_news,'mu_news'=>$this->top_mu_news));
+    	$top_news= Article::model()->PriceMaterialList()->findAll();
+    	$top_mu_news= Article::model()->PriceSummaryList()->findAll();
+        $this->render('index_news',array('data'=>$top_news,'mu_news'=>$top_mu_news));
     }
 }

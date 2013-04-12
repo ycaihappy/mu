327d13f9a611eaafa970a86436f56a3157ef7650
ycaihappy <ycaihappy@163.com>
2013-04-12 21:00:25 +0800

    limit 8

--- code/webroot/protected/widgets/views/index_news.php
+++ code/webroot/protected/widgets/views/index_news.php
@@ -1,4 +1,4 @@
-<?php   if($this->beginCache('indexNews')){ ?>       
+<?php  # if($this->beginCache('indexNews')){ ?>       
         <div class="m-new-hx" id="J_New_Tab_List">
 			<div class="hd">
 				<span class="on"><a href="">今日行情</a></span>
@@ -7,15 +7,15 @@
 			</div>
 			<div class="bd">
 				<ul class="on">
-		<?php for($index=0;$index<6;$index++):?>
+		<?php for($index=0;$index<8;$index++):?>
                             <li><a href="<?php echo Yii::app()->controller->createUrl('news/view',array('art_id'=>$data[$index]['art_id']));?>" target="_blank"><?php echo $data[$index]['art_title'] ?></a></li>
 					<?php endfor;?>			
 				</ul>
 				<ul>
-        <?php for($index=0;$index<6;$index++):?>
+        <?php for($index=0;$index<8;$index++):?>
                             <li><a href="<?php echo Yii::app()->controller->createUrl('news/view',array('art_id'=>$mu_news[$index]['art_id']));?>" target="_blank"><?php echo $mu_news[$index]['art_title'] ?></a></li>
                     <?php endfor;?>			
 				</ul>
 			</div>
 		</div>
-<?php $this->endCache(); } ?>
+<?php #$this->endCache(); } ?>

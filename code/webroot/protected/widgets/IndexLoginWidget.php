5d20e23c9d0b20116b3e50c79f436f55c438c2cc
ycaihappy <ycaihappy@163.com>
2013-04-12 09:33:00 +0800

    commit

--- code/webroot/protected/widgets/IndexLoginWidget.php
+++ code/webroot/protected/widgets/IndexLoginWidget.php
@@ -4,16 +4,17 @@ class IndexLoginWidget extends CWidget
 
     public function run()
     {
+    	$adv=CCacheHelper::getAdvertisement(144);
         if ( Yii::app()->user->getID())
         {
         $user = User::model()->findbyPk(Yii::app()->user->getID());
 
-        $this->render('index_login',array('user_name'=>$user->user_name));
+        $this->render('index_login',array('user_name'=>$user->user_name,'adv'=>$adv));
         }
         else
         {
             
-        $this->render('index_login');
+        $this->render('index_login',array('adv'=>$adv));
         }
     }
 }

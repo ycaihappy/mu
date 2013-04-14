c03a6af22c1fa71bc80036b62a8baaa91f72f19c
ycaihappy <ycaihappy@163.com>
2013-04-14 23:36:12 +0800

    hanld

--- code/webroot/protected/widgets/views/theme_product.php
+++ code/webroot/protected/widgets/views/theme_product.php
@@ -7,14 +7,14 @@
 			<ul>
 			<table cellspacing="0" cellpadding="0" width="100%">
 				<tbody><tr><th>品种</th><th>价格</th><th>涨跌</th><th>市场</th><th>日期</th></tr>
+<?php
+for($i=0;$i<7;$i++)
+{
+?>
 				<tr><td>钼精矿</td><td>1800</td><td> ↑ 20</td><td>浙江有色</td><td>2013-04-11</td></tr>
-				<tr><td>钼精矿</td><td>1803</td><td> ↑ 12</td><td>浙江有色</td><td>2013-04-10</td></tr>
-				<tr><td>钼精矿</td><td>1810</td><td> - </td><td>浙江有色</td><td>2013-04-09</td></tr>
-				<tr><td>钼精矿</td><td>1820</td><td> ↑ 20</td><td>浙江有色</td><td>2013-04-08</td></tr>
-				<tr><td>钼精矿</td><td>1830</td><td> ↑ 44</td><td>浙江有色</td><td>2013-04-07</td></tr>
-				<tr><td>钼精矿</td><td>1808</td><td> - </td><td>浙江有色</td><td>2013-04-06</td></tr>
-				<tr><td>钼精矿</td><td>1807</td><td> ↑ 20</td><td>浙江有色</td><td>2013-04-05</td></tr>
-
+<?php
+}
+?>
 			</tbody></table>
 			</ul>
 			

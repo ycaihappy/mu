e8a85f8cb34ac887035899d8acee6ed2129401f2
ycaihappy <ycaihappy@163.com>
2013-04-14 23:30:38 +0800

    handle theme

--- code/webroot/themes/classic/views/theme/special.php
+++ code/webroot/themes/classic/views/theme/special.php
@@ -1,48 +1,7 @@
 <div class="layout-area">
 	<!--module start-->
-	<div class="price-area special-index">
-		<div class="ui-purple-hd  ui-m-tab4 ui-m-border">
-				<span class="on">现货价格</span>
-				
-		</div>
-		<div class="bd">
-			<ul>
-			<table cellspacing="0" cellpadding="0" width="100%">
-				<tbody><tr><th>品种</th><th>价格</th><th>涨跌</th><th>市场</th><th>日期</th></tr>
-				<tr><td>钼精矿</td><td>1800</td><td> ↑ 20</td><td>浙江有色</td><td>2013-04-11</td></tr>
-				<tr><td>钼精矿</td><td>1803</td><td> ↑ 12</td><td>浙江有色</td><td>2013-04-10</td></tr>
-				<tr><td>钼精矿</td><td>1810</td><td> - </td><td>浙江有色</td><td>2013-04-09</td></tr>
-				<tr><td>钼精矿</td><td>1820</td><td> ↑ 20</td><td>浙江有色</td><td>2013-04-08</td></tr>
-				<tr><td>钼精矿</td><td>1830</td><td> ↑ 44</td><td>浙江有色</td><td>2013-04-07</td></tr>
-				<tr><td>钼精矿</td><td>1808</td><td> - </td><td>浙江有色</td><td>2013-04-06</td></tr>
-				<tr><td>钼精矿</td><td>1807</td><td> ↑ 20</td><td>浙江有色</td><td>2013-04-05</td></tr>
-
-			</tbody></table>
-			</ul>
-			
-		</div>
-	</div>
-	<div class="price-area special-index">
-		<div class="ui-purple-hd  ui-m-tab4 ui-m-border">
-				<span class="on">市场价格</span>				
-		</div>
-		<div class="bd">
-		
-			<ul>
-			<table cellspacing="0" cellpadding="0" width="100%">
-				<tbody><tr><th>品种</th><th>价格</th><th>涨跌</th><th>市场</th><th>日期</th></tr>
-				<tr><td>钼精矿</td><td>1800</td><td> ↑ 23</td><td>浙江有色</td><td>2013-04-11</td></tr>
-				<tr><td>钼精矿</td><td>1802</td><td> ↑ 23</td><td>浙江有色</td><td>2013-04-11</td></tr>
-				<tr><td>钼精矿</td><td>1804</td><td> ↑ 21</td><td>浙江有色</td><td>2013-04-11</td></tr>
-				<tr><td>钼精矿</td><td>1806</td><td> - </td><td>浙江有色</td><td>2013-04-11</td></tr>
-				<tr><td>钼精矿</td><td>1807</td><td> ↑ 23</td><td>浙江有色</td><td>2013-04-11</td></tr>
-				<tr><td>钼精矿</td><td>1808</td><td> ↑ 23</td><td>浙江有色</td><td>2013-04-11</td></tr>
-				<tr><td>钼精矿</td><td>1809</td><td> - </td><td>浙江有色</td><td>2013-04-11</td></tr>
-				
-			</tbody></table>
-			</ul>
-		</div>
-	</div>
+       <?php $this->widget('ThemeProductWidget');?>
+       <?php $this->widget('ThemeMarketWidget');?>
 	<!--module end-->
 	<!--module start-->
 	<div id="J_IndexAd" class="m-special-ad">

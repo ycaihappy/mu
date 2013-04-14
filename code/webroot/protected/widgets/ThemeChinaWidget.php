122177250182789c050cfb6bb04bb1cada6c08d7
ycaihappy <ycaihappy@163.com>
2013-04-14 23:28:21 +0800

    handle news

--- code/webroot/protected/widgets/ThemeChinaWidget.php
+++ code/webroot/protected/widgets/ThemeChinaWidget.php
@@ -3,6 +3,15 @@ class ThemeChinaWidget extends CWidget
 {
     public function run()
     {
-        $this->render('theme_china',array());
+        $category = Term::model()->getTermsListByGroupId(14); 
+        $criteria=new CDbCriteria;
+        $criteria->order='art_id DESC';
+        $criteria->addCondition('art_category_id in (63,64,65,66,67,68)');
+        $criteria->addCondition('art_title like "%'.$category[$_GET['type']].'%"');
+        $criteria->limit = 20;
+
+
+        $art_list =Article::model()->findAll($criteria);
+        $this->render('theme_china',array('data'=>$art_list,'title'=>$category[$_GET['type']]));
     }
 }

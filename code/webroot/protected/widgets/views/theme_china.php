bd35b71e1437257a316f2d05c124332b45c6925a
ycaihappy <ycaihappy@163.com>
2013-04-14 23:28:46 +0800

    cm

--- code/webroot/protected/widgets/views/theme_china.php
+++ code/webroot/protected/widgets/views/theme_china.php
@@ -1,15 +1,19 @@
-                <div class="hd"><span class="on"><a href="#">国内标准</a></span></div>
-                 
-				<div class="bd">
-				<h3 class="bigsize"><a target="_blank" href="/index.php?r=knowledge/view&amp;art_id=1067">钼钨氧化矿选矿工艺</a></h3><a target="_blank" href="/index.php?r=knowledge/view&amp;art_id=1067">
-				</a><ul class="mod-list main-list"><a target="_blank" href="/index.php?r=knowledge/view&amp;art_id=1067">
-																</a><li><a target="_blank" href="/index.php?r=knowledge/view&amp;art_id=1067"></a><a target="_blank" href="/index.php?r=knowledge/view&amp;art_id=1069">钼铁的牌号及其用途</a></li>
-																		<li><a target="_blank" href="/index.php?r=knowledge/view&amp;art_id=1081">钼及其化合物的物理化学性质</a></li>
-																		<li><a target="_blank" href="/index.php?r=knowledge/view&amp;art_id=1082">金堆城钼业公司选矿工艺现状分析</a></li>
-																		<li><a target="_blank" href="/index.php?r=knowledge/view&amp;art_id=1090">关于钼的一般常识</a></li>
-																		<li><a target="_blank" href="/index.php?r=knowledge/view&amp;art_id=1093">磨矿介质对钼精矿碳含量影响研究</a></li>
-																		<li><a target="_blank" href="/index.php?r=knowledge/view&amp;art_id=1094">钼及其化合物的物理化学性质</a></li>
-																		<li><a target="_blank" href="/index.php?r=knowledge/view&amp;art_id=1095">钼及其化合物的物理化学性质</a></li>
-												</ul>
-				 </div>
-         </div>
+  <div class="ui-m-tab ui-m-border">
+<div class="hd"><span class="on"><a href="#"><?php echo $title;?>百科</a></span></div>
+
+        <div class="bd">
+      <ul class="mod-list main-list">
+<?php
+$i = 0;
+foreach ($data as $art_one)
+{
+    if ( $i>8) continue;
+?>
+    <li><a target="_blank" href="<?php echo Yii::app()->controller->createUrl('news/view',array('art_id'=>$art_one->art_id));?>"><?php echo $art_one->art_title;?></a></li>
+<?php
+    $i++;
+}
+?>
+           </ul>
+        </div>
+</div>
--- code/webroot/protected/widgets/views/theme_world.php
+++ code/webroot/protected/widgets/views/theme_world.php
@@ -1,16 +1,19 @@
 		 <div class="ui-m-tab ui-m-border">
-			   <div class="hd"><span class="on"><a href="#">国际标准</a></span></div>
+<div class="hd"><span class="on"><a href="#"><?php echo $title;?>新闻</a></span></div>
                
                 <div class="bd">
-                     <h3 class="bigsize"><a target="_blank" href="/index.php?r=knowledge/view&amp;art_id=1063">金堆城钼业公司选矿工艺现状分析</a></h3>
                      <ul class="mod-list main-list">
-   			                            <li><a target="_blank" href="/index.php?r=knowledge/view&amp;art_id=1065">钼及其化合物的物理化学性质</a></li>
-					                            <li><a target="_blank" href="/index.php?r=knowledge/view&amp;art_id=1066">金堆城钼业公司选矿工艺现状分析</a></li>
-					                            <li><a target="_blank" href="/index.php?r=knowledge/view&amp;art_id=1071">钼钨氧化矿选矿工艺</a></li>
-					                            <li><a target="_blank" href="/index.php?r=knowledge/view&amp;art_id=1074">钼铁的冶炼</a></li>
-					                            <li><a target="_blank" href="/index.php?r=knowledge/view&amp;art_id=1076">钼及其化合物的物理化学性质</a></li>
-					                            <li><a target="_blank" href="/index.php?r=knowledge/view&amp;art_id=1080">钼铁的冶炼</a></li>
-					                          
+   		<?php
+$i = 0;
+foreach ($data as $art_one)
+{
+    if ( $i>8) continue;
+?>
+    <li><a target="_blank" href="<?php echo Yii::app()->controller->createUrl('news/view',array('art_id'=>$art_one->art_id));?>"><?php echo $art_one->art_title;?></a></li>
+<?php
+    $i++;
+}
+?>			                          
 								
                      </ul>
                     

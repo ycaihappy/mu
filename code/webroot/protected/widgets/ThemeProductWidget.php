9fd7953c960b25fc121fbb8fad9db92199b24d2f
ycaihappy <ycaihappy@163.com>
2013-04-14 23:36:37 +0800

    commit

--- code/webroot/protected/widgets/ThemeProductWidget.php
+++ code/webroot/protected/widgets/ThemeProductWidget.php
@@ -3,6 +3,20 @@ class ThemeProductWidget extends CWidget
 {
     public function run()
     {
-        $this->render('theme_product',array());
+        $unit_type= Term::model()->getTermsListByGroupId(2);
+
+        $creteria=new CDbCriteria();
+        $creteria->condition="city_mu=1 and city_level=2";
+        $creteria->limit =3;
+        $city_three=City::model()->findAll($creteria);
+        foreach ($city_three as $city_one)
+        {
+            $select_city[$city_one->city_name] = $city_one->city_id;
+        }
+        $connection = Yii::app()->db;
+        $sql = 'select sum_unit,sum_product_type,sum_product_zone,sum_price from mu_price_summary where sum_product_zone in ('.implode(',',$select_city).' and sum_product_type='.$_GET['type'].')
+            group by sum_product_type,sum_product_zone';
+        $day_price = $connection->createCommand($sql)->queryAll();
+        $this->render('theme_product',array('data'=>$day_price));
     }
 }

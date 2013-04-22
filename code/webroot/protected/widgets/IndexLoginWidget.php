47c8ce79194e4f98edc0866c09238d6e4fcd7ab1
ycaihappy <ycaihappy@163.com>
2013-04-22 22:27:44 +0800

    server auto

--- code/webroot/protected/widgets/IndexLoginWidget.php
+++ code/webroot/protected/widgets/IndexLoginWidget.php
@@ -6,16 +6,17 @@ class IndexLoginWidget extends CWidget
     {
     	$adv=CCacheHelper::getAdvertisement(157);
     	$adv2=CCacheHelper::getAdvertisement(158);
+
+        $service_list = Article::model()->service()->findAll();
+        $server_type = Term::getTermsListByGroupId(10);
         if ( Yii::app()->user->getID())
         {
-        $user = User::model()->findbyPk(Yii::app()->user->getID());
-
-        $this->render('index_login',array('user_name'=>$user->user_name,'adv'=>$adv));
+            $user = User::model()->findbyPk(Yii::app()->user->getID());
+            $this->render('index_login',array('user_name'=>$user->user_name,'adv'=>$adv,'slist'=>$service_list,'stype'=>$server_type));
         }
         else
         {
-            
-        $this->render('index_login',array('adv'=>$adv,'adv2'=>$adv2));
+            $this->render('index_login',array('adv'=>$adv,'adv2'=>$adv2,'slist'=>$service_list,'stype'=>$server_type));
         }
     }
 }

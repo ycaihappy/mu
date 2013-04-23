440883786479271fbc62fe137e395c438acf9524
ycaihappy <ycaihappy@163.com>
2013-04-23 10:21:50 +0800

    commit

--- code/webroot/protected/widgets/PriceNewsWidget.php
+++ code/webroot/protected/widgets/PriceNewsWidget.php
@@ -4,8 +4,10 @@ class PriceNewsWidget extends CWidget
     public function run()
     {
         $top_news = $top_mu_news = array();
-    	$top_news = Article::model()->PriceMarketList()->findAll();
-        $top_mu_news = Article::model()->PriceAnalyList()->findAll();
+    #	$top_news = Article::model()->PriceMarketList()->findAll();
+    #    $top_mu_news = Article::model()->PriceAnalyList()->findAll();
+    	$top_news = Article::model()->PriceDayList()->findAll();
+    	$top_mu_news= Article::model()->PriceWeekList()->findAll();
         $this->render('price_news',array('data'=>$top_news,'mu_news'=>$top_mu_news));
     }
 }

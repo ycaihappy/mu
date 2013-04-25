094bfaefb813765d5f06cc2448ddcb499f8d23d8
ycaihappy <ycaihappy@163.com>
2013-04-26 00:22:45 +0800

    price summary

--- code/webroot/protected/controllers/PriceController.php
+++ code/webroot/protected/controllers/PriceController.php
@@ -39,34 +39,21 @@ class PriceController extends BasicAccessController
 	}
 	public function actionSummary() {
 		$newsCategoryId=(int)@$_REQUEST['subcategory_id'];
-		if($newsCategoryId)
-		{
-			$newsCriteria=new CDbCriteria();
-			$newsCriteria->select='art_id,art_title,art_post_date';
-			$newsCriteria->condition='art_status=1 and art_category_id=16 and art_subcategory_id='.$newsCategoryId;
-			$newsCriteria->order='art_post_date desc';
-			$count = Article::model()->count($newsCriteria);//
-			$pager = new CPagination($count);
-			$pager -> pageSize = 25; 
-			$pager->applyLimit($newsCriteria);
-			$newses=Article::model()->findAll($newsCriteria);
-			if($newses)
-			{
-				foreach ($newses as &$news)
-				{//用其他字段封装链接
-					$news->art_title=CStringHelper::truncate_utf8_string($news->art_title, 20);
-					$news->art_source=$this->createUrl('/news/view',array('art_id'=>$news->art_id));
-				}
-			}
-			$allTerm=CCacheHelper::getAllTerm();
-			$categoryName=$allTerm[$newsCategoryId]->term_name;
-			$data=compact('newses','pager','categoryName');
-			$this->siteConfig->siteMetaTitle=$categoryName;
-		}
-		else {
-			$this->redirect(array('index'));
-		}
-		$this->render('summary',$data);
+        $sql = 'select re_type,re_added_time, re_market, re_price from mu_relative_re_price where re_type in(148,149,163)
+            and re_name_type='.$newsCategoryId.' group by re_added_time';
+
+        $connection = Yii::app()->db;
+        $data = $connection->createCommand($sql)->queryAll();
+        if ( !empty($data) )
+        {
+            foreach ( $data as $data_one)
+            {
+                $date_key = date("Ymd", strtotime($data_one['re_added_time']));
+                $sum[$date_key][$data_one['re_type']] = $data_one['re_price']."(".$data_one['re_market'].")";
+            }
+        }
+
+		$this->render('summary',array('summary'=>$sum));
 	}
 	
     public function actionQuery()

3245bdc2ee6c654faeb94c75778cb7f6c44125d6
ycaihappy <ycaihappy@163.com>
2013-04-26 20:56:00 +0800

    handle news

--- code/webroot/protected/widgets/KnowNewsWidget.php
+++ code/webroot/protected/widgets/KnowNewsWidget.php
@@ -4,9 +4,10 @@ class KnowNewsWidget extends CWidget
     public function run()
     {
     	$KnowledgeList01 = Article::model()->knowledgeProductList()->findAll();
+        $KnowledgeList04 = Article::model()->topTrendsNews()->findAll(); 
 
         $adv1=CCacheHelper::getAdvertisement(142);
         if ( !empty($KnowledgeList01) )
-        $this->render('know_news',array('adv1'=>$adv1,'data01'=>$KnowledgeList01));
+        $this->render('know_news',array('adv1'=>$adv1,'data01'=>$KnowledgeList01,'data04'=>$KnowledgeList04));
     }
 }

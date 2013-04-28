3f355dd86d56915027d0f5124a39b18346e44430
ycaihappy <ycaihappy@163.com>
2013-04-28 11:47:15 +0800

    commit

--- code/webroot/protected/widgets/ThemeProductWidget.php
+++ code/webroot/protected/widgets/ThemeProductWidget.php
@@ -11,7 +11,7 @@ class ThemeProductWidget extends CWidget
     	$selectedType=Yii::app()->request->getParam('type',31);//默认选择钼初级
     	//产品推荐
     	$recProductCriteria=new CDbCriteria();
-    	$recProductCriteria->select='product_id,product_name,product_image_src,product_mu_content,product_quanity,product_price,product_water_content';
+    	$recProductCriteria->select='product_id,product_name,product_image_src,product_mu_content,product_quanity,product_price,product_water_content,product_join_date';
     	$recProductCriteria->with=array('unit'=>array('select'=>'term_name'),'user'=>array('select'=>'user_name,user_first_name,user_mobile_no'),'type'=>array('select'=>'term_name'),'city'=>array('select'=>'city_name'));
     	$recProductCriteria->condition='product_status=1 and product_type_id='.$selectedType;
     	$recProductCriteria->order='product_id desc';

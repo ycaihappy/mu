fd010536f046950dd3d574b3be968ca11024c263
ycaihappy <ycaihappy@163.com>
2013-04-28 11:01:53 +0800

    handle product

--- code/webroot/protected/widgets/IndexProductWidget.php
+++ code/webroot/protected/widgets/IndexProductWidget.php
@@ -13,7 +13,7 @@ class IndexProductWidget extends CWidget
     	$typies=Term::getMuCategoryByGroup();
     	//产品推荐
     	$recProductCriteria=new CDbCriteria();
-    	$recProductCriteria->select='product_id,product_name,product_image_src,product_mu_content,product_quanity,product_price,product_water_content';
+    	$recProductCriteria->select='product_id,product_name,product_image_src,product_mu_content,product_quanity,product_price,product_water_content,product_join_date';
     	$recProductCriteria->with=array('unit'=>array('select'=>'term_name'),'user'=>array('select'=>'user_name,user_first_name,user_mobile_no'),'type'=>array('select'=>'term_name'),'city'=>array('select'=>'city_name'));
     	$recProductCriteria->condition='product_status=1 and product_special=0';
     	$recProductCriteria->order='product_id desc';
